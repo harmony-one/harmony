91f8c6d4f724e1fb276543072fa22136e5ebbb7a
alok <myidforsub@gmail.com>
2018-05-24 00:14:17 -0700

    transaction generator pours transaction into a queue, every transaction is then broadcasted to all nodes

--- README.md
+++ README.md
@@ -21,12 +21,20 @@ more to come ..
 
 https://github.com/lightningnetwork/lnd
 
+
 #New Plan:
 
 https://docs.google.com/document/d/1u-8C9MiEUYPA7QC1Ekg7Cg-yL09ArZIxxyUtCUF9Wp0/edit
 
 #TODO
 
+#DEBUG
 https://gist.github.com/rushilgupta/228dfdf379121cb9426d5e90d34c5b96 
 
 to setup transaction channel and pour those txns (randomString) into a queue from where the leader node can read it in chunks.
+
+
+#TOREAD
+
+https://play.golang.org/p/jwdtDXVHJk
+https://stackoverflow.com/questions/15715605/multiple-goroutines-listening-on-one-channel/15721380#15721380
--- node.go
+++ node.go
@@ -5,84 +5,111 @@ import (
 	"math/rand"
 )
 
-var node_ips = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-var numOfTxns = 1000
-var numOfTxnsInBlock = 10
-var lenOfRandomString = 10
-
-
 type Node struct {
-	ip     int
-	leader bool
+	ip          int
+	leader      bool
+	reciveBlock chan string
+	sendBlock   chan string
 }
+
 type Nodes struct {
 	Nodes []*Node
 }
-type txn struct {
-	tx string
-}
-
 
 func randomInt(min, max int) int {
-    return min + rand.Intn(max-min)
+	return min + rand.Intn(max-min)
 }
 
 // Generate a random string of A-Z chars with len = l
 func randomString(len int) string {
-    bytes := make([]byte, len)
-    for i := 0; i < len; i++ {
-        bytes[i] = byte(randomInt(97, 122))
-    }
-    return string(bytes)
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		bytes[i] = byte(randomInt(97, 122))
+	}
+	return string(bytes)
 }
 
-func (n Node) send() {
-	fmt.Printf("Leader with id: %d has sent message\n", n.ip)
+func (n Node) send(cin <-chan string, id int) {
+	for msg := range cin {
+		fmt.Printf("Leader has sent message %s to %d\n", msg, id)
+	}
 }
+
 func (n Node) receive() {
 	fmt.Printf("Node: %d received message\n", n.ip)
 }
+
 func createNode(ip int, isLeader bool) Node {
 	n := Node{ip: ip, leader: isLeader}
 	return n
 }
-func pickLeader(i int) bool {
+
+func pickLeader(i int) Node {
 	if i == 0 {
-		return true
+		return createNode(i, true)
 	} else {
-		return false
+		return createNode(i, false)
 	}
 }
 
-var N = make([]Node, 10)
+func BufferedTxnQueueWithFanOut(ch <-chan string, size int) []chan string { // This needs
+	cs := make([]chan string, size)
+	for i, _ := range cs {
+		// The size of the channels buffer controls how far behind the recievers
+		// of the fanOut channels can lag the other channels.
+		cs[i] = make(chan string)
+	}
+	go func() {
+		for txs := range ch {
+			for _, c := range cs {
+				c <- txs
+			}
+		}
+		for _, c := range cs {
+			// close all our fanOut channels when the input channel is exhausted.
+			close(c)
+		}
+	}()
+	return cs
+}
 
-func gen() <- chan string{
+func TxnGenerator(numOfTxns int, lenOfRandomString int) <-chan string {
 	out := make(chan string)
 	go func() {
-		for i :=0; i<numOfTxns;i++{
+		for i := 0; i < numOfTxns; i++ {
 			out <- randomString(lenOfRandomString)
 		}
 		close(out)
 	}()
 	return out
 }
+
 func main() {
-	for i, id := range node_ips {
-		isLeader := pickLeader(i)
-		m := createNode(id, isLeader)
+	var (
+		isLeader          Node
+		numOfTxns         = 1000
+		numOfTxnsInBlock  = 10
+		N                 = make([]Node, 10)
+		lenOfRandomString = 10
+		node_ips          = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	)
+	for i := range node_ips {
+		m := pickLeader(i)
 		N[i] = m
-	}
-	for i, _ := range N {
-		m := N[i]
 		if m.leader {
-			m.send()
-		} else {
-			m.receive()
+			isLeader = m
 		}
 	}
-	k := gen()
-	for i := 0; i < 100; i++ {
-		fmt.Println(<-k)
-}
+	txnqueue := TxnGenerator(numOfTxns, lenOfRandomString)
+	blockFullOfTxns := BufferedTxnQueueWithFanOut(txnqueue, numOfTxnsInBlock)
+	for num := range blockFullOfTxns {
+		fmt.Println(num)
+		txn := blockFullOfTxns[num]
+		for i, m := range N {
+			if !m.leader {
+				go isLeader.send(txn, i)
+			}
+		}
 
+	}
 }
