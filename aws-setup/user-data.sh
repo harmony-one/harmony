4bd9c782ce63c2e2b6fb9fdf98a49d04aab37973
alok <myidforsub@gmail.com>
2018-06-08 15:07:20 -0700

    app spec

--- aws-setup/user-data.sh
+++ aws-setup/user-data.sh
@@ -1,7 +1,9 @@
-sudo yum update
-sudo yum install wget
-REGION=$(curl 16.9.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[a-z]$//' )
+#!/bin/bash -x
+REGION=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/[a-z]$//')
+yum update -y
+yum install ruby wget -y
 cd /home/ec2-user
-wget https://aws-codedeploy-$REGION.s3.amazonaws.com/lates/install
+wget https://aws-codedeploy-$REGION.s3.amazonaws.com/latest/install
 chmod +x ./install
-sudo ./install auto
+./install auto
+mkdir projects
