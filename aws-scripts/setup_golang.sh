0cd19013729e7d042eb06c5ba8cf4389bd87b9b4
alok <alok@simple-rules.com>
2018-06-11 22:30:01 -0700

    adjusting path of transfer of code

--- aws-scripts/setup_golang.sh
+++ aws-scripts/setup_golang.sh
@@ -1,5 +1,3 @@
-cd /home/ec2-user/
-mkdir -p projects/src
 
 sudo yum update -y
 sudo yum install -y golang
@@ -10,14 +8,5 @@ export GOROOT=/usr/lib/golang
 # GOPATH is the location of your work directory
 export GOPATH=$HOME/projects
 
-
 # PATH in order to access go binary system wide
 export PATH=$PATH:$GOROOT/bin
-
-# GOROOT is the location where Go package is installed on your system
-export GOROOT=/usr/lib/golang
-
-# GOPATH is the location of your work directory
-export GOPATH=$HOME/projects
-
-# PATH in order to access go binary system wide
