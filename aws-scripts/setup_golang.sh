12a5eec030578ee10018f97ca2bb323244d474ba
alok <alok@simple-rules.com>
2018-06-11 22:03:13 -0700

    setting up golang and golang paths via codedeploy

---
+++ aws-scripts/setup_golang.sh
@@ -0,0 +1,30 @@
+sudo yum update -y
+sudo yum install -y golang
+
+# GOROOT is the location where Go package is installed on your system
+export GOROOT=/usr/lib/golang
+
+# GOPATH is the location of your work directory
+export GOPATH=$HOME/projects
+
+# PATH in order to access go binary system wide
+Aloks-iMac:harmony-benchmark alokkothari$ cat /Users/alokkothari/Documents/goworkspace/src/github.com/harmony-benchmark/setup_golang.sh
+sudo yum update -y
+sudo yum install -y golang
+
+# GOROOT is the location where Go package is installed on your system
+export GOROOT=/usr/lib/golang
+
+# GOPATH is the location of your work directory
+export GOPATH=$HOME/projects
+
+# PATH in order to access go binary system wide
+export PATH=$PATH:$GOROOT/bin
+
+# GOROOT is the location where Go package is installed on your system
+export GOROOT=/usr/lib/golang
+
+# GOPATH is the location of your work directory
+export GOPATH=$HOME/projects
+
+# PATH in order to access go binary system wide
