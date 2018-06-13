15f8bc607a0358c0d460b58575a1698514a5f329
Rongjian Lan <rongjian.lan@gmail.com>
2018-06-12 21:19:07 -0700

    Scaled consensus to 100 nodes。

--- p2p/peer.go
+++ p2p/peer.go
@@ -53,26 +53,25 @@ func ConstructP2pMessage(msgType byte, payload []byte) []byte {
 
 // SocketClient is to connect a socket given a port and send the given message.
 func sendWithSocketClient(ip, port string, message []byte) (res string) {
-	log.Printf("Sending message to ip %s and port %s\n", ip, port)
+	//log.Printf("Sending message to ip %s and port %s\n", ip, port)
 	addr := strings.Join([]string{ip, port}, ":")
 	conn, err := net.Dial("tcp", addr)
 
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
+		return
 	}
 	defer conn.Close()
 
 	conn.Write(message)
-	log.Printf("Sent to ip %s and port %s: %s\n", ip, port, message)
+	//log.Printf("Sent to ip %s and port %s: %s\n", ip, port, message)
 
 	// No ack (reply) message from the receiver for now.
-	// TODO: think about
 	return
 }
 
 // Send a message to another node with given port.
 func send(ip, port string, message []byte) (returnMessage string) {
-	returnMessage = sendWithSocketClient(ip, port, message)
-	log.Println(returnMessage)
+	sendWithSocketClient(ip, port, message)
 	return
 }
