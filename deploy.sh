33626014b4a17002369ab0dcb417256953a839fb
Richard Liu <liuzhenglaichn@gmail.com>
2018-06-15 22:27:42 -0700

    last line without '\n' won't be ignored

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 ./kill_node.sh
 ipfile=$1
-while read ip port mode; do 
+while IFS='' read -r line || [[ -n "$line" ]]; do
+  IFS=' ' read ip port mode <<< $line
 	#echo $ip $port $mode $ipfile
   go run ./benchmark_main.go -ip $ip -port $port -ipfile $ipfile&
 done < $ipfile
