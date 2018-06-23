40e485356e941a7f290172c9c7799fbe8a85d2f8
Richard Liu <liuzhenglaichn@gmail.com>
2018-06-22 19:52:34 -0700

    split log per node

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
+timestamp() {
+    date +"%Y%m%d-%H%M%S"
+}
+
+t=`timestamp`
+
 ./kill_node.sh
 config=$1
 while IFS='' read -r line || [[ -n "$line" ]]; do
   IFS=' ' read ip port mode <<< $line
 	#echo $ip $port $mode $config
-  go run ./benchmark_main.go -ip $ip -port $port -config_file $config&
+  go run ./benchmark_main.go -ip $ip -port $port -config_file $config -timestamp $t&
 done < $config
 
-go run ./aws-code/transaction_generator.go -config_file $config
+go run ./aws-code/transaction_generator.go -config_file $config -timestamp $t
