47962e73209f32049f174dd0d6533aa536ba79d4
alok <myidforsub@gmail.com>
2018-06-23 15:25:50 -0700

    reseting GOLANG paths

--- aws-scripts/setup_golang.sh
+++ aws-scripts/setup_golang.sh
@@ -2,17 +2,18 @@
 sudo yum update -y
 sudo yum install -y golang
 
+
 # GOROOT is the location where Go package is installed on your system
 echo "export GOROOT=/usr/lib/golang" >> $HOME/.bash_profile
 
 # GOPATH is the location of your work directory
-echo "export GOROOT=/usr/lib/golang" >> $HOME/.bash_profile
+echo "export GOPATH=$HOME/projects" >> $HOME/.bash_profile
 
 # PATH in order to access go binary system wide
 echo "export PATH=$PATH:$GOROOT/bin" >> $HOME/.bash_profile
 
 export GOROOT=/usr/lib/golang
-export GOROOT=/usr/lib/golang
+export GOPATH=$HOME/projects
 export PATH=$PATH:$GOROOT/bin
 source $HOME/.bash_profile
 sudo go get github.com/go-stack/stack
