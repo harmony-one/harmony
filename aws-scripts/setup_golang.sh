a19bd7f14ded10d17d432958393ae2a0babdcae4
alok <myidforsub@gmail.com>
2018-06-25 12:04:18 -0700

    HOME variable is different on AWS Linux setup

--- aws-scripts/setup_golang.sh
+++ aws-scripts/setup_golang.sh
@@ -2,19 +2,19 @@
 sudo yum update -y
 sudo yum install -y golang
 
-HOME=/home/ec2-user
+MyHOME=/home/ec2-user
 echo "now setting up go-lang paths"
 # GOROOT is the location where Go package is installed on your system
-echo "export GOROOT=/usr/lib/golang" >> $HOME/.bash_profile
+echo "export GOROOT=/usr/lib/golang" >> $MyHOME/.bash_profile
 
 # GOPATH is the location of your work directory
-echo "export GOPATH=$HOME/projects" >> $HOME/.bash_profile
+echo "export GOPATH=$MyHOME/projects" >> $MyHOME/.bash_profile
 
 # PATH in order to access go binary system wide
-echo "export PATH=$PATH:$GOROOT/bin" >> $HOME/.bash_profile
+echo "export PATH=$PATH:$GOROOT/bin" >> $MyHOME/.bash_profile
 
 export GOROOT=/usr/lib/golang
-export GOPATH=$HOME/projects
+export GOPATH=$MyHOME/projects
 export PATH=$PATH:$GOROOT/bin
-source $HOME/.bash_profile
+source $MyHOME/.bash_profile
 sudo go get github.com/go-stack/stack
