8cee77e82e6218aa9d02e3fb9dd3182e24995a63
Rongjian Lan <rongjian.lan@gmail.com>
2018-06-29 10:37:58 -0700

    Move tx generator client out of aws-code into client folder

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -19,4 +19,4 @@ while read ip port mode; do
 	#echo $ip $port $mode $config
   go run ./benchmark_main.go -ip $ip -port $port -config_file $config&
 done < $config
-go run ./aws-code/transaction_generator.go -config_file $config
+go run ./client/txgen/main.go -config_file $config
