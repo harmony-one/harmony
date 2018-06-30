a823e923977398d80ce6a3face2889c3092f9cde
alok <myidforsub@gmail.com>
2018-06-29 17:59:42 -0700

    create client and node isntances

--- aws-code/create_node_instances.sh
+++ aws-code/create_node_instances.sh
@@ -5,4 +5,4 @@ aws ec2 create-security-group --group-name mcDG --description "mcdg"
 aws ec2 authorize-security-group-ingress --group-name MySecurityGroup --protocol tcp  --port all --cidr 0.0.0.0/0
 
 aws ec2 run-instances --image-id ami-a9d09ed1 --count 5 --instance-type t2.micro --key-name main --security-group-ids mcDG \
---user-data user-data.sh --iam-instance-profile Name=CodeDeployDemo-EC2-Instance-Profile --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Client}]'
+--user-data user-data.sh --iam-instance-profile Name=CodeDeployDemo-EC2-Instance-Profile --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Node}]'
