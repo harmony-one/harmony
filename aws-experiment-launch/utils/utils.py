cb7ade981d10952f09bb20da2f982313c244663b
Minh Doan <minh@simple-rules.com>
2018-07-05 23:18:50 -0700

    add ip distribution

--- aws-experiment-launch/utils/utils.py
+++ aws-experiment-launch/utils/utils.py
@@ -8,9 +8,9 @@ REGION_HUMAN_NAME = 'region_human_name'
 INSTANCE_TYPE = 't2.small'
 REGION_AMI = 'region_ami'
 
-def read_configuration_file(filename='configuration.txt'):
+def read_configuration_file(region_config='configuration.txt'):
     config = {}
-    with open(filename, 'r') as f:
+    with open(region_config, 'r') as f:
         for myline in f:
             mylist = myline.strip().split(',')
             region_num = mylist[0]
@@ -22,8 +22,8 @@ def read_configuration_file(filename='configuration.txt'):
             config[region_num][REGION_AMI] = mylist[5]
     return config
 
-def collect_public_ips(region_number, node_name_tag, config):
-    config = read_configuration_file(config)
+def collect_public_ips(region_number, node_name_tag, region_config):
+    config = read_configuration_file(region_config)
     region_name = config[region_number][REGION_NAME]
     session = boto3.Session(region_name=region_name)
     ec2_client = session.client('ec2')
@@ -33,6 +33,25 @@ def collect_public_ips(region_number, node_name_tag, config):
     for reservation in response[u'Reservations']:
         ip_list.extend(instance['PublicIpAddress'] for instance in reservation['Instances'])
     return ip_list
+
+def generate_config_file(shard_num, client_num, ip_list, config_filename):
+    if len(ip_list) < shard_num * 2 + client_num:
+        print("Not enough nodes to generate a config file")
+        return False
+
+    # Create ip for clients.
+    client_id, leader_id, validator_id = 0, 0, 0
+    with open(config_filename, "w") as fout:
+        for i in range(len(ip_list)):
+            if client_id < client_num:
+                fout.write("%s 9000 client %d\n" % (ip_list[i], client_id % shard_num))
+                client_id = client_id + 1
+            elif leader_id < shard_num:
+                fout.write("%s 9000 leader %d\n" % (ip_list[i], leader_id))
+                leader_id = leader_id + 1
+            else:
+                fout.write("%s 9000 validator %d\n" % (ip_list[i], validator_id % shard_num))
+                validator_id = validator_id + 1
 
 if __name__ == "__main__":
     collect_public_ips('4', "4-NODE-14-46-47-2018-07-05", "configuration.txt")
