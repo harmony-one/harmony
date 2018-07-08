5d74815705cee1dc04549b2629d79b53e524ef6b
Minh Doan <minh@simple-rules.com>
2018-07-08 16:16:51 -0700

    add logging

--- aws-experiment-launch/terminate_instances.py
+++ aws-experiment-launch/terminate_instances.py
@@ -3,8 +3,14 @@ import argparse
 import os
 import random
 import sys
+import logging
 
 from utils import utils
+
+
+logging.basicConfig(level=logging.INFO, format='%(threadName)s %(asctime)s - %(name)s - %(levelname)s - %(message)s')
+LOGGER = logging.getLogger(__file__)
+LOGGER.setLevel(logging.INFO)
 
 def terminate_instances_by_region(region_number, region_config):
     ec2_client, _ = utils.create_ec2_client(region_number, args.region_config)
@@ -12,11 +18,12 @@ def terminate_instances_by_region(region_number, region_config):
     instance_ids = utils.get_instance_ids(ec2_client.describe_instances(Filters=filters))
     if instance_ids:
         ec2_client.terminate_instances(InstanceIds=instance_ids)
-        print "waiting until instances with tag %s died." % node_name_tag
+        LOGGER.info("waiting until instances with tag %s died." % node_name_tag)
         waiter = ec2_client.get_waiter('instance_terminated')
         waiter.wait(InstanceIds=instance_ids)
     else:
-        print 'there is no instances to terminate'
+        pass
+        LOGGER.warn("there is no instances to terminate")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='This script helps you to collect public ips')
@@ -30,14 +37,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if not args.instance_output or not os.path.isfile(args.instance_output):
-        print "%s is not existed" % args.instance_output
+        LOGGER.info("%s is not existed" % args.instance_output)
         sys.exit(1)
     if args.region_number and args.node_name_tag:
         ec2_client, session = utils.create_ec2_client(args.region_number, args.region_config)
         filters = [{'Name': 'tag:Name','Values': [args.node_name_tag]}]
         instance_ids = utils.get_instance_ids(ec2_client.describe_instances(Filters=filters))
         ec2_client.terminate_instances(InstanceIds=instance_ids)
-        print "waiting until instances with tag %s died." % args.node_name_tag
+        LOGGER.info("waiting until instances with tag %s died." % args.node_name_tag)
         waiter = ec2_client.get_waiter('instance_terminated')
         waiter.wait(InstanceIds=instance_ids)
     elif args.instance_output:
@@ -52,4 +59,4 @@ if __name__ == "__main__":
                 thread_pool.append(t)
             for t in thread_pool:
                 t.join()
-            print("done.")
+            LOGGER.info("done.")
