cbbf0039046346ad0ecc66df49bce0aceede2bca
Minh Doan <minh@simple-rules.com>
2018-07-10 11:46:29 -0700

    fix comments

--- aws-experiment-launch/create_deploy_pipeline.sh
+++ aws-experiment-launch/create_deploy_pipeline.sh
@@ -8,12 +8,8 @@ python create_instances.py --regions 1,2,3,4,5,6,7,8 --instances $INSTANCE_NUM,$
 echo "Rung collecint raw ips"
 python collect_public_ips.py --instance_output instance_output.txt
 
-echo "Rung collecint raw ips"
-python collect_public_ips.py --instance_output instance_output.txt
-
-echo "Rung collecint raw ips"
+echo "Generate distribution_config"
 python generate_distribution_config.py --ip_list_file raw_ip.txt --shard_num $SHARD_NUM --client_num $CLIENT_NUM
 
 echo "Deploy"
 python deploy.py
-
--- aws-experiment-launch/create_instances_only_50.sh
+++ aws-experiment-launch/create_instances_only_50.sh
@@ -1,4 +1,4 @@
-INSTANCE_NUM=50
+INSTANCE_NUM=$1
 
 echo "Creating $$INSTANCE_NUM instances at 8 regions"
 python create_instances.py --regions 1,2,3,4,5,6,7,8 --instances $INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM --instance_output instance_output_500.txt
