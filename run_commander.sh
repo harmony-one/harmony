cb020471d48a542be0a7f8a02e811e6c85da92e4
Minh Doan <minh@simple-rules.com>
2018-07-10 17:45:13 -0700

    modify logic

--- run_commander.sh
+++ run_commander.sh
@@ -2,7 +2,9 @@ cd ~/projects/src/harmony-benchmark/
 # Compile
 sudo go build -o bin/commander aws-experiment-launch/experiment/commander/main.go
 cd bin
+sudo cp /tmp/distribution_config.txt .
+sudo cp /tmp/commander_info.txt .
 # Take ip address
 IP=`head -n 1 commander_info.txt`
 # Run commander
-sudo ./bin/commander -ip $IP -port 9000 -config_file distribution_config.txt
+sudo ./commander -ip $IP -port 9000 -config_file distribution_config.txt
