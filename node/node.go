68f344ade5bc971b10daa5f82738da5ea4d88d97
Minh Doan <minh@simple-rules.com>
2018-07-11 13:10:19 -0700

    refactor node module

--- node/node.go
+++ node/node.go
@@ -8,7 +8,6 @@ import (
 	"harmony-benchmark/p2p"
 	"net"
 	"os"
-	"strconv"
 	"sync"
 )
 
@@ -84,17 +83,6 @@ func (node *Node) listenOnPort(port string) {
 
 func (node *Node) String() string {
 	return node.Consensus.String()
-}
-
-// [Testing code] Should be deleted after production
-// Creates in genesis block numAddress transactions which assign 1000 token to each address in [0 - numAddress)
-func (node *Node) AddTestingAddresses(numAddress int) {
-	txs := make([]*blockchain.Transaction, numAddress)
-	for i := range txs {
-		txs[i] = blockchain.NewCoinbaseTX(strconv.Itoa(i), "", node.Consensus.ShardID)
-	}
-	node.blockchain.Blocks[0].Transactions = append(node.blockchain.Blocks[0].Transactions, txs...)
-	node.UtxoPool.Update(txs)
 }
 
 // Count the total number of transactions in the blockchain
