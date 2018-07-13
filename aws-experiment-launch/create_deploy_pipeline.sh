798587894833c2ea97895b8e843617b6d35666e5
Minh Doan <minh@simple-rules.com>
2018-07-13 01:14:56 -0700

    make more sleep

--- aws-experiment-launch/create_deploy_pipeline.sh
+++ aws-experiment-launch/create_deploy_pipeline.sh
@@ -10,11 +10,14 @@ CLIENT_NUM=$3
 echo "Creating $INSTANCE_NUM instances at 8 regions"
 python create_instances.py --regions 1,2,3,4,5,6,7,8 --instances $INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM,$INSTANCE_NUM
 
+sleep 10
 echo "Rung collecint raw ips"
 python collect_public_ips.py --instance_output instance_output.txt
 
+sleep 10
 echo "Generate distribution_config"
 python generate_distribution_config.py --ip_list_file raw_ip.txt --shard_num $SHARD_NUM --client_num $CLIENT_NUM
 
+sleep 20
 echo "Deploy"
 python deploy.py
