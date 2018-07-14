ceb7da919920c8fc7cdbfc5c1860e6d7d78ad75b
Richard Liu <liuzhenglaichn@gmail.com>
2018-07-14 01:08:51 -0700

    use gist as source of truth

--- aws-experiment-launch/spot-instance/userdata.sh
+++ aws-experiment-launch/spot-instance/userdata.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-curl http://54.186.246.9:8080/soldier -o soldier
+commanderIP=`curl https://gist.githubusercontent.com/lzl124631x/c44471f85ced6743a173cc113393dc81/raw/4a6b73493f75363de5a61051c8e339294191ca00/commanderIP`
+curl http://$commanderIP:8080/soldier -o soldier
 chmod +x ./soldier
-curl http://54.186.246.9:8080/benchmark -o benchmark
+curl http://$commanderIP:8080/benchmark -o benchmark
 chmod +x ./benchmark
-curl http://54.186.246.9:8080/txgen -o txgen
+curl http://$commanderIP:8080/txgen -o txgen
 chmod +x ./txgen
 
 # Get My IP
 wget http://169.254.169.254/latest/meta-data/public-ipv4
 ip=$(head -n 1 public-ipv4)
-# Run soldier
+
 node_port=9000
+soldier_port=1$node_port
+# Kill existing soldier
+fuser -k -n tcp $soldier_port
+
+# Run soldier
 ./soldier -ip $ip -port $node_port > soldier_log 2>&1 &
