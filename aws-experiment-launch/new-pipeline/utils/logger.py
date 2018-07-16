0156d8fdb3a5744e5cca62cc2ff3bac2661f4536
Minh Doan <minh@simple-rules.com>
2018-07-16 16:52:51 -0700

    add logging debug for boto3 to see more logging

--- aws-experiment-launch/new-pipeline/utils/logger.py
+++ aws-experiment-launch/new-pipeline/utils/logger.py
@@ -2,6 +2,8 @@ import logging
 
 logging.basicConfig(level=logging.INFO,
                     format='%(threadName)s %(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger('botocore').setLevel(logging.DEBUG)
+logging.getLogger('boto3').setLevel(logging.DEBUG)
 
 def getLogger(file):
     LOGGER = logging.getLogger(file)
