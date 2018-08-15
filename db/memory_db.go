58f9571bb9ec948e2627615cc8ff0c1c937401f7
Minh Doan <minh@simple-rules.com>
2018-08-15 15:26:49 -0700

    add in-memory db used for db_test

---
+++ db/memory_db.go
@@ -0,0 +1,127 @@
+package db
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/simple-rules/harmony-benchmark/utils"
+)
+
+/*
+ * This is a test memory database. Do not use for any production it does not get persisted
+ */
+type MemDatabase struct {
+	db   map[string][]byte
+	lock sync.RWMutex
+}
+
+func NewMemDatabase() *MemDatabase {
+	return &MemDatabase{
+		db: make(map[string][]byte),
+	}
+}
+
+func NewMemDatabaseWithCap(size int) *MemDatabase {
+	return &MemDatabase{
+		db: make(map[string][]byte, size),
+	}
+}
+
+func (db *MemDatabase) Put(key []byte, value []byte) error {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	db.db[string(key)] = utils.CopyBytes(value)
+	return nil
+}
+
+func (db *MemDatabase) Has(key []byte) (bool, error) {
+	db.lock.RLock()
+	defer db.lock.RUnlock()
+
+	_, ok := db.db[string(key)]
+	return ok, nil
+}
+
+func (db *MemDatabase) Get(key []byte) ([]byte, error) {
+	db.lock.RLock()
+	defer db.lock.RUnlock()
+
+	if entry, ok := db.db[string(key)]; ok {
+		return utils.CopyBytes(entry), nil
+	}
+	return nil, errors.New("not found")
+}
+
+func (db *MemDatabase) Keys() [][]byte {
+	db.lock.RLock()
+	defer db.lock.RUnlock()
+
+	keys := [][]byte{}
+	for key := range db.db {
+		keys = append(keys, []byte(key))
+	}
+	return keys
+}
+
+func (db *MemDatabase) Delete(key []byte) error {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	delete(db.db, string(key))
+	return nil
+}
+
+func (db *MemDatabase) Close() {}
+
+func (db *MemDatabase) NewBatch() Batch {
+	return &memBatch{db: db}
+}
+
+func (db *MemDatabase) Len() int { return len(db.db) }
+
+type kv struct {
+	k, v []byte
+	del  bool
+}
+
+type memBatch struct {
+	db     *MemDatabase
+	writes []kv
+	size   int
+}
+
+func (b *memBatch) Put(key, value []byte) error {
+	b.writes = append(b.writes, kv{utils.CopyBytes(key), utils.CopyBytes(value), false})
+	b.size += len(value)
+	return nil
+}
+
+func (b *memBatch) Delete(key []byte) error {
+	b.writes = append(b.writes, kv{utils.CopyBytes(key), nil, true})
+	b.size += 1
+	return nil
+}
+
+func (b *memBatch) Write() error {
+	b.db.lock.Lock()
+	defer b.db.lock.Unlock()
+
+	for _, kv := range b.writes {
+		if kv.del {
+			delete(b.db.db, string(kv.k))
+			continue
+		}
+		b.db.db[string(kv.k)] = kv.v
+	}
+	return nil
+}
+
+func (b *memBatch) ValueSize() int {
+	return b.size
+}
+
+func (b *memBatch) Reset() {
+	b.writes = b.writes[:0]
+	b.size = 0
+}
