ca61713dc0e8551b31885c56766d84a6a385fd5b
Alok Kothari <alokkothari@aloks-mbp-2.lan>
2018-08-20 16:00:02 -0700

    making identity block

--- identitychain/identityblock.go
+++ identitychain/identityblock.go
@@ -1,9 +1,67 @@
 package identitychain
 
-import "github.com/simple-rules/harmony-benchmark/p2p"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"log"
+	"time"
+
+	"github.com/simple-rules/harmony-benchmark/utils"
+	"github.com/simple-rules/harmony-benchmark/waitnode"
+)
 
 // IdentityBlock has the information of one node
 type IdentityBlock struct {
-	Peer          p2p.Peer
+	Timestamp     int64
+	PrevBlockHash [32]byte
 	NumIdentities int32
+	Identities    []*waitnode.WaitNode
+}
+
+// Serialize serializes the block
+func (b *IdentityBlock) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+	return result.Bytes()
+}
+
+// DeserializeBlock deserializes a block
+func DeserializeBlock(d []byte) *IdentityBlock {
+	var block IdentityBlock
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+	return &block
+}
+
+// NewBlock creates and returns a new block.
+func NewBlock(Identities []*waitnode.WaitNode, prevBlockHash [32]byte) *IdentityBlock {
+	numTxs := int32(len(Identities))
+	var Ids []*waitnode.WaitNode
+	for _, ids := range Identities {
+		Ids = append(Ids, ids)
+	}
+	block := &IdentityBlock{Timestamp: time.Now().Unix(), PrevBlockHash: prevBlockHash, NumIdentities: numTxs, Identities: Ids}
+	return block
+}
+
+// CalculateBlockHash returns a hash of the block
+func (b *IdentityBlock) CalculateBlockHash() []byte {
+	var hashes [][]byte
+	var blockHash [32]byte
+	hashes = append(hashes, utils.ConvertFixedDataIntoByteArray(b.Timestamp))
+	hashes = append(hashes, b.PrevBlockHash[:])
+	for _, id := range b.Identities {
+		hashes = append(hashes, id)
+	}
+	hashes = append(hashes, utils.ConvertFixedDataIntoByteArray(b.ShardId))
+	blockHash = sha256.Sum256(bytes.Join(hashes, []byte{}))
+	return blockHash[:]
 }
