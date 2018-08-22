33ef9a096bf2ccbdf1bdfede1c945751a3f74372
Minh Doan <minh@simple-rules.com>
2018-08-22 14:54:23 -0700

    add more test

--- blockchain/merkle_tree_test.go
+++ blockchain/merkle_tree_test.go
@@ -37,12 +37,13 @@ func TestNewMerkleTree(t *testing.T) {
 		[]byte("node1"),
 		[]byte("node2"),
 		[]byte("node3"),
+		[]byte("node4"),
 	}
 	// Level 1
 	n1 := NewMerkleNode(nil, nil, data[0])
 	n2 := NewMerkleNode(nil, nil, data[1])
 	n3 := NewMerkleNode(nil, nil, data[2])
-	n4 := NewMerkleNode(nil, nil, data[2])
+	n4 := NewMerkleNode(nil, nil, data[3])
 
 	// Level 2
 	n5 := NewMerkleNode(n1, n2, nil)
