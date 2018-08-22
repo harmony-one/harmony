2af2c2205357a69d2bd72508b4169c66f49fe1ef
Alok Kothari <alokkothari@Aloks-MacBook-Pro-2.local>
2018-08-21 20:59:37 -0700

    identity chain

---
+++ identitychain/identitychain_test.go
@@ -0,0 +1,16 @@
+package identitychain
+
+import (
+	"testing"
+)
+
+func TestConnAlive(test *testing.T) {
+	// peer := p2p.Peer{Ip: "127.0.0.1", Port: "8080"}
+	// IDC := New(peer)
+	// IDC.listenOnPort()
+	// // conn, err := net.Dial("tcp", IDC.Peer.Ip+":"+IDC.Peer.Port)
+	// // if err != nil {
+	// // 	test.Fatal(err)
+	// // }
+	// // defer conn.Close()
+}
