2688973d56058de9dab8594657c0c917bc9b6ed3
Minh Doan <minh@simple-rules.com>
2018-08-30 16:12:22 -0700

    fix the code format

--- runwait/run_wait.go
+++ runwait/run_wait.go
@@ -8,11 +8,11 @@ package main
 // )
 
 func main() {
-// 	ip := flag.String("ip", "127.0.0.0", "IP of the node")
-// 	port := flag.String("port", "8080", "port of the node")
-// 	flag.Parse()
-// 	peer := p2p.Peer{Ip: *ip, Port: *port}
-// 	idcpeer := p2p.Peer{Ip: "localhost", Port: "9000"} //Hardcoded here.
-// 	node := waitnode.New(peer)
-// 	node.ConnectIdentityChain(idcpeer)
+	// 	ip := flag.String("ip", "127.0.0.0", "IP of the node")
+	// 	port := flag.String("port", "8080", "port of the node")
+	// 	flag.Parse()
+	// 	peer := p2p.Peer{Ip: *ip, Port: *port}
+	// 	idcpeer := p2p.Peer{Ip: "localhost", Port: "9000"} //Hardcoded here.
+	// 	node := waitnode.New(peer)
+	// 	node.ConnectIdentityChain(idcpeer)
 }
