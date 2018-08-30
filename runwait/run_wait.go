4ac29baacf245dcd7aa3a7cf69c073a4fd219522
ak <alok@harmony.one>
2018-08-30 15:50:48 -0700

    unbreaking travis

--- runwait/run_wait.go
+++ runwait/run_wait.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // import (
 // 	"flag"
 
@@ -7,12 +9,13 @@ package main
 // 	"github.com/simple-rules/harmony-benchmark/waitnode"
 // )
 
-// func main() {
-// 	ip := flag.String("ip", "127.0.0.0", "IP of the node")
-// 	port := flag.String("port", "8080", "port of the node")
-// 	flag.Parse()
-// 	peer := p2p.Peer{Ip: *ip, Port: *port}
-// 	idcpeer := p2p.Peer{Ip: "localhost", Port: "9000"} //Hardcoded here.
-// 	node := waitnode.New(peer)
-// 	node.ConnectIdentityChain(idcpeer)
-// }
+func main() {
+	fmt.Println("hello")
+	// 	ip := flag.String("ip", "127.0.0.0", "IP of the node")
+	// 	port := flag.String("port", "8080", "port of the node")
+	// 	flag.Parse()
+	// 	peer := p2p.Peer{Ip: *ip, Port: *port}
+	// 	idcpeer := p2p.Peer{Ip: "localhost", Port: "9000"} //Hardcoded here.
+	// 	node := waitnode.New(peer)
+	// 	node.ConnectIdentityChain(idcpeer)
+}
