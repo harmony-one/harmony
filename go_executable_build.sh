7a575101020b158eac3612ad6a764f558dbd42bb
Leo Chen <leo@harmony.one>
2018-09-01 05:37:44 +0000

    clean up code after removal of aws-experiment-launch directory
    move kill_node.sh to experiment-deploy repo
    
    Signed-off-by: Leo Chen <leo@harmony.one>

--- go_executable_build.sh
+++ go_executable_build.sh
@@ -3,8 +3,6 @@
 GOOS=linux
 GOARCH=amd64
 env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/benchmark benchmark.go
-env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/soldier aws-experiment-launch/experiment/soldier/main.go
-env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/commander aws-experiment-launch/experiment/commander/main.go
 env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/txgen client/txgen/main.go
 
 AWSCLI=aws
@@ -13,7 +11,4 @@ if [ "$1" != "" ]; then
 fi
 
 $AWSCLI s3 cp bin/benchmark s3://unique-bucket-bin/benchmark --acl public-read-write
-$AWSCLI s3 cp bin/soldier s3://unique-bucket-bin/soldier --acl public-read-write
-$AWSCLI s3 cp bin/commander s3://unique-bucket-bin/commander --acl public-read-write
 $AWSCLI s3 cp bin/txgen s3://unique-bucket-bin/txgen --acl public-read-write
-$AWSCLI s3 cp kill_node.sh s3://unique-bucket-bin/kill_node.sh --acl public-read-write
