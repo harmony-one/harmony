ea1035cdeec53c194727af5cd37d59d10156e102
Leo Chen <leo@harmony.one>
2018-09-02 00:53:13 +0000

    enable build/upload profiler
    Signed-off-by: Leo Chen <leo@harmony.one>

--- go_executable_build.sh
+++ go_executable_build.sh
@@ -4,6 +4,7 @@ GOOS=linux
 GOARCH=amd64
 env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/benchmark benchmark.go
 env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/txgen client/txgen/main.go
+env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/profiler profiler/main.go
 
 AWSCLI=aws
 if [ "$1" != "" ]; then
@@ -12,3 +13,4 @@ fi
 
 $AWSCLI s3 cp bin/benchmark s3://unique-bucket-bin/benchmark --acl public-read-write
 $AWSCLI s3 cp bin/txgen s3://unique-bucket-bin/txgen --acl public-read-write
+$AWSCLI s3 cp bin/profiler s3://unique-bucket-bin/profiler --acl public-read-write
