8a709bcf35276b1fa335d7814a1ae7f6281c7983
Leo Chen <leo@harmony.one>
2018-09-04 07:53:23 +0000

    remove reference of profiler binary as it was removed already
    Signed-off-by: Leo Chen <leo@harmony.one>

--- go_executable_build.sh
+++ go_executable_build.sh
@@ -4,7 +4,6 @@ GOOS=linux
 GOARCH=amd64
 env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/benchmark benchmark.go
 env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/txgen client/txgen/main.go
-env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/profiler profiler/main.go
 
 AWSCLI=aws
 if [ "$1" != "" ]; then
@@ -13,4 +12,3 @@ fi
 
 $AWSCLI s3 cp bin/benchmark s3://unique-bucket-bin/benchmark --acl public-read-write
 $AWSCLI s3 cp bin/txgen s3://unique-bucket-bin/txgen --acl public-read-write
-$AWSCLI s3 cp bin/profiler s3://unique-bucket-bin/profiler --acl public-read-write
