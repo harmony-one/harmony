de692464ed23575e3336e1c8f27ea10771658774
Minh Doan <minh@simple-rules.com>
2018-09-13 17:24:50 -0700

    fix go_executable

--- go_executable_build.sh
+++ go_executable_build.sh
@@ -2,12 +2,8 @@
 GOOS=linux
 GOARCH=amd64
 env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/benchmark benchmark.go
-env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/soldier aws-experiment-launch/experiment/soldier/main.go
-env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/commander aws-experiment-launch/experiment/commander/main.go
 env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/txgen client/txgen/main.go
 
 aws s3 cp bin/benchmark s3://unique-bucket-bin/benchmark --acl public-read-write
-aws s3 cp bin/soldier s3://unique-bucket-bin/soldier --acl public-read-write
-aws s3 cp bin/commander s3://unique-bucket-bin/commander --acl public-read-write
 aws s3 cp bin/txgen s3://unique-bucket-bin/txgen --acl public-read-write
 aws s3 cp kill_node.sh s3://unique-bucket-bin/kill_node.sh --acl public-read-write
