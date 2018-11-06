a952efb603f794dea83eb9296a56628d43758b44
Minh Doan <minh@simple-rules.com>
2018-11-05 23:14:53 -0800

    fix lint for  attack package

--- attack/attack.go
+++ attack/attack.go
@@ -9,67 +9,74 @@ import (
 	"github.com/simple-rules/harmony-benchmark/log"
 )
 
+// Constants used for attack model.
 const (
 	DroppingTickDuration    = 2 * time.Second
 	HitRate                 = 10
 	DelayResponseDuration   = 10 * time.Second
-	ConsensusIdThresholdMin = 10
-	ConsensusIdThresholdMax = 100
+	ConsensusIDThresholdMin = 10
+	ConsensusIDThresholdMax = 100
 )
 
-type AttackType byte
+// Type is the type of attack model.
+type Type byte
 
+// Constants of different attack models.
 const (
-	KilledItself AttackType = iota
+	KilledItself Type = iota
 	DelayResponse
 	IncorrectResponse
 )
 
-// AttackModel contains different models of attacking.
-type Attack struct {
+// Model contains different models of attacking.
+type Model struct {
 	AttackEnabled             bool
-	attackType                AttackType
-	ConsensusIdThreshold      uint32
+	attackType                Type
+	ConsensusIDThreshold      uint32
 	readyByConsensusThreshold bool
 	log                       log.Logger // Log utility
 }
 
-var attack *Attack
+var attackModel *Model
 var once sync.Once
 
 // GetInstance returns attack model by using singleton pattern.
-func GetInstance() *Attack {
+func GetInstance() *Model {
 	once.Do(func() {
-		attack = &Attack{}
-		attack.Init()
+		attackModel = &Model{}
+		attackModel.Init()
 	})
-	return attack
+	return attackModel
 }
 
-func (attack *Attack) Init() {
+// Init initializes attack model.
+func (attack *Model) Init() {
 	attack.AttackEnabled = false
 	attack.readyByConsensusThreshold = false
 }
 
-func (attack *Attack) SetAttackEnabled(AttackEnabled bool) {
+// SetAttackEnabled sets attack model enabled.
+func (attack *Model) SetAttackEnabled(AttackEnabled bool) {
 	attack.AttackEnabled = AttackEnabled
 	if AttackEnabled {
-		attack.attackType = AttackType(rand.Intn(3))
-		attack.ConsensusIdThreshold = uint32(ConsensusIdThresholdMin + rand.Intn(ConsensusIdThresholdMax-ConsensusIdThresholdMin))
+		attack.attackType = Type(rand.Intn(3))
+		attack.ConsensusIDThreshold = uint32(ConsensusIDThresholdMin + rand.Intn(ConsensusIDThresholdMax-ConsensusIDThresholdMin))
 	}
 }
 
-func (attack *Attack) SetLogger(log log.Logger) {
+// SetLogger sets the logger for doing logging.
+func (attack *Model) SetLogger(log log.Logger) {
 	attack.log = log
 }
 
-func (attack *Attack) Run() {
+// Run runs enabled attacks.
+func (attack *Model) Run() {
 	attack.NodeKilledByItSelf()
 	attack.DelayResponse()
 }
 
 // NodeKilledByItSelf runs killing itself attack
-func (attack *Attack) NodeKilledByItSelf() {
+func (attack *Model) NodeKilledByItSelf() {
 	if !attack.AttackEnabled || attack.attackType != KilledItself || !attack.readyByConsensusThreshold {
 		return
 	}
@@ -80,29 +87,32 @@ func (attack *Attack) NodeKilledByItSelf() {
 	}
 }
 
-func (attack *Attack) DelayResponse() {
+// DelayResponse does attack by delaying response.
+func (attack *Model) DelayResponse() {
 	if !attack.AttackEnabled || attack.attackType != DelayResponse || !attack.readyByConsensusThreshold {
 		return
 	}
 	if rand.Intn(HitRate) == 0 {
-		attack.log.Debug("******************Attack: DelayResponse******************", "PID: ", os.Getpid())
+		attack.log.Debug("******************Model: DelayResponse******************", "PID: ", os.Getpid())
 		time.Sleep(DelayResponseDuration)
 	}
 }
 
-func (attack *Attack) IncorrectResponse() bool {
+// IncorrectResponse returns if the attack model enable incorrect responding.
+func (attack *Model) IncorrectResponse() bool {
 	if !attack.AttackEnabled || attack.attackType != IncorrectResponse || !attack.readyByConsensusThreshold {
 		return false
 	}
 	if rand.Intn(HitRate) == 0 {
-		attack.log.Debug("******************Attack: IncorrectResponse******************", "PID: ", os.Getpid())
+		attack.log.Debug("******************Model: IncorrectResponse******************", "PID: ", os.Getpid())
 		return true
 	}
 	return false
 }
 
-func (attack *Attack) UpdateConsensusReady(consensusID uint32) {
-	if consensusID > attack.ConsensusIdThreshold {
+// UpdateConsensusReady enables an attack type given the current consensusID.
+func (attack *Model) UpdateConsensusReady(consensusID uint32) {
+	if consensusID > attack.ConsensusIDThreshold {
 		attack.readyByConsensusThreshold = true
 	}
 }
