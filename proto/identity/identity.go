d46de70d91432620ee674200770dd3b129f6d84e
Leo Chen <leo@harmony.one>
2018-11-17 19:11:37 -0800

    add init version of messages in IDC
    the messages exchanged between IDC and new node during bootstrap
    
    Signed-off-by: Leo Chen <leo@harmony.one>

--- proto/identity/identity.go
+++ proto/identity/identity.go
@@ -7,6 +7,55 @@ import (
 	"github.com/harmony-one/harmony/proto"
 )
 
+// TODO: the message structure should be defined in a structure
+//       and use reflect to serialize/deseralize it, instead of
+//       manual serialization/deserialization functions
+
+/*
+Identity message is the payload of message between nodes and IDC
+Identity message data structure:
+
+Register:
+---- message start -----
+1 byte            - identity.MessageType
+                    0x00 - Register
+                    0x01 - Acknowledge
+                    0x02 - Peers
+                    ...
+4 byte            - protocol version
+32 byte           - pubKey
+4 byte            - myIP
+4 byte            - myPort
+(n bytes)         - transaction data for PoS
+----  message end  -----
+
+Acknowledge:
+---- message start -----
+1 byte            - identity.MessageType
+                    0x00 - Register
+                    0x01 - Acknowledge
+                    0x02 - Peers
+                    ...
+4 byte            - protocol version
+----  message end  -----
+
+Peer:
+---- message start -----
+1 byte            - identity.MessageType
+                    0x00 - Register
+                    0x01 - Acknowledge
+                    0x02 - Peers
+                    ...
+4 byte            - number of shards
+32 byte           - randomness
+(n bytes)         - leader info list
+* 32 byte         - leader pubKey
+* 4 byte          - leader IP
+64 byte           - signature
+----  message end  -----
+
+*/
+
 // IdentityMessageTypeBytes is the number of bytes consensus message type occupies
 const IdentityMessageTypeBytes = 1
 
