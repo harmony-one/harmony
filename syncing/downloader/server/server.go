87eed384ceb8a181bad1abb0a7d09a4e2a50f734
Minh Doan <minh@simple-rules.com>
2018-11-25 13:14:23 -0800

    convert working client/server into apis

--- syncing/downloader/server/server.go
+++ syncing/downloader/server/server.go
@@ -2,36 +2,51 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
 
+	"github.com/harmony-one/harmony/node"
 	pb "github.com/harmony-one/harmony/syncing/downloader/proto"
 )
 
-type downloaderServer struct {
+// Server ...
+type Server struct {
+	node *node.Node
 }
 
-// GetFeature returns the feature at the given point.
-func (s *downloaderServer) Query(ctx context.Context, request *pb.DownloaderRequest) (*pb.DownloaderResponse, error) {
+// Query returns the feature at the given point.
+func (s *Server) Query(ctx context.Context, request *pb.DownloaderRequest) (*pb.DownloaderResponse, error) {
 	response := pb.DownloaderResponse{}
 	response.Payload = [][]byte{{0, 0, 2}}
 	return &response, nil
 }
 
-func newServer() *downloaderServer {
-	s := &downloaderServer{}
-	return s
-}
-
-func main() {
-	lis, err := net.Listen("tcp", "localhost:9999")
+// Start ...
+func (s *Server) Start(ip, port string) error {
+	// if s.node == nil {
+	// 	return ErrDownloaderWithNoNode
+	// }
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%s", ip, port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
-	pb.RegisterDownloaderServer(grpcServer, newServer())
+	pb.RegisterDownloaderServer(grpcServer, s)
 	grpcServer.Serve(lis)
+	return nil
+}
+
+// NewServer ...
+func NewServer(node *node.Node) *Server {
+	s := &Server{node: node}
+	return s
+}
+
+func main() {
+	s := NewServer(nil)
+	s.Start("localhost", "9999")
 }
