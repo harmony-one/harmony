31e4e43d414d65cc69fc30de6981a05c9b72f3c1
ak <alok@harmony.one>
2018-11-30 18:25:19 -0800

    beacon chain communication and running

---
+++ bcconn/bcconn.go
@@ -0,0 +1,67 @@
+package bcconn
+
+import (
+	"bytes"
+	"encoding/gob"
+
+	"github.com/harmony-one/harmony/log"
+	"github.com/harmony-one/harmony/p2p"
+)
+
+type NodeInfo struct { //TODO: to be merged with Leo's key.
+	Self p2p.Peer
+	PubK []byte
+}
+type ResponseRandomNumber struct {
+	NumberOfShards     int
+	NumberOfNodesAdded int
+	Leaders            []*NodeInfo
+}
+
+//SerializeNodeInfo
+func SerializeNodeInfo(nodeinfo *NodeInfo) []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(nodeinfo)
+	if err != nil {
+		log.Error("Could not serialize node info", err)
+	}
+	return result.Bytes()
+}
+
+// DeserializeNodeInfo deserializes the node
+func DeserializeNodeInfo(d []byte) *NodeInfo {
+	var wn NodeInfo
+	r := bytes.NewBuffer(d)
+	decoder := gob.NewDecoder(r)
+	err := decoder.Decode(&wn)
+	if err != nil {
+		log.Error("Could not de-serialize node info", err)
+	}
+	return &wn
+}
+
+//SerializeRandomInfo
+func SerializeRandomInfo(response ResponseRandomNumber) []byte {
+	//Needs to escape the serialization of unexported fields
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(response)
+	if err != nil {
+		log.Crit("Could not serialize randomn number information", "error", err)
+	}
+
+	return result.Bytes()
+}
+
+// DeserializeRandomInfo deserializes the node
+func DeserializeRandomInfo(d []byte) ResponseRandomNumber {
+	var wn ResponseRandomNumber
+	r := bytes.NewBuffer(d)
+	decoder := gob.NewDecoder(r)
+	err := decoder.Decode(&wn)
+	if err != nil {
+		log.Crit("Could not de-serialize random number information")
+	}
+	return wn
+}
---
+++ runbeacon/run-beacon.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/harmony-one/harmony/beaconchain"
+)
+
+func main() {
+	numShards := flag.Int("numShards", 1, "number of shards of identity chain")
+	ip := flag.String("ip", "127.0.0.1", "ip on which beaconchain listens")
+	port := flag.String("port", "8081", "port on which beaconchain listens")
+	flag.Parse()
+	bc := beaconchain.New(*numShards, *ip, *port)
+	bc.StartServer()
+}
