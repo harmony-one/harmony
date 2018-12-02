876c2fc7b56d6e0d44c46e274ff9f7f378422043
Leo Chen <leo@harmony.one>
2018-12-01 20:00:14 -0800

    fix deploy.sh to support original model
    to run peer discovery mode, please use
    
    ./deploy.sh -p <config_file>
    
    Signed-off-by: Leo Chen <leo@harmony.one>

--- deploy.sh
+++ deploy.sh
@@ -73,8 +73,12 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
   IFS=' ' read ip port mode shardID <<< $line
 	#echo $ip $port $mode
   if [ "$mode" != "client" ]; then
-    ./bin/benchmark -ip $ip -port $port -log_folder $log_folder $DB $PEER -min_peers $MIN &
-    sleep 10
+    if [ -z "$PEER" ]; then
+      ./bin/benchmark -ip $ip -port $port -config_file $config -log_folder $log_folder $DB -min_peers $MIN &
+    else
+      ./bin/benchmark -ip $ip -port $port -log_folder $log_folder $DB $PEER -min_peers $MIN &
+      sleep 5
+    fi
   fi
 done < $config
 
