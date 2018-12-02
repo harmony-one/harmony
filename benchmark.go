8ba071608931d283dbee99d78d8c4d46527d75c3
Richard Liu <liuzhenglaichn@gmail.com>
2018-12-01 23:08:54 -0800

    comment out fmt.Println

--- benchmark.go
+++ benchmark.go
@@ -141,7 +141,7 @@ func main() {
 		clientPeer = distributionConfig.GetClientPeer()
 	}
 
-	fmt.Println(peers, leader, selfPeer, clientPeer, *logFolder, *minPeers) //TODO: to be replaced by a logger later: ak, rl
+	// fmt.Println(peers, leader, selfPeer, clientPeer, *logFolder, *minPeers) //TODO: to be replaced by a logger later: ak, rl
 
 	var role string
 	if leader.IP == *ip && leader.Port == *port {
