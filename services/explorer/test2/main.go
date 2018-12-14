912b51ea6f7793e404dddc0bbdfc5ce6e5fcd5bc
Minh Doan <minh@simple-rules.com>
2018-12-13 23:24:28 -0800

    remove breaking param
    set up leveldb to store explorer info
    
    add storage
    
    adding dumping new block logic into db
    
    add fake data and structs for fake data
    
    explorer backend service with fake data
    
    clean something up
    
    set up leveldb to store explorer info
    
    add logic for blockinfo & refactor code

--- services/explorer/test2/main.go
+++ services/explorer/test2/main.go
@@ -3,7 +3,10 @@ package main
 import "github.com/harmony-one/harmony/services/explorer"
 
 func main() {
-	service := &explorer.Service{}
+	service := &explorer.Service{
+		IP:   "127.0.0.1",
+		Port: "9000",
+	}
 	service.Init()
 	service.Run()
 }
