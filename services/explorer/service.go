85499475f79982646b1ca81cb29f012b9a144f82
Minh Doan <minh@simple-rules.com>
2018-12-14 18:47:04 -0800

    add tx and blocks end points

--- services/explorer/service.go
+++ services/explorer/service.go
@@ -8,9 +8,11 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/gorilla/mux"
+	"github.com/harmony-one/harmony/core/types"
 )
 
 // Constants for explorer service.
@@ -38,50 +40,52 @@ func (s *Service) Run() {
 
 	// Set up router
 	s.router = mux.NewRouter()
-	s.router.Path("/block_info").Queries("from", "{[0-9]*?}", "to", "{[0-9]*?}").HandlerFunc(s.GetExplorerBlockInfo).Methods("GET")
-	s.router.Path("/block_info").HandlerFunc(s.GetExplorerBlockInfo)
+	// s.router.Path("/block_info").Queries("from", "{[0-9]*?}", "to", "{[0-9]*?}").HandlerFunc(s.GetExplorerBlockInfo).Methods("GET")
+	// s.router.Path("/block_info").HandlerFunc(s.GetExplorerBlockInfo)
 
-	s.router.HandleFunc("/block", s.GetExplorerBlock).Methods("GET")
-	s.router.HandleFunc("/address", s.GetExplorerAddress).Methods("GET")
-	s.router.HandleFunc("/tx", s.GetExplorerTransaction).Methods("GET")
+	s.router.Path("/blocks").Queries("from", "{[0-9]*?}", "to", "{[0-9]*?}").HandlerFunc(s.GetExplorerBlocks).Methods("GET")
+	s.router.Path("/blocks").HandlerFunc(s.GetExplorerBlocks)
+
+	s.router.Path("/tx").Queries("id", "{[0-9A-F]*?}").HandlerFunc(s.GetExplorerTransaction).Methods("GET")
+	s.router.Path("/tx").HandlerFunc(s.GetExplorerTransaction)
 	// Do serving now.
 	fmt.Println("Listening to:", ExplorerServicePort)
 	go log.Fatal(http.ListenAndServe(addr, s.router))
 }
 
 // GetExplorerBlockInfo ...
-func (s *Service) GetExplorerBlockInfo(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	from := r.FormValue("from")
-	to := r.FormValue("to")
+// func (s *Service) GetExplorerBlockInfo(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	from := r.FormValue("from")
+// 	to := r.FormValue("to")
 
-	data := &Data{
-		Blocks: []*BlockInfo{},
-	}
-	if from == "" {
-		json.NewEncoder(w).Encode(data.Blocks)
-		return
-	}
-	db := s.storage.GetDB()
-	fromInt, err := strconv.Atoi(from)
-	var toInt int
-	if to == "" {
-		bytes, err := db.Get([]byte(BlockHeightKey))
-		if err == nil {
-			toInt, err = strconv.Atoi(string(bytes))
-		}
-	} else {
-		toInt, err = strconv.Atoi(to)
-	}
-	fmt.Println("from", fromInt, "to", toInt)
-	if err != nil {
-		json.NewEncoder(w).Encode(data.Blocks)
-		return
-	}
+// 	data := &Data{
+// 		Blocks: []*BlockInfo{},
+// 	}
+// 	if from == "" {
+// 		json.NewEncoder(w).Encode(data.Blocks)
+// 		return
+// 	}
+// 	db := s.storage.GetDB()
+// 	fromInt, err := strconv.Atoi(from)
+// 	var toInt int
+// 	if to == "" {
+// 		bytes, err := db.Get([]byte(BlockHeightKey))
+// 		if err == nil {
+// 			toInt, err = strconv.Atoi(string(bytes))
+// 		}
+// 	} else {
+// 		toInt, err = strconv.Atoi(to)
+// 	}
+// 	fmt.Println("from", fromInt, "to", toInt)
+// 	if err != nil {
+// 		json.NewEncoder(w).Encode(data.Blocks)
+// 		return
+// 	}
 
-	data.Blocks = s.PopulateBlockInfo(fromInt, toInt)
-	json.NewEncoder(w).Encode(data.Blocks)
-}
+// 	data.Blocks = s.PopulateBlockInfo(fromInt, toInt)
+// 	json.NewEncoder(w).Encode(data.Blocks)
+// }
 
 // PopulateBlockInfo ...
 func (s *Service) PopulateBlockInfo(from, to int) []*BlockInfo {
@@ -105,35 +109,147 @@ func (s *Service) PopulateBlockInfo(from, to int) []*BlockInfo {
 	return blocks
 }
 
-// GetExplorerBlock ...
-func (s *Service) GetExplorerBlock(w http.ResponseWriter, r *http.Request) {
-	// w.Header().Set("Content-Type", "application/json")
-	// id := r.FormValue("id")
-
-	// data := &Data{}
-	// db := s.storage.GetDB()
-
-	// if id == "" {
-	// 	json.NewEncoder(w).Encode(data.Block)
-	// 	return
-	// }
-	// idInt, err := strconv.Atoi(id)
-	// if err != nil {
-	// 	json.NewEncoder(w).Encode(data.Block)
-	// 	return
-	// }
-	// data, err := db.Get([]byte(GetBlockKey(idInt)))
-
-	// data.Block = storage.GetDB()
-	// json.NewEncoder(w).Encode(data.Blocks)
+// GetAccountBlocks ...
+func (s *Service) GetAccountBlocks(from, to int) []*types.Block {
+	blocks := []*types.Block{}
+	for i := from - 1; i <= to+1; i++ {
+		if i < 0 {
+			blocks = append(blocks, nil)
+			continue
+		}
+		key := GetBlockKey(i)
+		fmt.Println("getting blockinfo with key ", key)
+		data, err := storage.db.Get([]byte(key))
+		if err != nil {
+			if i > to {
+				blocks = append(blocks, nil)
+				continue
+			}
+			fmt.Println("Error on getting from db")
+			os.Exit(1)
+		}
+		block := new(types.Block)
+		if rlp.DecodeBytes(data, block) != nil {
+			fmt.Println("RLP Decoding error")
+			os.Exit(1)
+		}
+		blocks = append(blocks, block)
+		fmt.Println("rlp decode successfully ")
+	}
+	return blocks
 }
 
-// GetExplorerAddress ...
-func (s *Service) GetExplorerAddress(w http.ResponseWriter, r *http.Request) {
-	// json.NewEncoder(w).Encode(s.data.Address)
+// GetTime ...
+func GetTime(timestamp int64) string {
+	return time.Unix(timestamp, 0).String()
+}
+
+// GetTransaction ...
+func GetTransaction(tx *types.Transaction) Transaction {
+	return Transaction{
+		ID: tx.Hash().Hex(),
+		// Timestamp: GetTime(accountBlock.Time().Int64()),
+		Timestamp: GetTime(3422343),
+		From:      tx.To().Hex(),
+		To:        tx.To().Hex(),
+		Value:     tx.GasPrice().Int64(), // TODO(minh): fix it
+	}
+}
+
+// GetExplorerBlocks ...
+func (s *Service) GetExplorerBlocks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	from := r.FormValue("from")
+	to := r.FormValue("to")
+
+	data := &Data{
+		Blocks: []*Block{},
+	}
+	if from == "" {
+		json.NewEncoder(w).Encode(data.Blocks)
+		return
+	}
+	db := s.storage.GetDB()
+	fromInt, err := strconv.Atoi(from)
+	var toInt int
+	if to == "" {
+		bytes, err := db.Get([]byte(BlockHeightKey))
+		if err == nil {
+			toInt, err = strconv.Atoi(string(bytes))
+		}
+	} else {
+		toInt, err = strconv.Atoi(to)
+	}
+	fmt.Println("from", fromInt, "to", toInt)
+	if err != nil {
+		json.NewEncoder(w).Encode(data.Blocks)
+		return
+	}
+
+	accountBlocks := s.GetAccountBlocks(fromInt, toInt)
+	for id, accountBlock := range accountBlocks {
+		if id == 0 || id == len(accountBlocks)-1 {
+			continue
+		}
+		block := &Block{
+			Height:     string(id + fromInt - 1),
+			Hash:       accountBlock.Hash().Hex(),
+			TXCount:    string(accountBlock.Transactions().Len()),
+			Timestamp:  GetTime(accountBlock.Time().Int64()),
+			MerkleRoot: accountBlock.Hash().Hex(),
+		}
+		// Populate transactions
+		for _, tx := range accountBlock.Transactions() {
+			block.TXs = append(block.TXs, GetTransaction(tx))
+		}
+		if accountBlocks[id-1] == nil {
+			block.PrevBlock = RefBlock{
+				ID:     "",
+				Height: "",
+			}
+		} else {
+			block.PrevBlock = RefBlock{
+				ID:     accountBlocks[id-1].Hash().Hex(),
+				Height: string(id + fromInt - 2),
+			}
+		}
+		if accountBlocks[id+1] == nil {
+			block.NextBlock = RefBlock{
+				ID:     "",
+				Height: "",
+			}
+		} else {
+			block.NextBlock = RefBlock{
+				ID:     accountBlocks[id+1].Hash().Hex(),
+				Height: string(id + fromInt),
+			}
+		}
+		data.Blocks = append(data.Blocks, block)
+	}
+	json.NewEncoder(w).Encode(data.Blocks)
 }
 
 // GetExplorerTransaction ...
 func (s *Service) GetExplorerTransaction(w http.ResponseWriter, r *http.Request) {
-	// json.NewEncoder(w).Encode(s.data.Address)
+	w.Header().Set("Content-Type", "application/json")
+	id := r.FormValue("id")
+
+	data := &Data{}
+	if id == "" {
+		json.NewEncoder(w).Encode(data.TX)
+		return
+	}
+	db := s.storage.GetDB()
+	bytes, err := db.Get([]byte(GetTXKey(id)))
+	if err != nil {
+		fmt.Println("Error on getting from db")
+		os.Exit(1)
+	}
+	tx := new(types.Transaction)
+	if rlp.DecodeBytes(bytes, tx) != nil {
+		fmt.Println("RLP Decoding error")
+		os.Exit(1)
+	}
+	data.TX = GetTransaction(tx)
+	json.NewEncoder(w).Encode(data.TX)
 }
