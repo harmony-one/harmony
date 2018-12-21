2e48f66a575b55dcfef570af12b11101b06ccb12
Rongjian Lan <rongjian.lan@gmail.com>
2018-12-21 11:11:45 -0800

    Fix build error by removing legacy reference

--- client/client.go
+++ client/client.go
@@ -2,9 +2,6 @@ package client
 
 import (
 	"github.com/harmony-one/harmony/core/types"
-	"github.com/simple-rules/harmony-benchmark/blockchain"
-	"sync"
-
 	"github.com/harmony-one/harmony/log"
 	"github.com/harmony-one/harmony/p2p"
 	"github.com/harmony-one/harmony/p2p/host"
@@ -15,9 +12,7 @@ type Client struct {
 	Leaders      *map[uint32]p2p.Peer // Map of shard Id and corresponding leader
 	UpdateBlocks func([]*types.Block) // Closure function used to sync new block with the leader. Once the leader finishes the consensus on a new block, it will send it to the clients. Clients use this method to update their blockchain
 
-	ShardUtxoMap      map[uint32]blockchain.UtxoMap
-	ShardUtxoMapMutex sync.Mutex // Mutex for the UTXO maps
-	log               log.Logger // Log utility
+	log log.Logger // Log utility
 
 	// The p2p host used to send/receive p2p messages
 	host host.Host
@@ -31,37 +26,6 @@ func NewClient(host host.Host, leaders *map[uint32]p2p.Peer) *Client {
 	// Logger
 	client.log = log.New()
 	return &client
-}
-
-// BuildOutputShardTransactionMap builds output shard transaction map.
-func BuildOutputShardTransactionMap(txs []*blockchain.Transaction) map[uint32][]*blockchain.Transaction {
-	txsShardMap := make(map[uint32][]*blockchain.Transaction)
-
-	// Put txs into corresponding output shards
-	for _, crossTx := range txs {
-		for curShardID := range GetOutputShardIDsOfCrossShardTx(crossTx) {
-			txsShardMap[curShardID] = append(txsShardMap[curShardID], crossTx)
-		}
-	}
-	return txsShardMap
-}
-
-// GetInputShardIDsOfCrossShardTx gets input shardID.
-func GetInputShardIDsOfCrossShardTx(crossTx *blockchain.Transaction) map[uint32]bool {
-	shardIDs := map[uint32]bool{}
-	for _, txInput := range crossTx.TxInput {
-		shardIDs[txInput.ShardID] = true
-	}
-	return shardIDs
-}
-
-// GetOutputShardIDsOfCrossShardTx gets output shard ids.
-func GetOutputShardIDsOfCrossShardTx(crossTx *blockchain.Transaction) map[uint32]bool {
-	shardIDs := map[uint32]bool{}
-	for _, txOutput := range crossTx.TxOutput {
-		shardIDs[txOutput.ShardID] = true
-	}
-	return shardIDs
 }
 
 // GetLeaders returns leader peers.
