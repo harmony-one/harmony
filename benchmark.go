466309c26622ba9988808c1bae4e84fed02a3c5d
Leo Chen <leo@harmony.one>
2018-12-21 18:54:05 +0000

    enable peer discovery by default
    Signed-off-by: Leo Chen <leo@harmony.one>

--- benchmark.go
+++ benchmark.go
@@ -91,7 +91,7 @@ func main() {
 	//This IP belongs to jenkins.harmony.one
 	bcIP := flag.String("bc", "127.0.0.1", "IP of the identity chain")
 	bcPort := flag.String("bc_port", "8081", "port of the identity chain")
-	peerDiscovery := flag.Bool("peer_discovery", false, "Enable Peer Discovery")
+	peerDiscovery := flag.Bool("peer_discovery", true, "Enable Peer Discovery")
 
 	//Leader needs to have a minimal number of peers to start consensus
 	minPeers := flag.Int("min_peers", 100, "Minimal number of Peers in shard")
