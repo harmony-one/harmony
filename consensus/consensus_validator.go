87bbe5b6ff3790e256270051df7d0b02e9e0e8ab
Minh Doan <minh@simple-rules.com>
2018-12-31 17:00:03 -0800

    move attack under internal

--- consensus/consensus_validator.go
+++ consensus/consensus_validator.go
@@ -2,12 +2,13 @@ package consensus
 
 import (
 	"bytes"
+
 	"github.com/dedis/kyber/sign/schnorr"
 	"github.com/ethereum/go-ethereum/rlp"
-	"github.com/harmony-one/harmony/attack"
 	consensus2 "github.com/harmony-one/harmony/consensus/proto"
 	"github.com/harmony-one/harmony/core/types"
 	"github.com/harmony-one/harmony/crypto"
+	"github.com/harmony-one/harmony/internal/attack"
 	"github.com/harmony-one/harmony/log"
 	proto_consensus "github.com/harmony-one/harmony/proto/consensus"
 	"github.com/harmony-one/harmony/utils"
