f3825e5537afaf848b82f26c0495d70f7cf94b50
Leo Chen <leo@harmony.one>
2019-01-03 04:42:36 +0000

    move client/wallet to cmd/client/wallet
    Signed-off-by: Leo Chen <leo@harmony.one>

--- go_executable_build.sh
+++ go_executable_build.sh
@@ -3,7 +3,7 @@ declare -A SRC
 SRC[benchmark]=benchmark.go
 SRC[txgen]=cmd/client/txgen/main.go
 SRC[beacon]=cmd/beaconchain/main.go
-SRC[wallet]=client/wallet/main.go
+SRC[wallet]=cmd/client/wallet/main.go
 
 BINDIR=bin
 BUCKET=unique-bucket-bin
