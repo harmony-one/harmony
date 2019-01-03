b5f2c780ab12aaa21c90668b8d6ba40cbe926c88
Minh Doan <minh@simple-rules.com>
2019-01-03 10:58:36 -0800

    test for attack

---
+++ internal/attack/attack_test.go
@@ -0,0 +1,28 @@
+package attack
+
+import (
+	"testing"
+
+	"github.com/harmony-one/harmony/log"
+	"github.com/stretchr/testify/assert"
+)
+
+// Simple test for IncorrectResponse
+func TestIncorrectResponse(t *testing.T) {
+	GetInstance().SetAttackEnabled(false)
+	assert.False(t, GetInstance().IncorrectResponse(), "error")
+	GetInstance().SetAttackEnabled(true)
+}
+
+// Simple test for UpdateConsensusReady
+func TestUpdateConsensusReady(t *testing.T) {
+	model := GetInstance()
+	model.SetLogger(log.New())
+	model.NodeKilledByItSelf()
+
+	model.UpdateConsensusReady(model.ConsensusIDThreshold - 1)
+	model.DelayResponse()
+
+	model.UpdateConsensusReady(model.ConsensusIDThreshold + 1)
+	model.DelayResponse()
+}
