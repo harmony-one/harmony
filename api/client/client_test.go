16cb1711b051dfc0b95b1d5e4233047a5b636970
Leo Chen <leo@harmony.one>
2019-01-03 08:21:04 +0000

    add test for api/client
    Signed-off-by: Leo Chen <leo@harmony.one>

---
+++ api/client/client_test.go
@@ -0,0 +1,24 @@
+package client
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/harmony-one/harmony/p2p"
+)
+
+func TestClient(t *testing.T) {
+	leaders := map[uint32]p2p.Peer{
+		0: p2p.Peer{
+			IP:   "127.0.0.1",
+			Port: "90",
+		},
+	}
+	client := NewClient(nil, &leaders)
+
+	leadersGot := client.GetLeaders()
+
+	if len(leadersGot) != 1 || !reflect.DeepEqual(leaders[0], leadersGot[0]) {
+		t.Errorf("expected: %v, got: %v\n", leaders[0], leadersGot[0])
+	}
+}
