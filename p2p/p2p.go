fdcce8e4deb777d1f171dbb36da260ee5eba5ac1
Rongjian Lan <rongjian.lan@gmail.com>
2019-01-03 18:09:07 -0800

    Add tests for node_handler.go; move stream interface to its own file;create mock for stream.go

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,18 +1,8 @@
 package p2p
 
 import (
-	"time"
-
 	"github.com/dedis/kyber"
 )
-
-// Stream is abstract p2p stream from where we read message
-type Stream interface {
-	Read([]byte) (int, error)
-	Write([]byte) (int, error)
-	Close() error
-	SetReadDeadline(time.Time) error
-}
 
 // StreamHandler handles incoming p2p message.
 type StreamHandler func(Stream)
