d0247f0bb748aea0bc7a4e51b2ee814f5586fe47
Minh Doan <minh@simple-rules.com>
2019-01-08 15:31:03 -0800

    add singleton for logging

--- internal/utils/singleton.go
+++ internal/utils/singleton.go
@@ -5,6 +5,8 @@ package utils
 import (
 	"sync"
 	"sync/atomic"
+
+	"github.com/harmony-one/harmony/log"
 )
 
 // UniqueValidatorID defines the structure of unique validator ID
@@ -13,7 +15,9 @@ type UniqueValidatorID struct {
 }
 
 var instance *UniqueValidatorID
+var logInstance log.Logger
 var once sync.Once
+var onceForLog sync.Once
 
 // GetUniqueValidatorIDInstance returns a singleton instance
 func GetUniqueValidatorIDInstance() *UniqueValidatorID {
@@ -28,4 +32,11 @@ func GetUniqueValidatorIDInstance() *UniqueValidatorID {
 // GetUniqueID returns a unique ID and increment the internal variable
 func (s *UniqueValidatorID) GetUniqueID() uint32 {
 	return atomic.AddUint32(&s.uniqueID, 1)
+}
+
+func GetLogInstance() log.Logger {
+	onceForLog.Do(func() {
+		logInstance = log.New()
+	})
+	return logInstance
 }
