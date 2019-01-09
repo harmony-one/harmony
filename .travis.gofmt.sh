dce14e51f4b98ab174a28305d06fd27d0ad54012
Minh Doan <minh@simple-rules.com>
2019-01-08 23:35:02 -0800

    run consensus check at travis

--- .travis.gofmt.sh
+++ .travis.gofmt.sh
@@ -5,6 +5,10 @@ if [ $(golint ./... | wc | awk '{print $1}') -gt 2 ]; then
     gofmt -d .
     exit 1
 fi
+if [ $(./test/deploy.sh ./test/configs/local_config1.txt 2>&1  | grep "HOORAY" | wc | awk '{print $1}') -lt 10 ]; then
+    echo "The code did not produce enough consensus."
+    exit 1
+fi
 if [ -n "$(gofmt -l .)" ]; then
     echo "Go code is not formatted:"
     gofmt -d .
