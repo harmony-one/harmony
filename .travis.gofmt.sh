fa832e09fdf1ad84c540129fc37105b522668057
Minh Doan <minh@simple-rules.com>
2019-01-08 23:47:28 -0800

    add comments

--- .travis.gofmt.sh
+++ .travis.gofmt.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Check golint.
 if [ $(golint ./... | wc | awk '{print $1}') -gt 2 ]; then
     echo "Go code is not formatted:"
     gofmt -d .
     exit 1
 fi
+# Run deploy.sh and count how many times HOORAY appearing in the output. If it does not produce enough the submission may cause the consensus.
 if [ $(./test/deploy.sh ./test/configs/local_config1.txt 2>&1  | grep "HOORAY" | wc | awk '{print $1}') -lt 10 ]; then
     echo "The code did not produce enough consensus."
     exit 1
 fi
+# Run kill nodes in case deploy.sh got broken and did not kill all nodes.
+./test/kill_node.sh
+# Check gofmt.
 if [ -n "$(gofmt -l .)" ]; then
     echo "Go code is not formatted:"
     gofmt -d .
