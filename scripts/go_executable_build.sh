8e14357e4499736e8bbeebfa995c2c9fa4645c12
Rongjian Lan <rongjian.lan@gmail.com>
2019-01-24 20:30:31 -0800

    Use GOPATH instead of PWD

--- scripts/go_executable_build.sh
+++ scripts/go_executable_build.sh
@@ -13,16 +13,17 @@ GOARCH=amd64
 FOLDER=/${WHOAMI:-$USER}
 RACE=
 
-export CGO_CFLAGS="-I$PWD/../bls/include -I$PWD/../mcl/include"
-export CGO_LDFLAGS="-L$PWD/../bls/lib"
-export LD_LIBRARY_PATH=$PWD/../bls/lib:$PWD/../mcl/lib
+HMY_PATH=$GOPATH/src/github.com/harmony-one
+export CGO_CFLAGS="-I$HMY_PATH/bls/include -I$HMY_PATH/mcl/include"
+export CGO_LDFLAGS="-L$HMY_PATH/bls/lib"
+export LD_LIBRARY_PATH=$HMY_PATH/bls/lib:$HMY_PATH/mcl/lib
 
 OS=$(uname -s)
 case $OS in
    Darwin)
-      export CGO_CFLAGS="-I$PWD/../bls/include -I$PWD/../mcl/include -I/usr/local/opt/openssl/include"
-      export CGO_LDFLAGS="-L$PWD/../bls/lib -L/usr/local/opt/openssl/lib"
-      export LD_LIBRARY_PATH=$PWD/../bls/lib:$PWD/../mcl/lib:/usr/local/opt/openssl/lib
+      export CGO_CFLAGS="-I$HMY_PATH/bls/include -I$HMY_PATH/mcl/include -I/usr/local/opt/openssl/include"
+      export CGO_LDFLAGS="-L$HMY_PATH/bls/lib -L/usr/local/opt/openssl/lib"
+      export LD_LIBRARY_PATH=$HMY_PATH/bls/lib:$HMY_PATH/mcl/lib:/usr/local/opt/openssl/lib
       export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
       ;;
 esac
