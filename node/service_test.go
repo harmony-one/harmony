c1d97e499273055b6b330527bd89840da4d2412d
Minh Doan <minh@harmony.one>
2019-01-25 12:10:11 -0800

    add service register

--- node/service_test.go
+++ node/service_test.go
@@ -1,18 +1,30 @@
 package node
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
+type SupportSyncingTest struct{}
+
+func (s *SupportSyncingTest) Start() {
+	fmt.Println("SupportSyncingTest starting")
+}
+
+func (s *SupportSyncingTest) Stop() {
+	fmt.Println("SupportSyncingTest stopping")
+}
+
 func TestTakeAction(t *testing.T) {
 	node := &Node{}
 	node.Start()
+	node.RegisterService(SupportSyncing, &SupportSyncingTest{})
 
 	for i := 0; i < 2; i++ {
 		select {
 		case <-time.After(WaitForStatusUpdate):
-			node.SendAction(&Action{action: Start, serviceType: SyncingSupport})
+			node.SendAction(&Action{action: Start, serviceType: SupportSyncing})
 		}
 	}
 
