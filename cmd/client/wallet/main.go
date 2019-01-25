c56c85a7e7e28400e14f9c8b70255d974cf20a2b
Leo Chen <leo@harmony.one>
2019-01-25 00:10:26 +0000

    add peer of leader to wallet node
    Signed-off-by: Leo Chen <leo@harmony.one>

--- cmd/client/wallet/main.go
+++ cmd/client/wallet/main.go
@@ -295,18 +295,24 @@ func CreateWalletNode() *node.Node {
 	bcClient := beaconchain.NewClient("54.183.5.66", strconv.Itoa(port+libs.BeaconchainServicePortDiff))
 	response := bcClient.GetLeaders()
 
+	// dummy host for wallet
+	self := p2p.Peer{IP: "127.0.0.1", Port: "6789"}
+	priKey, _, _ := utils.GenKeyP2P("127.0.0.1", "6789")
+	host, err := p2pimpl.NewHost(&self, priKey)
+	if err != nil {
+		panic(err)
+	}
+
 	for _, leader := range response.Leaders {
 		peerID, err := peer.IDB58Decode(leader.PeerID)
 		if err != nil {
 			panic(err)
 		}
-		shardIDLeaderMap[leader.ShardId] = p2p.Peer{IP: leader.Ip, Port: leader.Port, PeerID: peerID}
+		leaderPeer := p2p.Peer{IP: leader.Ip, Port: leader.Port, PeerID: peerID}
+		shardIDLeaderMap[leader.ShardId] = leaderPeer
+		host.AddPeer(&leaderPeer)
 	}
 
-	// dummy host for wallet
-	self := p2p.Peer{IP: "127.0.0.1", Port: "6789"}
-	priKey, _, _ := utils.GenKeyP2P("127.0.0.1", "6789")
-	host, _ := p2pimpl.NewHost(&self, priKey)
 	walletNode := node.New(host, nil, nil)
 	walletNode.Client = client.NewClient(walletNode.GetHost(), &shardIDLeaderMap)
 	return walletNode
