7f5b1678494a8e61610fa6b09a02daa4a548a675
Eugene Kim <ek@harmony.one>
2019-01-25 17:00:08 -0800

    Let caller specify where to find OpenSSL/MCL/BLS
    This is done by defining {OPENSSL,MCL,BLS}_DIR environment variable.

--- scripts/setup_bls_build_flags.sh
+++ scripts/setup_bls_build_flags.sh
@@ -3,16 +3,19 @@
 unset -v gopath
 gopath=$(go env GOPATH)
 HMY_PATH="${gopath%%:*}/src/github.com/harmony-one"
-export CGO_CFLAGS="-I$HMY_PATH/bls/include -I$HMY_PATH/mcl/include"
-export CGO_LDFLAGS="-L$HMY_PATH/bls/lib"
-export LD_LIBRARY_PATH=$HMY_PATH/bls/lib:$HMY_PATH/mcl/lib
+: ${OPENSSL_DIR="/usr/local/opt/openssl"}
+: ${MCL_DIR="${HMY_PATH}/mcl"}
+: ${BLS_DIR="${HMY_PATH}/bls"}
+export CGO_CFLAGS="-I${BLS_DIR}/include -I${MCL_DIR}/include"
+export CGO_LDFLAGS="-L${BLS_DIR}/lib"
+export LD_LIBRARY_PATH=${BLS_DIR}/lib:${MCL_DIR}/lib
 
 OS=$(uname -s)
 case $OS in
    Darwin)
-      export CGO_CFLAGS="-I$HMY_PATH/bls/include -I$HMY_PATH/mcl/include -I/usr/local/opt/openssl/include"
-      export CGO_LDFLAGS="-L$HMY_PATH/bls/lib -L/usr/local/opt/openssl/lib"
-      export LD_LIBRARY_PATH=$HMY_PATH/bls/lib:$HMY_PATH/mcl/lib:/usr/local/opt/openssl/lib
+      export CGO_CFLAGS="-I${BLS_DIR}/include -I${MCL_DIR}/include -I${OPENSSL_DIR}/include"
+      export CGO_LDFLAGS="-L${BLS_DIR}/lib -L${OPENSSL_DIR}/lib"
+      export LD_LIBRARY_PATH=${BLS_DIR}/lib:${MCL_DIR}/lib:${OPENSSL_DIR}/lib
       export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
       ;;
 esac
