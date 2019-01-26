68594d8bd42ce852ed36b6146edd1a6ddef2f140
Eugene Kim <ek@harmony.one>
2019-01-25 16:55:20 -0800

    If unset, take default GOPATH from go env

--- scripts/setup_bls_build_flags.sh
+++ scripts/setup_bls_build_flags.sh
@@ -1,6 +1,8 @@
 # no shebang; to be sourced from other scripts
 
-HMY_PATH=$GOPATH/src/github.com/harmony-one
+unset -v gopath
+gopath=$(go env GOPATH)
+HMY_PATH="${gopath}/src/github.com/harmony-one"
 export CGO_CFLAGS="-I$HMY_PATH/bls/include -I$HMY_PATH/mcl/include"
 export CGO_LDFLAGS="-L$HMY_PATH/bls/lib"
 export LD_LIBRARY_PATH=$HMY_PATH/bls/lib:$HMY_PATH/mcl/lib
