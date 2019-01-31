4b8762682479b3d1fadc0f51cf41b14e11310fda
alok <alok@simple-rules.com>
2019-01-30 16:52:41 -0800

    nit renaming node to node address

--- contracts/DepositContract.sol
+++ contracts/DepositContract.sol
@@ -10,7 +10,7 @@ contract DepositContract {
   event LogUpdateStake(address indexed nodeAddress, uint index,  uint stake);
   event LogDeleteNode(address indexed nodeAddress, uint index);
   
-  function isNode(address nodeAddress) public constant returns(bool isIndeed) 
+  function isNodeAddress(address nodeAddress) public constant returns(bool isIndeed) 
   {
     if(nodeAddressList.length == 0) return false;
     return (nodeAddressList[Indices[nodeAddress]] == nodeAddress);
