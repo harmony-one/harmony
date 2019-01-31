03f513b50e2562ba10568affad255dce13e05043
Minh Doan <minh@harmony.one>
2019-01-31 07:55:37 -0800

    retouch rgen

---
+++ api/service/consensus/consensus.go
@@ -0,0 +1,35 @@
+package consensus
+
+import (
+	"github.com/harmony-one/harmony/consensus"
+	"github.com/harmony-one/harmony/core/types"
+	"github.com/harmony-one/harmony/internal/utils"
+)
+
+// Service is the consensus service.
+type Service struct {
+	blockChannel chan *types.Block // The channel to receive new blocks from Node
+	consensus    *consensus.Consensus
+	stopChan     chan struct{}
+	stoppedChan  chan struct{}
+}
+
+// NewService returns consensus service.
+func NewService(blockChannel chan *types.Block, consensus *consensus.Consensus) *Service {
+	return &Service{blockChannel: blockChannel, consensus: consensus}
+}
+
+// StartService starts service.
+func (s *Service) StartService() {
+	s.stopChan = make(chan struct{})
+	s.stoppedChan = make(chan struct{})
+	s.consensus.WaitForNewBlock(s.blockChannel, s.stopChan, s.stoppedChan)
+}
+
+// StopService stops service.
+func (s *Service) StopService() {
+	utils.GetLogInstance().Info("Stopping consensus service.")
+	s.stopChan <- struct{}{}
+	<-s.stoppedChan
+	utils.GetLogInstance().Info("Consensus service stopped.")
+}
