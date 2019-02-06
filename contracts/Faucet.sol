8ceaad693f40fcb4eabf0c8d298eebb937258d70
alok <alok@simple-rules.com>
2019-02-05 22:32:22 -0800

    Add Faucet Contract to contracts

---
+++ contracts/Faucet.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.4.22 <0.6.0;
+contract Faucet {
+    mapping(address => bool) processed;
+    uint quota = 0.5 ether;
+    address owner;
+    constructor() public payable {
+        owner = msg.sender;
+    }
+    function request(address payable requestor) public {
+        require(msg.sender == owner);
+        require(quota <= address(this).balance);
+        require(!processed[requestor]);
+        processed[requestor] = true;
+        requestor.transfer(quota);
+	}
+    function money() public view returns(uint) {
+        return address(this).balance;
+	}
+}
