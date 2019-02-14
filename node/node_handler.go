23660d7b2a6d9c6542c71c26e59269fe23e9cae4
Minh Doan <minh@harmony.one>
2019-02-14 07:58:52 -0800

    fix typos

--- node/node_handler.go
+++ node/node_handler.go
@@ -135,7 +135,6 @@ func (node *Node) messageHandler(content []byte, sender string) {
 			utils.GetLogInstance().Info("NET: Leader received message:", "messageCategory", msgCategory)
 			consensusObj.ProcessMessageLeader(msgPayload)
 		} else {
-<<<<<<< HEAD
 			utils.GetLogInstance().Info("NET: Validator received message:", "messageCategory", msgCategory)
 			consensusObj.ProcessMessageValidator(msgPayload)
 			// TODO(minhdoan): add logic to check if the current blockchain is not sync with other consensus
