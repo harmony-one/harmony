7615c7bf5a9470af69d7620febe71272e17009a8
Eugene Kim <ek@harmony.one>
2019-02-15 10:20:05 -0800

    Refactor for readability
    Suggested-by: Leo Chen <leo@harmony.one>

--- scripts/gogenerate.sh
+++ scripts/gogenerate.sh
@@ -1,33 +1,9 @@
 #!/bin/sh
+# run go generate on .go files under source control; group by dir (package).
 unset -v progdir
 case "${0}" in
 */*) progdir="${0%/*}";;
 *) progdir=.;;
 esac
-git grep -l '^//go:generate ' | sort -t/ | (
-	gogenerate() {
-		case $# in 0) return;; esac
-		go generate "$@"
-	}
-	unset -v prevdir dir file
-	prevdir=
-	set --
-	while read -r file
-	do
-		case "${file}" in
-		*/*) dir="${file%/*}";;
-		*) dir=".";;
-		esac
-		case "${prevdir}" in
-		"${dir}")
-			set -- "${@}" "${file}"
-			;;
-		*) # includes ""
-			gogenerate "${@}" # no-op for ""
-			set -- "${file}"
-			;;
-		esac
-		prevdir="${dir}"
-	done
-	gogenerate "$@"
-)
+git grep -l '^//go:generate ' -- '*.go' | \
+	"${progdir}/xargs_by_dir.sh" go generate
