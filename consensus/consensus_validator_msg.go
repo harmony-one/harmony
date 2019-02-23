8aba358faffcc653b01da8c8bf4f5f372d1c953d
Minh Doan <minh@harmony.one>
2019-02-22 16:59:17 -0800

    fix typos

--- consensus/consensus_validator_msg.go
+++ consensus/consensus_validator_msg.go
@@ -9,7 +9,7 @@ import (
 // Construct the prepare message to send to leader (assumption the consensus data is already verified)
 func (consensus *Consensus) constructPrepareMessage() []byte {
 	message := &msg_pb.Message{
-		ReceiverType: msg_pb.ReceiverType_VALIDATOR,
+		ReceiverType: msg_pb.ReceiverType_LEADER,
 		ServiceType:  msg_pb.ServiceType_CONSENSUS,
 		Type:         msg_pb.MessageType_PREPARE,
 		Request: &msg_pb.Message_Consensus{
@@ -36,7 +36,7 @@ func (consensus *Consensus) constructPrepareMessage() []byte {
 // Construct the commit message which contains the signature on the multi-sig of prepare phase.
 func (consensus *Consensus) constructCommitMessage(multiSigAndBitmap []byte) []byte {
 	message := &msg_pb.Message{
-		ReceiverType: msg_pb.ReceiverType_VALIDATOR,
+		ReceiverType: msg_pb.ReceiverType_LEADER,
 		ServiceType:  msg_pb.ServiceType_CONSENSUS,
 		Type:         msg_pb.MessageType_COMMIT,
 		Request: &msg_pb.Message_Consensus{
