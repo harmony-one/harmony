23565101a63f2ed7fe98431e8ac4d0d7a2e650de
Minh Doan <minh@harmony.one>
2019-03-01 16:21:01 -0800

    add stop all services in service managers except the liveServices list

--- api/service/manager.go
+++ api/service/manager.go
@@ -199,3 +199,17 @@ func (m *Manager) StopService(t Type) {
 		service.StopService()
 	}
 }
+
+// StopServicesByRole stops all service of the given role.
+func (m *Manager) StopServicesByRole(liveServices []Type) {
+	marked := make(map[Type]bool)
+	for _, s := range liveServices {
+		marked[s] = true
+	}
+
+	for t := range m.GetServices() {
+		if _, ok := marked[t]; !ok {
+			m.StopService(t)
+		}
+	}
+}
