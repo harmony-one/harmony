5fd6271ebb8f6abf04f0ddec7ee2368048072ac0
animeshg <animesh.g@zeta.tech>
2019-03-03 14:44:37 +0530

    Add tests in Msg Validator & Leader

--- drand/drand_leader_msg_test.go
+++ drand/drand_leader_msg_test.go
@@ -24,3 +24,21 @@ func TestConstructInitMessage(test *testing.T) {
 		test.Errorf("Init message is not constructed in the correct size: %d", len(msg))
 	}
 }
+
+func TestProcessCommitMessage(test *testing.T) {
+	leader := p2p.Peer{IP: "127.0.0.1", Port: "19999"}
+	validator := p2p.Peer{IP: "127.0.0.1", Port: "55555"}
+	priKey, _, _ := utils.GenKeyP2P("127.0.0.1", "9902")
+	host, err := p2pimpl.NewHost(&leader, priKey)
+	if err != nil {
+		test.Fatalf("newhost failure: %v", err)
+	}
+	dRand := New(host, "0", []p2p.Peer{leader, validator}, leader, nil, true)
+	dRand.blockHash = [32]byte{}
+	msg := dRand.constructCommitMessage([32]byte{}, []byte{})
+
+	if len(msg) != 191 {
+		test.Errorf("Commit message is not constructed in the correct size: %d", len(msg))
+	}
+	dRand.ProcessMessageLeader(msg)
+}
--- drand/drand_validator_msg_test.go
+++ drand/drand_validator_msg_test.go
@@ -24,3 +24,22 @@ func TestConstructCommitMessage(test *testing.T) {
 		test.Errorf("Commit message is not constructed in the correct size: %d", len(msg))
 	}
 }
+
+func TestProcessInitMessage(test *testing.T) {
+	leader := p2p.Peer{IP: "127.0.0.1", Port: "19999"}
+	validator := p2p.Peer{IP: "127.0.0.1", Port: "55555"}
+	priKey, _, _ := utils.GenKeyP2P("127.0.0.1", "9902")
+	host, err := p2pimpl.NewHost(&leader, priKey)
+	if err != nil {
+		test.Fatalf("newhost failure: %v", err)
+	}
+	dRand := New(host, "0", []p2p.Peer{leader, validator}, leader, nil, true)
+	dRand.blockHash = [32]byte{}
+	msg := dRand.constructInitMessage()
+
+	if len(msg) != 93 {
+		test.Errorf("Init message is not constructed in the correct size: %d", len(msg))
+	}
+
+	dRand.ProcessMessageValidator(msg)
+}
