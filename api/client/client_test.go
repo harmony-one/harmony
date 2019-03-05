fd4954bf4f71777950940099651e79648ce8be6f
chao <chao@harmony.one>
2019-03-04 19:14:39 -0800

    remove unnecessary leader map

--- api/client/client_test.go
+++ api/client/client_test.go
@@ -1,24 +1,14 @@
 package client
 
 import (
-	"reflect"
 	"testing"
-
-	"github.com/harmony-one/harmony/p2p"
 )
 
 func TestClient(t *testing.T) {
-	leaders := map[uint32]p2p.Peer{
-		0: p2p.Peer{
-			IP:   "127.0.0.1",
-			Port: "90",
-		},
-	}
-	client := NewClient(nil, leaders)
+	shardIDs := []uint32{0}
+	client := NewClient(nil, shardIDs)
 
-	leadersGot := client.GetLeaders()
-
-	if len(leadersGot) != 1 || !reflect.DeepEqual(leaders[0], leadersGot[0]) {
-		t.Errorf("expected: %v, got: %v\n", leaders[0], leadersGot[0])
+	if len(client.ShardIDs) != 1 || client.ShardIDs[0] != 0 {
+		t.Errorf("client initiate incorrect")
 	}
 }
