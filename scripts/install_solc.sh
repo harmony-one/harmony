b213e890d3a8739b3ff49cd8415db7384e476f21
Rongjian Lan <rongjian.lan@gmail.com>
2019-03-07 18:52:32 -0800

    auto yes

--- scripts/install_solc.sh
+++ scripts/install_solc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:ethereum/ethereum
-sudo apt-get update
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get -y update
 sudo apt-get -y install solc
