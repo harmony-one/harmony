661a090a67a833532b1ba69825d3c3c7b14bed29
Minh Doan <minh@harmony.one>
2019-03-12 23:08:05 -0700

    add gen.sh

---
+++ contracts/gen.sh
@@ -0,0 +1,3 @@
+abigen -sol Lottery.sol  -out Lottery.go --pkg contracts
+# abigen -sol Faucet.sol  -out Faucet.go --pkg contracts
+# abigen -sol StakeLockContract.sol  -out StakeLockContract.go --pkg contracts
