d9804bce8993b1c890554d9915bba04c52a08cbe
Minh Doan <minh@harmony.one>
2019-03-14 13:51:40 -0700

    wip

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -20,8 +20,8 @@ func main() {
 
 	router := mux.NewRouter()
 	// Set up router for blocks.
-	// s.router.Path("/blocks").Queries("from", "{[0-9]*?}", "to", "{[0-9]*?}").HandlerFunc(s.GetExplorerBlocks).Methods("GET")
-	// s.router.Path("/blocks").HandlerFunc(s.GetExplorerBlocks)
+	s.router.Path("/blocks").Queries("from", "{[0-9]*?}", "to", "{[0-9]*?}").HandlerFunc(s.GetExplorerBlocks).Methods("GET")
+	s.router.Path("/blocks").HandlerFunc(s.GetExplorerBlocks)
 
 	server := &http.Server{Addr: addr, Handler: router}
 	server.ListenAndServe()
