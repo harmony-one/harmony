6457e05ceafefbf53288fd15b5398ed932a399a9
Leo Chen <leo@harmony.one>
2019-03-19 07:33:43 +0000

    [build] fix jenkins build issue
    Signed-off-by: Leo Chen <leo@harmony.one>

--- scripts/setup_bls_build_flags.sh
+++ scripts/setup_bls_build_flags.sh
@@ -1,8 +1,18 @@
 # no shebang; to be sourced from other scripts
 
+unset -v progdir
+case "${0}" in
+*/*) progdir="${0%/*}";;
+*) progdir=.;;
+esac
+
 unset -v gopath
 gopath=$(go env GOPATH)
+# HMY_PATH is the common root directory of all harmony repos
 HMY_PATH="${gopath%%:*}/src/github.com/harmony-one"
+if [ ! -d $HMY_PATH ]; then
+   HMY_PATH=$(realpath $progdir/../..)
+fi
 : ${OPENSSL_DIR="/usr/local/opt/openssl"}
 : ${MCL_DIR="${HMY_PATH}/mcl"}
 : ${BLS_DIR="${HMY_PATH}/bls"}
