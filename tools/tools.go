3fe16fd8cc7878abb33cedf10737deb0b16e8cd3
Eugene Kim <ek@harmony.one>
2019-03-20 10:48:09 -0700

    Version-peg mockgen
    The provider module (github.com/golang/mock) was already in go.mod as
    a test dependency.

--- tools/tools.go
+++ tools/tools.go
@@ -10,6 +10,7 @@ package tools
 // Put only installable tools into this list.
 // scripts/install_build_tools.sh parses these imports to install them.
 import (
+	_ "github.com/golang/mock/mockgen"
 	_ "github.com/harmony-ek/gencodec"
 	_ "golang.org/x/lint/golint"
 	_ "golang.org/x/tools/cmd/goimports"
