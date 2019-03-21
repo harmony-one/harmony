67c2e875713b75cec06b615bede24163a92980ba
Minh Doan <minh@harmony.one>
2019-03-20 18:09:37 -0700

    add rest client support cmd

---
+++ cmd/restclientsupport/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/harmony-one/harmony/api/service/restclientsupport"
+)
+
+func main() {
+	s := restclientsupport.New(nil, nil, nil)
+	s.StartService()
+	fmt.Println("Server started")
+	select {}
+}
