ca6fd788ce96fb35396d82399d4927572cc86b24
Eugene Kim <ek@harmony.one>
2019-03-30 12:42:35 -0700

    Factor build tool installation out into a script
    For now it only moves protoc installation.  Support for version-pegging
    “go get”-able tools is included, but since tools/tools.go lists no tools
    yet, it is a no-op.  The list will be populated/migrated in later
    commits.

---
+++ scripts/install_build_tools.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eu
+
+unset -v progdir
+case "${0}" in
+/*) progdir="/";;
+*/*) progdir="${0%/*}";;
+*) progdir=".";
+esac
+
+sed -n 's/^	_ "\([^"]*\)"$/\1/p' "${progdir}/../tools/tools.go" | \
+	xargs "${progdir}/goget.sh"
+
+"${progdir}/install_protoc.sh" -V 3.6.1
