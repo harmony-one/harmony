d586b29fd6b507237e55bfce791c2b51ad415864
Eugene Kim <ek@harmony.one>
2019-03-30 15:14:30 -0700

    Record tools in tools/tools.go
    The list is empty; it will be populated in later commits.

---
+++ tools/tools.go
@@ -0,0 +1,13 @@
+// Do not remove the following build tag line: It exempts this file from normal
+// builds, which would fail because the imports are programs – package main –
+// and not really importable packages.
+//
+// +build tools
+
+// Package tools provides build tools necessary for Harmony.
+package tools
+
+// Put only installable tools into this list.
+// scripts/install_build_tools.sh parses these imports to install them.
+import (
+)
