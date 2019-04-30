167cc2e5b2d3e0f2a5b3fcbdcd5389d1ae29a6b3
Minh Doan <minh@harmony.one>
2019-04-30 00:19:43 -0700

    add puzzle contract

---
+++ contracts/Puzzle.sol
@@ -0,0 +1,36 @@
+pragma solidity >=0.4.22; 
+contract Puzzle {
+    string internal constant INSUFFICIENT_FUND_MESSAGE = "Insufficient Fund";
+    string internal constant RESTRICTED_MESSAGE = "Unauthorized Access";
+    address public manager;  // The adress of the owner of this contract
+    mapping(address => uint8) public level_map;
+
+    constructor() public payable {
+        manager = msg.sender;
+    }
+
+    /**
+     * @dev The player enters into the current game session by
+     *      paying at least 2 token.
+     */
+    function play(uint8 level) public payable {
+        require(msg.value >= 2 ether, INSUFFICIENT_FUND_MESSAGE);
+        level_map[msg.sender] = level;
+    }
+
+    /**
+     * @dev pay the player if they have crossed their last best level.
+     */
+    function payout(address player, uint8 new_level) public payable restricted {
+        uint8 cur_level = level_map[player];
+        if (new_level >= cur_level) {
+            player.transfer(2 * (new_level - cur_level + 1) * 1 ether);
+        }
+        delete level_map[player];
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager, RESTRICTED_MESSAGE);
+        _;
+    }
+}
