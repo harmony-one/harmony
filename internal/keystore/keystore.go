3865c075de37966d2a28db5bdf61f47d92dc994b
Leo Chen <leo@harmony.one>
2019-05-21 22:42:53 -0700

    [keystore] add functions to unlock/signtx
    Signed-off-by: Leo Chen <leo@harmony.one>

--- internal/keystore/keystore.go
+++ internal/keystore/keystore.go
@@ -1,9 +1,12 @@
 package keystore
 
 import (
+	"fmt"
 	"sync"
 
+	"github.com/harmony-one/harmony/accounts"
 	"github.com/harmony-one/harmony/accounts/keystore"
+	"github.com/harmony-one/harmony/core/types"
 )
 
 var (
@@ -13,6 +16,7 @@ var (
 	scryptN            = keystore.StandardScryptN
 	scryptP            = keystore.StandardScryptP
 	hmyKeystore        *keystore.KeyStore
+	hmyPass            string
 )
 
 // GetHmyKeyStore returns the only keystore of the node
@@ -21,4 +25,24 @@ func GetHmyKeyStore() *keystore.KeyStore {
 		hmyKeystore = keystore.NewKeyStore(DefaultKeyStoreDir, scryptN, scryptP)
 	})
 	return hmyKeystore
+}
+
+// SetHmyPass set the passphrase
+func SetHmyPass(pass string) {
+	hmyPass = pass
+}
+
+// Unlock unlocks the account using passphrase
+func Unlock(account accounts.Account) {
+	if hmyKeystore != nil {
+		hmyKeystore.Unlock(account, hmyPass)
+	}
+}
+
+// SignTx signs transaction using account key
+func SignTx(account accounts.Account, tx *types.Transaction) (*types.Transaction, error) {
+	if hmyKeystore != nil {
+		return hmyKeystore.SignTx(account, tx, nil)
+	}
+	return tx, fmt.Errorf("un-initialized keystore")
 }
