18bf30e6c89064d9167861333f956aa6d57ed6b4
Eugene Kim <ek@harmony.one>
2019-05-23 20:32:01 -0700

    Use uint32 as shard ID type instead of [4]byte
    This does not change RLP encoding.

--- core/genesis.go
+++ core/genesis.go
@@ -240,7 +240,7 @@ func (g *Genesis) ToBlock(db ethdb.Database) *types.Block {
 	head := &types.Header{
 		Number:         new(big.Int).SetUint64(g.Number),
 		Nonce:          types.EncodeNonce(g.Nonce),
-		ShardID:        types.EncodeShardID(g.ShardID),
+		ShardID:        g.ShardID,
 		Time:           new(big.Int).SetUint64(g.Timestamp),
 		ParentHash:     g.ParentHash,
 		Extra:          g.ExtraData,
