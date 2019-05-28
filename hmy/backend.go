73eb3a9541266010f846400851c3863562da414b
Richard Liu <liuzhenglaichn@gmail.com>
2019-05-27 22:41:00 -0700

    add support for bloom indexer

--- hmy/backend.go
+++ hmy/backend.go
@@ -3,12 +3,18 @@ package hmy
 import (
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/event"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/harmony-one/harmony/accounts"
 	"github.com/harmony-one/harmony/core"
+	"github.com/ethereum/go-ethereum/core/bloombits"
 )
 
 // Harmony implements the Harmony full node service.
 type Harmony struct {
+	// Channel for shutting down the service
+	shutdownChan chan bool // Channel for shutting down the Harmony
+	bloomRequests chan chan *bloombits.Retrieval // Channel receiving bloom data retrieval requests
+
 	blockchain     *core.BlockChain
 	txPool         *core.TxPool
 	accountManager *accounts.Manager
@@ -21,16 +27,18 @@ type Harmony struct {
 }
 
 // New creates a new Harmony object (including the
-// initialisation of the common Ethereum object)
+// initialisation of the common Harmony object)
 func New(blockchain *core.BlockChain, txPool *core.TxPool, accountManager *accounts.Manager, eventMux *event.TypeMux) (*Harmony, error) {
 	chainDb := blockchain.ChainDB()
 	hmy := &Harmony{
+		shutdownChan:   make(chan bool),
+		bloomRequests:  make(chan chan *bloombits.Retrieval),
 		blockchain:     blockchain,
 		txPool:         txPool,
 		accountManager: accountManager,
 		eventMux:       eventMux,
 		chainDb:        chainDb,
-		bloomIndexer:   nil, // TODO(ricl): implement
+		bloomIndexer:   NewBloomIndexer(chainDb, params.BloomBitsBlocks, params.BloomConfirms),
 	}
 
 	hmy.APIBackend = &APIBackend{hmy}
