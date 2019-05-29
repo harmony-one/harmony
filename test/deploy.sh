8fbba28a905516e945058f31b74c1baa271fa423
chao <chao@harmony.one>
2019-05-29 11:30:25 -0700

    Clean up default duration logic

--- test/deploy.sh
+++ test/deploy.sh
@@ -69,6 +69,9 @@ EOU
    exit 0
 }
 
+DEFAULT_DURATION_NOSYNC=60
+DEFAULT_DURATION_SYNC=200
+
 DB=false
 TXGEN=true
 DURATION=
@@ -97,13 +100,14 @@ if [ -z "$config" ]; then
    usage
 fi
 
-if [ "$SYNC" == "true" ] && [ -z "$DURATION" ]; then
-   DURATION=200 # set duration to be 200 if duration is unset.
-fi
-
-if [ -z "$DURATION" ]; then
-   DURATION=60 # if duration is unset, set it to be 60
-fi
+case "${DURATION-}" in
+"")
+    case "${SYNC}" in
+    false) DURATION="${DEFAULT_DURATION_NOSYNC}";;
+    true) DURATION="${DEFAULT_DURATION_SYNC}";;
+    esac
+    ;;
+esac
 
 # Kill nodes if any
 cleanup
