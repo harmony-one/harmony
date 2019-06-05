95713a6e2f5bc6bdff771f8e538efcf17becb633
coolcottontail <coolcottontail@gmail.com>
2019-06-04 18:19:34 -0700

    added bls_vrf base code and test case

---
+++ crypto/vrf/bls/bls_vrf.go
@@ -0,0 +1,96 @@
+package bls_vrf
+
+import (
+	"crypto"
+	"crypto/sha256"
+	"errors"
+	"github.com/harmony-one/bls/ffi/go/bls"
+	"github.com/harmony-one/harmony/crypto/vrf"
+)
+
+var (
+
+	ErrInvalidVRF = errors.New("invalid VRF proof")
+)
+
+// PublicKey holds a public VRF key.
+type PublicKey struct {
+	*bls.PublicKey
+}
+
+// PrivateKey holds a private VRF key.
+type PrivateKey struct {
+	*bls.SecretKey
+}
+
+func init() {
+	bls.Init(bls.BLS12_381)
+}
+
+// Public returns the corresponding public key as bytes.
+func (k *PrivateKey) Public() crypto.PublicKey {
+	return *k.SecretKey.GetPublicKey()
+}
+
+// Serialize serialize the public key into bytes
+func (pk *PublicKey) Serialize() []byte {
+	return pk.Serialize()
+}
+
+// Deserialize de-serialize bytes into public key
+func (pk *PublicKey) Deserialize(data []byte) {
+	pk.Deserialize(data)
+}
+
+// NewVRFVerifier creates a verifier object from a public key.
+func NewVRFVerifier(pubkey *bls.PublicKey) vrf.PublicKey {
+	return &PublicKey{pubkey}
+}
+
+// NewVRFSigner creates a signer object from a private key.
+func NewVRFSigner(seck *bls.SecretKey) vrf.PrivateKey {
+	return &PrivateKey{seck}
+}
+
+// verifiable unpredictable function using BLS
+// reference:  https://tools.ietf.org/html/draft-goldbe-vrf-01
+// properties of VRF-BLS:
+// 1) Full Uniqueness : satisfied, it is deterministic
+// 2) Full Pseudorandomness : satisfied through sha256
+// 3) Full Collison Resistance : satisfied through sha256
+func (k *PrivateKey) Evaluate(alpha []byte) ([32]byte, []byte) {
+	//get the BLS signature of the message
+	//pi = VRF_prove(SK, alpha)
+	msgHash := sha256.Sum256(alpha)
+	pi := k.SignHash(msgHash[:])
+
+	//hash the signature and output as VRF beta
+	//beta = VRF_proof2hash(pi)
+	beta := sha256.Sum256(pi.Serialize())
+
+	return beta, pi.Serialize()
+}
+
+// ProofToHash asserts that proof is correct for input alpha and output VRF hash
+func (pk *PublicKey) ProofToHash(alpha, pi []byte) ([32]byte, error) {
+	nilIndex := [32]byte{}
+
+	if len(pi) == 0 {
+		return nilIndex, ErrInvalidVRF
+	}
+
+	msgSig := bls.Sign{}
+
+	err := msgSig.Deserialize(pi)
+
+	if err != nil {
+		return nilIndex, err
+	}
+
+	msgHash := sha256.Sum256(alpha)
+	if !msgSig.VerifyHash(pk.PublicKey, msgHash[:]) {
+		return nilIndex, ErrInvalidVRF
+	}
+
+	return sha256.Sum256(pi), nil
+}
