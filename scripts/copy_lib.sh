81ab6759d989ee2e7298dde6d8b41be3401bb5c8
Minh Doan <minh@harmony.one>
2019-06-11 12:44:56 -0700

    script used to find and upload libs to aws in the future.

---
+++ scripts/copy_lib.sh
@@ -0,0 +1,6 @@
+# Scripts used to find all deps library in order we need to update and upload the lib to aws in the future.
+cp /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib ~/Downloads/
+cp /Users/$USER/go/src/github.com/harmony-one/bls/lib/libbls384_256.dylib ~/Downloads/
+cp /Users/$USER/go/src/github.com/harmony-one/mcl/lib/libmcl.dylib ~/Downloads/
+cp /usr/local/lib/libgmpxx.4.dylib ~/Downloads/
+cp /usr/local/lib/libgmp.10.dylib ~/Downloads/
