47e3bce79bdba22fa9ef44fbb5ff4001d16c22de
Rongjian Lan <rongjian.lan@gmail.com>
2019-06-10 23:19:00 -0700

    fix lint

--- internal/genesis/newnodes.go
+++ internal/genesis/newnodes.go
@@ -1,7 +1,6 @@
 package genesis
 
 // NewNodeAccounts are the accounts for the new node
-
 var NewNodeAccounts = [...]DeployAccount{
 	{Address: "0x0e59b767D5E74cf7B29Ef9bEc3dA4c402d357C6C", BlsPriKey: "bb0f9464c1ef92db53cf22953b4eeff4b78dd5af47c65a991ee6cf081b8d9569"},
 	{Address: "0x0fAAda81c203C74CAc786786f7D428477a04bF9c", BlsPriKey: "eaff001d11db657636b1b62895cfa0f35ac767579d08e5959826a56cf7c43667"},
@@ -12,4 +11,5 @@ var NewNodeAccounts = [...]DeployAccount{
 	{Address: "0x141B0e0f05739B7B784654E973e9b9146473aAb9", BlsPriKey: "06c2aac72636fb74df815688696fe12c4894a50c016e6016a6e51f9452c77b36"},
 	{Address: "0x1492ebD0EcfD54B4c211b37C8891bA3493c52100", BlsPriKey: "04e762a7c540d2f2f566c0f1729475753914b42e6293a8e6653f29fc6ffba273"},
 	{Address: "0x1530A04592F9C3bF06aC6044525f08937ED38edB", BlsPriKey: "68bd252b49616db29a5b923c5508374b3ee8184b2d3899a1d71d5ea0f3e68348"},
-	{Address: "0xE2ab78ecf325084485957B2599d53Bcf944Cbca8", BlsPriKey: "d09a1c5efd391ef432d5be413d062374643f7f6f68fa59e58409d88d985c6d38"}}
+	{Address: "0xE2ab78ecf325084485957B2599d53Bcf944Cbca8", BlsPriKey: "d09a1c5efd391ef432d5be413d062374643f7f6f68fa59e58409d88d985c6d38"},
+}
