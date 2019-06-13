e476b8aa580786995ccf144845100a52c1a5a68b
Minh Doan <minh@harmony.one>
2019-06-13 10:55:32 -0700

    fix to make it working

--- core/resharding.go
+++ core/resharding.go
@@ -2,10 +2,8 @@ package core
 
 import (
 	"encoding/binary"
-	"fmt"
 	"math/big"
 	"math/rand"
-	"os"
 	"sort"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -228,15 +226,16 @@ func GetInitShardState() types.ShardState {
 		com := types.Committee{ShardID: uint32(i)}
 		for j := 0; j < GenesisShardHarmonyNodes; j++ {
 			index := i + j*GenesisShardNum // The initial account to use for genesis nodes
-			pubKey := types.BlsPublicKey{}
+
+			// TODO: Old code. Will remove it later as long as the migration works.
+			// priKey := bls.SecretKey{}
+			// priKey.DeserializeHexStr(genesis.GenesisAccounts[index].BlsPriKey)
+			// pubKey := types.BlsPublicKey{}
+			// pubKey.FromLibBLSPublicKey(priKey.GetPublicKey())
+
 			pub := &bls.PublicKey{}
 			pub.DeserializeHexStr(genesis.GenesisAccounts[index].BlsPublicKey)
-			priKey := bls.SecretKey{}
-			priKey.DeserializeHexStr(genesis.GenesisAccounts[index].BlsPriKey)
-			if !pub.IsEqual(priKey.GetPublicKey()) {
-				fmt.Println("SO WRONG")
-				os.Exit(11)
-			}
+			pubKey := types.BlsPublicKey{}
 			pubKey.FromLibBLSPublicKey(pub)
 			// TODO: directly read address for bls too
 			curNodeID := types.NodeID{common2.ParseAddr(genesis.GenesisAccounts[index].Address), pubKey}
@@ -246,10 +245,16 @@ func GetInitShardState() types.ShardState {
 		// add FN runner's key
 		for j := GenesisShardHarmonyNodes; j < GenesisShardSize; j++ {
 			index := i + (j-GenesisShardHarmonyNodes)*GenesisShardNum
-			priKey := bls.SecretKey{}
-			priKey.DeserializeHexStr(genesis.GenesisFNAccounts[index].BlsPriKey)
+
+			// TODO: this is old code. We will remove as long as the migration works.
+			// priKey := bls.SecretKey{}
+			// priKey.DeserializeHexStr(genesis.GenesisFNAccounts[index].BlsPriKey)
+
+			pub := &bls.PublicKey{}
+			pub.DeserializeHexStr(genesis.GenesisAccounts[index].BlsPublicKey)
+
 			pubKey := types.BlsPublicKey{}
-			pubKey.FromLibBLSPublicKey(priKey.GetPublicKey())
+			pubKey.FromLibBLSPublicKey(pub)
 			// TODO: directly read address for bls too
 			curNodeID := types.NodeID{common2.ParseAddr(genesis.GenesisFNAccounts[index].Address), pubKey}
 			com.NodeList = append(com.NodeList, curNodeID)
