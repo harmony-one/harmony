16eed6666d56a6c5443dbdb5a3f5a840e4bc526b
Minh Doan <minh@harmony.one>
2019-06-18 22:25:17 -0700

    disable 2nd call which is unnessary

--- cmd/harmony/main.go
+++ cmd/harmony/main.go
@@ -230,7 +230,7 @@ func createGlobalConfig() *nodeconfig.ConfigType {
 	nodeConfig.ShardID = uint32(genesisAccount.ShardID)
 
 	// Set up consensus keys.
-	setUpConsensusKeyAndReturnIndex(nodeConfig)
+	// setUpConsensusKeyAndReturnIndex(nodeConfig)
 
 	// P2p private key is used for secure message transfer between p2p nodes.
 	nodeConfig.P2pPriKey, _, err = utils.LoadKeyFromFile(*keyFile)
