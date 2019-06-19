5b5313a5294625e881a529c1226441c63e7acff1
Lukas Kozak <luks.kozak@gmail.com>
2019-06-20 00:52:51 +0200

    Fix consistency
    Signed-off-by: Lukas Kozak <luks.kozak@gmail.com>

--- scripts/bls_bintohex.py
+++ scripts/bls_bintohex.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
 
 import argparse
-import sys
 import binascii
+import sys
 
-parser = argparse.ArgumentParser(description="Tool to convert your BLS key from binary to the accepted hex format.")
+parser = argparse.ArgumentParser(description='Tool to convert your BLS key from binary to the accepted hex format.')
 parser.add_argument('file')
-parser.add_argument('-o', '--output', type=str, metavar="output", help="name your keyfile")
+parser.add_argument('-o', '--output', type=str, metavar='output', help='name your keyfile')
 args = parser.parse_args()
 
 try:
     with open(args.file, 'rb') as inputFile:
         binFormat = inputFile.read()
 except IOError:
-    print("File couldn't be opened.")
+    print('File couldn\'t be opened.')
     sys.exit(1)
 
 hexFormat = binascii.hexlify(binFormat)
 
+if args.output is not None:
+    outputName = args.output
+else:
+    outputName = 'PUB.key'
+
 try:
-    if args.output is not None:
-        outputName = args.output
-    else:
-        outputName = 'PUB.key'
     with open(outputName, 'wt') as outputFile:
         outputFile.write(hexFormat.decode('ascii'))
 except IOError:
-    print("File couldn't be written.")
+    print('File couldn\'t be written.')
     sys.exit(1)
