215ae81b079960ae6abe42dc17fe7b0f7dfd79cd
Leo Chen <leo@harmony.one>
2019-06-27 21:08:42 +0000

    [bootnode] add two more bootnodes for r3 release
    Signed-off-by: Leo Chen <leo@harmony.one>

--- scripts/node.sh
+++ scripts/node.sh
@@ -220,7 +220,7 @@ fi
 myip
 
 # public boot node multiaddress
-BN_MA=/ip4/100.26.90.187/tcp/9874/p2p/Qmdfjtk6hPoyrH1zVD9PEH4zfWLo38dP2mDvvKXfh3tnEv,/ip4/54.213.43.194/tcp/9874/p2p/QmZJJx6AdaoEkGLrYG4JeLCKeCKDjnFz2wfHNHxAqFSGA9
+BN_MA=/ip4/100.26.90.187/tcp/9874/p2p/Qmdfjtk6hPoyrH1zVD9PEH4zfWLo38dP2mDvvKXfh3tnEv,/ip4/54.213.43.194/tcp/9874/p2p/QmZJJx6AdaoEkGLrYG4JeLCKeCKDjnFz2wfHNHxAqFSGA9,/ip4/13.113.101.219/tcp/12019/p2p/QmQayinFSgMMw5cSpDUiD9pQ2WeP6WNmGxpZ6ou3mdVFJX,/ip4/99.81.170.167/tcp/12019/p2p/QmRVbTpEYup8dSaURZfF6ByrMTSKa4UyUzJhSjahFzRqNj
 
 if ${start_clean}
 then
