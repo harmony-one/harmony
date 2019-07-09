a7f4ea0374dfb721bc632b99b3bcb01fb99d71f5
Leo Chen <leo@harmony.one>
2019-07-09 03:22:41 -0700

    [testnet] add two set of new dummy keys
    Signed-off-by: Leo Chen <leo@harmony.one>

--- internal/configs/sharding/testnet.go
+++ internal/configs/sharding/testnet.go
@@ -14,9 +14,15 @@ type testnetSchedule struct{}
 
 func (testnetSchedule) InstanceForEpoch(epoch *big.Int) Instance {
 	switch {
+	case epoch.Cmp(big.NewInt(5)) >= 0:
+		return testnetV2
+	case epoch.Cmp(big.NewInt(2)) >= 0:
+		return testnetV1
 	default: // genesis
 		return testnetV0
 	}
 }
 
 var testnetV0 = MustNewInstance(2, 5, 5, genesis.TNHarmonyAccounts, genesis.FoundationalNodeAccounts)
+var testnetV1 = MustNewInstance(2, 5, 5, genesis.TNHarmonyAccountsV1, genesis.FoundationalNodeAccounts)
+var testnetV2 = MustNewInstance(2, 6, 6, genesis.TNHarmonyAccountsV2, genesis.FoundationalNodeAccounts)
