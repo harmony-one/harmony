7d353c41204a3ffd2ae6f0cccbc11615e8001bf4
Leo Chen <leo@harmony.one>
2019-07-10 01:10:04 -0700

    [test] enable epoch/resharding test on localnet network
    Signed-off-by: Leo Chen <leo@harmony.one>

--- internal/configs/sharding/localnet.go
+++ internal/configs/sharding/localnet.go
@@ -14,9 +14,15 @@ type localnetSchedule struct{}
 
 func (localnetSchedule) InstanceForEpoch(epoch *big.Int) Instance {
 	switch {
+	case epoch.Cmp(big.NewInt(20)) >= 0:
+		return localnetV2
+	case epoch.Cmp(big.NewInt(10)) >= 0:
+		return localnetV1
 	default: // genesis
 		return localnetV0
 	}
 }
 
 var localnetV0 = MustNewInstance(2, 7, 5, genesis.LocalHarmonyAccounts, genesis.LocalFnAccounts)
+var localnetV1 = MustNewInstance(2, 7, 5, genesis.LocalHarmonyAccountsV1, genesis.LocalFnAccountsV1)
+var localnetV2 = MustNewInstance(2, 7, 4, genesis.LocalHarmonyAccountsV2, genesis.LocalFnAccountsV2)
