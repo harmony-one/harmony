c63dc3251b19aad68bee86e5d3b9a529e5130350
Minh Doan <minh@harmony.one>
2019-07-10 17:50:13 -0700

    add minting demo addresses to local test

--- test/debug.sh
+++ test/debug.sh
@@ -1,6 +1,3 @@
 ./test/kill_node.sh
 rm -rf tmp_log*
-gsed -i 's/GenesisShardNum = 4/GenesisShardNum = 1/' core/resharding.go
-gsed -i 's/GenesisShardSize = 150/GenesisShardSize = 20/' core/resharding.go
-gsed -i 's/GenesisShardHarmonyNodes = 112/GenesisShardHarmonyNodes = 20/' core/resharding.go
-./test/deploy.sh -D -1 ./test/configs/beaconchain20.txt -network_type testnet
+./test/deploy.sh -D -1 ./test/configs/local.txt
