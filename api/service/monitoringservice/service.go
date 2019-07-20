bd852de962d140cd824165a4a962824522d92451
flicker-harmony <vlad@harmony.one>
2019-07-20 08:07:46 +0300

    Initial monitoring service

---
+++ api/service/monitoringservice/service.go
@@ -0,0 +1,217 @@
+package explorer
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/gorilla/mux"
+	libp2p_peer "github.com/libp2p/go-libp2p-peer"
+
+	msg_pb "github.com/harmony-one/harmony/api/proto/message"
+	"github.com/harmony-one/harmony/core/types"
+	common2 "github.com/harmony-one/harmony/internal/common"
+	"github.com/harmony-one/harmony/internal/ctxerror"
+	"github.com/harmony-one/harmony/internal/utils"
+	"github.com/harmony-one/harmony/p2p"
+)
+
+// Constants for explorer service.
+const (
+	monitoringServicePortDifference = 5000
+)
+
+// HTTPError is an HTTP error.
+type HTTPError struct {
+	Code int
+	Msg  string
+}
+
+//
+type ConnectionsLog struct {
+	Time int
+	ConnectionsNumber int
+}
+
+// Service is the struct for monitoring service.
+type Service struct {
+	router            *mux.Router
+	IP                string
+	Port              string
+	GetNodeIDs        func() []libp2p_peer.ID
+	storage           *MetricsStorage
+	server            *http.Server
+	messageChan       chan *msg_pb.Message
+}
+
+// New returns monitoring service.
+func New(selfPeer *p2p.Peer, GetNodeIDs func() []libp2p_peer.ID) *Service {
+	return &Service{
+		IP:                selfPeer.IP,
+		Port:              selfPeer.Port,
+		GetNodeIDs:        GetNodeIDs,
+	}
+}
+
+// StartService starts monitoring service.
+func (s *Service) StartService() {
+	utils.Logger().Info().Msg("Starting explorer service.")
+	s.Init(true)
+	s.server = s.Run()
+}
+
+// StopService shutdowns monitoring service.
+func (s *Service) StopService() {
+	utils.Logger().Info().Msg("Shutting down monitoring service.")
+	if err := s.server.Shutdown(context.Background()); err != nil {
+		utils.Logger().Error().Err(err).Msg("Error when shutting down monitoring server")
+	} else {
+		utils.Logger().Info().Msg("Shutting down monitoring server successufully")
+	}
+}
+
+// GetMonitoringServicePort returns the port serving monitorign service dashboard. This port is monitoringServicePortDifference less than the node port.
+func GetMonitoringSerivcePort(nodePort string) string {
+	if port, err := strconv.Atoi(nodePort); err == nil {
+		return fmt.Sprintf("%d", port-monitoringServicePortDifference)
+	}
+	utils.Logger().Error().Msg("error on parsing.")
+	return ""
+}
+
+// Init is to initialize for MonitoringService.
+func (s *Service) Init(remove bool) {
+	s.storage = GetStorageInstance(s.IP, s.Port, remove)
+}
+
+// Run is to run serving monitoring service.
+func (s *Service) Run() *http.Server {
+	// Init address.
+	addr := net.JoinHostPort("", GetMonitoringServicePort(s.Port))
+
+	s.router = mux.NewRouter()
+	// Set up router for connections number.
+	s.router.Path("/connectionsNumber").Queries("since", "{[0-9]*?}", "until", "{[0-9]*?}").HandlerFunc(s.GetMonitoringSerivceConnectionsNumber).Methods("GET")
+	s.router.Path("/connectionsNumber").HandlerFunc(s.GetMonitoringSerivceConnectionsNumber)
+
+	// Do serving now.
+	utils.Logger().Info().Str("port", GetMonitoringServicePort(s.Port)).Msg("Listening")
+	server := &http.Server{Addr: addr, Handler: s.router}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			utils.Logger().Warn().Err(err).Msg("server.ListenAndServe()")
+		}
+	}()
+	return server
+}
+
+// ReadConnectionsNumberFromDB returns a list of connections numbers to server connections number end-point.
+func (s *Service) ReadConnectionsNumberFromDB(since, until uint) []uint {
+	connectionsNumbers := []uint
+	for i := since; i <= until; i++ {
+		if i < 0 {
+			connectionsNumbers = append(connectionsNumbers, nil)
+			continue
+		}
+		key := GetConnectionsNumberKey(i)
+		data, err := storage.db.Get([]byte(key))
+		if err != nil {
+			connectionsNumbers = append(connectionsNumbers, nil)
+			continue
+		}
+		connectionsNumber := uint(0)
+		if rlp.DecodeBytes(data, block) != nil {
+			utils.Logger().Error().Msg("Error on getting from db")
+			os.Exit(1)
+		}
+		connectionsNumbers = append(connectionsNumbers, connectionsNumber)
+	}
+	return connectionsNumbers
+}
+
+// GetExplorerBlocks serves end-point /blocks
+func (s *Service) GetMonitoringSerivceConnectionsNumber(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	since := r.FormValue("since")
+	until := r.FormValue("until")
+
+	data := &Data{
+		ConnectionsNumbers: []*ConnectionLog,
+	}
+	defer func() {
+		if err := json.NewEncoder(w).Encode(data.Connections); err != nil {
+			utils.Logger().Warn().Err(err).Msg("cannot JSON-encode connections")
+		}
+	}()
+
+	if since == "" {
+		return
+	}
+	db := s.storage.GetDB()
+	sinceInt, err := strconv.Atoi(since)
+	if err != nil {
+		utils.Logger().Warn().Err(err).Str("since", since).Msg("invalid since parameter")
+		return
+	}
+	var untilInt int
+	if until == "" {
+		untilInt = time.Now().Unix()
+	} else {
+		untilInt, err = strconv.Atoi(until)
+	}
+	if err != nil {
+		utils.Logger().Warn().Err(err).Str("until", until).Msg("invalid until parameter")
+		return
+	}
+
+	connectionsNumbers := s.ReadConnectionsNumbersDB(sinceInt, untilInt)
+	for currentTime, connectionsNumber := range connectionsNumbers {
+		if accountBlocks[id-1] == nil {
+			block.PrevBlock = RefBlock{
+				ID:     "",
+				Height: "",
+			}
+		} else {
+			block.PrevBlock = RefBlock{
+				ID:     accountBlocks[id-1].Hash().Hex(),
+				Height: strconv.Itoa(id + fromInt - 2),
+			}
+		}
+		if accountBlocks[id+1] == nil {
+			block.NextBlock = RefBlock{
+				ID:     "",
+				Height: "",
+			}
+		} else {
+			block.NextBlock = RefBlock{
+				ID:     accountBlocks[id+1].Hash().Hex(),
+				Height: strconv.Itoa(id + fromInt),
+			}
+		}
+		data.ConnectionsNumbers = append(data.ConnectionsNumbers, ConnectionsLog{Time: currentTime, ConnectionsNumber: connectionsNumber})
+	}
+	return
+}
+
+// NotifyService notify service
+func (s *Service) NotifyService(params map[string]interface{}) {
+	return
+}
+
+// SetMessageChan sets up message channel to service.
+func (s *Service) SetMessageChan(messageChan chan *msg_pb.Message) {
+	s.messageChan = messageChan
+}
+
+// APIs for the services.
+func (s *Service) APIs() []rpc.API {
+	return nil
+}
---
+++ api/service/monitoringservice/storage.go
@@ -0,0 +1,98 @@
+package explorer
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/ethdb"
+	"github.com/ethereum/go-ethereum/rlp"
+
+	"github.com/harmony-one/harmony/core/types"
+	"github.com/harmony-one/harmony/internal/ctxerror"
+	"github.com/harmony-one/harmony/internal/utils"
+)
+
+// Constants for storage.
+const (
+	CurrentConnectionsNumberKey = "cnk"
+	ConnectionsNumberPrefix = "cnp"
+)
+
+// GetCurrentConnectionsNumberKey ...
+func GetCurrentConnectionsNumberKey(currentTime int) string {
+	return fmt.Sprintf("%s_%d", CurrentConnectionsNumberKey, currentTime)
+}
+
+
+// GetConnectionsNumberKey ...
+func GetConnectionsNumberKey(moment int) string {
+	return fmt.Sprintf("%s_%d", ConnectionsNumberPrefix, moment)
+}
+
+
+var storage *Storage
+var once sync.Once
+
+// Storage dump the block info into leveldb.
+type Storage struct {
+	db *ethdb.LDBDatabase
+}
+
+// GetStorageInstance returns attack model by using singleton pattern.
+func GetStorageInstance(ip, port string, remove bool) *Storage {
+	once.Do(func() {
+		storage = &Storage{}
+		storage.Init(ip, port, remove)
+	})
+	return storage
+}
+
+// Init initializes connections number storage.
+func (storage *Storage) Init(ip, port string, remove bool) {
+	dbFileName := "/tmp/monitoring_service_storage_" + ip + "_" + port
+	var err error
+	if remove {
+		var err = os.RemoveAll(dbFileName)
+		if err != nil {
+			utils.Logger().Error().Err(err).Msg("Failed to remove existing database files")
+		}
+	}
+	if storage.db, err = ethdb.NewLDBDatabase(dbFileName, 0, 0); err != nil {
+		utils.Logger().Error().Err(err).Msg("Failed to create new database")
+	}
+}
+
+// GetDB returns the LDBDatabase of the storage.
+func (storage *Storage) GetDB() *ethdb.LDBDatabase {
+	return storage.db
+}
+
+// Dump get time and current connections number and index them into lvdb for monitoring service.
+func (storage *Storage) Dump(connectionsNumber int, currentTime int) {
+	utils.Logger().Info().Int("Unix Time", currentTime).Msg("Store current connections number")
+	if block == nil {
+		return
+	}
+
+	batch := storage.db.NewBatch()
+	// Update current connections number.
+	if err := batch.Put([]byte(CurrentConnectionsNumberKey), []byte(strconv.Itoa(connectionsNumber))); err != nil {
+		utils.Logger().Warn().Err(err).Msg("cannot batch current connections number")
+	}
+
+	// Store connections number for current time.
+	connectionsNumberData, err := rlp.EncodeToBytes(connectionsNumber)
+	if err == nil {
+		if err := batch.Put([]byte(GetConnectionsNumberKey(currentTime)), connectionsNumberData); err != nil {
+			utils.Logger().Warn().Err(err).Msg("cannot batch connections number")
+		}
+	} else {
+		utils.Logger().Error().Err(err).Msg("Failed to serialize connections number")
+	}
+
+	if err := batch.Write(); err != nil {
+		ctxerror.Warn(utils.GetLogger(), err, "cannot write batch")
+	}
+}
