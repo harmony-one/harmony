ecc247357909e40f15cef24bf770ebaf4a8d5a58
flicker-harmony <vlad@harmony.one>
2019-07-26 19:17:56 +0300

    Add HTTP

--- api/service/monitoringservice/service.go
+++ api/service/monitoringservice/service.go
@@ -2,10 +2,15 @@ package monitoringservice
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net"
+	"time"
 	"strconv"
+	"net/http"
 
+
+	"github.com/gorilla/mux"
 	"google.golang.org/grpc"
 	"github.com/ethereum/go-ethereum/rpc"
 	libp2p_peer "github.com/libp2p/go-libp2p-peer"
@@ -18,16 +23,28 @@ import (
 // Constants for monitoring service.
 const (
 	monitoringServicePortDifference = 900
+	monitoringServiceHTTPPortDifference = 2000
 )
 
 // Service is the struct for monitoring service.
 type Service struct {
+	router            *mux.Router
 	IP                string
 	Port              string
 	GetNodeIDs        func() []libp2p_peer.ID
 	storage           *utils.MetricsStorage
 	server            *grpc.Server
+	httpServer        *http.Server
 	messageChan       chan *msg_pb.Message
+}
+
+type ConnectionsLog struct {
+	Time int
+	ConnectionsNumber int
+}
+
+type ConnectionsStatsHTTP struct {
+	ConnectionsLogs []ConnectionsLog
 }
 
 // New returns monitoring service.
@@ -41,8 +58,9 @@ func New(selfPeer *p2p.Peer, GetNodeIDs func() []libp2p_peer.ID) *Service {
 
 // StartService starts monitoring service.
 func (s *Service) StartService() {
-	utils.Logger().Info().Msg("Starting explorer service.")
+	utils.Logger().Info().Msg("Starting monitoring service.")
 	s.Run(true)
+	s.httpServer = s.RunHTTP()
 }
 
 // StopService shutdowns monitoring service.
@@ -59,6 +77,37 @@ func GetMonitoringSerivcePort(nodePort string) string {
 	utils.Logger().Error().Msg("error on parsing.")
 	return ""
 }
+
+// GetMonitoringServiceHTTPPort returns the port serving monitorign service dashboard. This port is monitoringServicePortDifference less than the node port.
+func GetMonitoringServiceHTTPPort(nodePort string) string {
+	if port, err := strconv.Atoi(nodePort); err == nil {
+		return fmt.Sprintf("%d", port-monitoringServiceHTTPPortDifference)
+	}
+	utils.Logger().Error().Msg("error on parsing.")
+	return ""
+}
+
+// Run is to run http serving monitoring service.
+func (s *Service) RunHTTP() *http.Server {
+	// Init address.
+	addr := net.JoinHostPort("", GetMonitoringServiceHTTPPort(s.Port))
+
+	s.router = mux.NewRouter()
+	// Set up router for connections stats.
+	s.router.Path("/connectionsstats").Queries("since", "{[0-9]*?}", "until", "{[0-9]*?}").HandlerFunc(s.GetConnectionsStats).Methods("GET")
+	s.router.Path("/connectionsstats").HandlerFunc(s.GetConnectionsStats)
+
+	// Do serving now.
+	utils.Logger().Info().Str("port", GetMonitoringServiceHTTPPort(s.Port)).Msg("Listening")
+	httpServer := &http.Server{Addr: addr, Handler: s.router}
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil {
+			utils.Logger().Warn().Err(err).Msg("httpServer.ListenAndServe()")
+		}
+	}()
+	return httpServer
+}
+
 
 
 // Run is to run serving monitoring service.
@@ -88,9 +137,9 @@ func (s *Service) Process(ctx context.Context, request *Request) (*Response, err
 	if request.GetMetricsType() != MetricsType_CONNECTIONS_STATS {
 		return &Response{}, nil
 	}
-	connectionsStatsRequest := request.GetConnectionsStatsRequest()
-	since := int(connectionsStatsRequest.GetSince())
-	until := int(connectionsStatsRequest.GetUntil())
+	ConnectionsStatsRequest := request.GetConnectionsStatsRequest()
+	since := int(ConnectionsStatsRequest.GetSince())
+	until := int(ConnectionsStatsRequest.GetUntil())
 
 	connectionsNumbers := s.storage.ReadConnectionsNumbersFromDB(since, until)
 	parsedConnectionsStats := []*ConnectionsStats{}
@@ -110,36 +159,30 @@ func (s *Service) Process(ctx context.Context, request *Request) (*Response, err
 }
 
 
-// For rpc/http later
-// GetMonitoringServiceConnectionsNumber serves end-point /connectionsNumber
-/*func (s *Service) GetMonitoringSerivceConnectionsNumber(w http.ResponseWriter, r *http.Request) {
+// For http/rpc
+// GetConnectionsLog serves end-point /ConnectionsLog
+func (s *Service) GetConnectionsStats(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	since := r.FormValue("since")
 	until := r.FormValue("until")
 
-	data := &Data{
-		ConnectionsNumbers: []*ConnectionLog,
-	}
+	connectionsLogs := []*ConnectionsLog{}
 	defer func() {
-		if err := json.NewEncoder(w).Encode(data.Connections); err != nil {
-			utils.Logger().Warn().Err(err).Msg("cannot JSON-encode connections")
+		if err := json.NewEncoder(w).Encode(connectionsLogs); err != nil {
+			utils.Logger().Warn().Err(err).Msg("cannot JSON-encode connections logs")
 		}
 	}()
-	var sinceInt int
 	var err error
-	if (since == "") {
-		since = 0, err = nil
-	} else {
+	sinceInt := 0
+	if (since != "") {
 		sinceInt, err = strconv.Atoi(since)
 	}
 	if err != nil {
 		utils.Logger().Warn().Err(err).Str("since", since).Msg("invalid since parameter")
 		return
 	}
-	var untilInt int
-	if until == "" {
-		untilInt = time.Now().Unix()
-	} else {
+	untilInt := int(time.Now().Unix())
+	if until != "" {
 		untilInt, err = strconv.Atoi(until)
 	}
 	if err != nil {
@@ -147,13 +190,13 @@ func (s *Service) Process(ctx context.Context, request *Request) (*Response, err
 		return
 	}
 
-	connectionsNumbers := s.ReadConnectionsNumbersDB(sinceInt, untilInt)
+	connectionsNumbers := s.storage.ReadConnectionsNumbersFromDB(sinceInt, untilInt)
 	for currentTime, connectionsNumber := range connectionsNumbers {
-		data.ConnectionsNumbers = append(data.ConnectionsNumbers, ConnectionsLog{Time: currentTime, ConnectionsNumber: connectionsNumber})
+		connectionsLogs = append(connectionsLogs, &ConnectionsLog{Time: currentTime, ConnectionsNumber: connectionsNumber})
 	}
 
 	return
-}*/
+}
 
 // NotifyService notify service
 func (s *Service) NotifyService(params map[string]interface{}) {
