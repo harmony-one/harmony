b38f5d7c861082576ecdf80946a89649b662fbdd
Leo Chen <leo@harmony.one>
2019-07-29 22:21:16 -0700

    [epoch] add 4 more FN keys for epoch1 on mainnet
    Signed-off-by: Leo Chen <leo@harmony.one>

--- internal/configs/sharding/mainnet.go
+++ internal/configs/sharding/mainnet.go
@@ -55,7 +55,7 @@ func (ms mainnetSchedule) IsLastBlock(blockNum uint64) bool {
 
 var mainnetReshardingEpoch = []*big.Int{big.NewInt(0), big.NewInt(mainnetV1Epoch)}
 var mainnetV0 = MustNewInstance(4, 150, 112, genesis.HarmonyAccounts, genesis.FoundationalNodeAccounts, mainnetReshardingEpoch)
-var mainnetV1 = MustNewInstance(4, 151, 112, genesis.HarmonyAccounts, genesis.FoundationalNodeAccountsV1, mainnetReshardingEpoch)
+var mainnetV1 = MustNewInstance(4, 152, 112, genesis.HarmonyAccounts, genesis.FoundationalNodeAccountsV1, mainnetReshardingEpoch)
 
 //var mainnetV2 = MustNewInstance(8, 200, 100)
 //var mainnet6400 = MustNewInstance(16, 400, 50)
