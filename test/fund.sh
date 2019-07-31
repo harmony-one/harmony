1d7f5b22bf45a71ec329c4b200f9b9789b8ff565
Minh Doan <minh@harmony.one>
2019-07-31 00:47:11 -0700

    add fund script to fund testing account

---
+++ test/fund.sh
@@ -0,0 +1,9 @@
+sleep 120
+./bin/wallet -p local transfer --from one1pdv9lrdwl0rg5vglh4xtyrv3wjk3wsqket7zxy --to 0x1a3e7a44ee21101d7D64FBf29B0F6F1fc295F723 --shardID 0 --amount 30
+./bin/wallet -p local transfer --from one12fuf7x9rgtdgqg7vgq0962c556m3p7afsxgvll --to 0x10A02A0a6e95a676AE23e2db04BEa3D1B8b7ca2E --shardID 0 --amount 30
+./bin/wallet -p local transfer --from one1pf75h0t4am90z8uv3y0dgunfqp4lj8wr3t5rsp --to 0x3e881F6C36A3A14a2D1816b0A5471d1caBB16F33 --shardID 0 --amount 30
+./bin/wallet -p local transfer --from one1spshr72utf6rwxseaz339j09ed8p6f8ke370zj --to 0x9d72989b68777a1f3FfD6F1DB079f1928373eE52 --shardID 0 --amount 30
+./bin/wallet -p local transfer --from one1d2rngmem4x2c6zxsjjz29dlah0jzkr0k2n88wc --to 0x67957240b6eB045E17B47dcE98102f09aaC03435 --shardID 0 --amount 30
+./bin/wallet -p local transfer --from one1a50tun737ulcvwy0yvve0pvu5skq0kjargvhwe --to 0xf70fBDB1AD002baDF19024785b1a4bf6F841F558 --shardID 0 --amount 30
+./bin/wallet -p local transfer --from one103q7qe5t2505lypvltkqtddaef5tzfxwsse4z7 --to 0x3f1A559be93C9456Ca75712535Fd522f5EC22c6B --shardID 0 --amount 30
+
