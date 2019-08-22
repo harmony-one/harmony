ef6ce5fe9caee0f7ae6368a72420d4d085aa5693
Edgar Aroutiounian <edgar.factorial@gmail.com>
2019-08-21 17:55:00 -0700

    [hmy_api] Call hmy_getBalance via RPC on localhost

--- cmd/client/hmy_cli/lib/query.go
+++ cmd/client/hmy_cli/lib/query.go
@@ -1,22 +1,57 @@
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"strings"
+	"io/ioutil"
+	"net/http"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
-func init() {
-	rootCmd.AddCommand(cmdTimes)
-}
+const (
+	JSON_RPC_VERSION = "2.0"
+)
 
-var cmdTimes = &cobra.Command{
-	Use:   "account",
-	Short: "Query account balance",
-	Long:  `Query account balances`,
-	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(strings.Join(args, " "))
-	},
+var (
+	node     string
+	queryID  = 0
+	cmdQuery = &cobra.Command{
+		Use:   "account",
+		Short: "Query account balance",
+		Long:  `Query account balances`,
+		Run: func(cmd *cobra.Command, args []string) {
+			requestBody, _ := json.Marshal(map[string]interface{}{
+				"jsonrpc": JSON_RPC_VERSION,
+				"id":      strconv.Itoa(queryID),
+				"method":  "hmy_getBalance",
+				"params":  [...]string{"0xD7Ff41CA29306122185A07d04293DdB35F24Cf2d", "latest"},
+			})
+
+			resp, err := http.Post(node, "application/json", bytes.NewBuffer(requestBody))
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println("error")
+			}
+			fmt.Println(string(body))
+		},
+	}
+)
+
+func init() {
+	cmdQuery.Flags().StringVarP(
+		&node,
+		"node",
+		"",
+		"http://localhost:9500",
+		"<host>:<port>",
+	)
+	rootCmd.AddCommand(cmdQuery)
 }
