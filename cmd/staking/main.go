a990431ecbeede90a45bf1e8bca25c7a70823680
Edgar Aroutiounian <edgar.factorial@gmail.com>
2019-10-02 16:25:55 -0700

    [staking] Satisfy jenkins build in temp CLI staking-iterate

--- cmd/staking/main.go
+++ cmd/staking/main.go
@@ -13,6 +13,10 @@ var (
 )
 
 func main() {
+	if os.Args[1] == "-version" {
+		fmt.Fprintf(os.Stderr, versionS()+"\n")
+		os.Exit(0)
+	}
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
