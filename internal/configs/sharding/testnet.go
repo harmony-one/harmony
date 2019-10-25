c8968d695122bc318c13d353ff5607f8f4363a21
Eugene Kim <ek@harmony.one>
2019-10-25 12:26:56 -0700

    Resize testnet to 3 shards * 100 nodes/shard

--- internal/configs/sharding/testnet.go
+++ internal/configs/sharding/testnet.go
@@ -15,9 +15,6 @@ var TestnetSchedule testnetSchedule
 type testnetSchedule struct{}
 
 const (
-	testnetV1Epoch = 1
-	testnetV2Epoch = 2
-
 	// 10 minutes per epoch (at 8s/block)
 	testnetBlocksPerEpoch = 75
 
@@ -38,10 +35,6 @@ const (
 
 func (testnetSchedule) InstanceForEpoch(epoch *big.Int) Instance {
 	switch {
-	case epoch.Cmp(big.NewInt(testnetV2Epoch)) >= 0:
-		return testnetV2
-	case epoch.Cmp(big.NewInt(testnetV1Epoch)) >= 0:
-		return testnetV1
 	default: // genesis
 		return testnetV0
 	}
@@ -125,8 +118,8 @@ func (ts testnetSchedule) GetShardingStructure(numShard, shardID int) []map[stri
 	return genShardingStructure(numShard, shardID, TestNetHTTPPattern, TestNetWSPattern)
 }
 
-var testnetReshardingEpoch = []*big.Int{big.NewInt(0), big.NewInt(testnetV1Epoch), big.NewInt(testnetV2Epoch)}
+var testnetReshardingEpoch = []*big.Int{
+	big.NewInt(0),
+}
 
-var testnetV0 = MustNewInstance(2, 150, 150, genesis.TNHarmonyAccounts, genesis.TNFoundationalAccounts, testnetReshardingEpoch)
-var testnetV1 = MustNewInstance(2, 160, 150, genesis.TNHarmonyAccounts, genesis.TNFoundationalAccounts, testnetReshardingEpoch)
-var testnetV2 = MustNewInstance(2, 170, 150, genesis.TNHarmonyAccounts, genesis.TNFoundationalAccounts, testnetReshardingEpoch)
+var testnetV0 = MustNewInstance(3, 100, 80, genesis.TNHarmonyAccounts, genesis.TNFoundationalAccounts, testnetReshardingEpoch)
