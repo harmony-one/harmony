77a5adae013cf28ff14abc77f1e115976155221c
Edgar Aroutiounian <edgar.factorial@gmail.com>
2019-11-08 17:16:37 -0800

    [core] Remove blockchain values

--- core/values/blockchain.go
+++
@@ -1,8 +0,0 @@
-package values
-
-const (
-	// VotingPowerReduceBlockThreshold roughly corresponds to 3 hours
-	VotingPowerReduceBlockThreshold = 1350
-	// VotingPowerFullReduce roughly corresponds to 12 hours
-	VotingPowerFullReduce = 4 * VotingPowerReduceBlockThreshold
-)
