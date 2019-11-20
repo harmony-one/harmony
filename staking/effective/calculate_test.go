6c32d569e7d227007bd461e4d008888ad979cf56
janet-harmony <56005637+janet-harmony@users.noreply.github.com>
2019-11-20 15:10:20 -0800

    [staking-testing] Fix staking median logic (#1839)
    * [staking-testing] Fix staking median logic
    	* Generate random purchase data to test calculate.go
    	* Add unit test for median function
    	* Update JSON to pretty print randomly generated data for debug
    
    * [staking-testing] Provide better error message on failure
    
    * [staking-testing] Address PR comments
    
    * [staking-testing] Address goimport changes
    
    * [staking-testing] Remove commented out code & go.mod
    
    * [staking-testing] Reset go.mod
    	* Format calculate.go for lint mistakes

--- staking/effective/calculate_test.go
+++ staking/effective/calculate_test.go
@@ -4,13 +4,29 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/big"
+	"math/rand"
+	"sort"
 	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/harmony-one/bls/ffi/go/bls"
+	"github.com/harmony-one/harmony/numeric"
+	"github.com/harmony-one/harmony/shard"
 )
 
 const eposTestingFile = "epos.json"
+const slotTestingFile = "slots.json"
 
 var (
-	testingSlots slotsData
+	testingSlots     slotsData
+	testingPurchases Slots
+	maxAccountGen    = int64(98765654323123134)
+	accountGen       = rand.New(rand.NewSource(1337))
+	maxKeyGen        = int64(98765654323123134)
+	keyGen           = rand.New(rand.NewSource(42))
+	maxStakeGen      = int64(200)
+	stakeGen         = rand.New(rand.NewSource(541))
 )
 
 type slotsData struct {
@@ -33,15 +49,45 @@ func init() {
 		fmt.Println(oops.Error())
 		panic("Could not unmarshal slots data into memory")
 	}
+	testingPurchases = generateRandomSlots(20)
+}
+
+func generateRandomSlots(num int) Slots {
+	randomSlots := Slots{}
+	for i := 0; i < num; i++ {
+		addr := common.Address{}
+		addr.SetBytes(big.NewInt(int64(accountGen.Int63n(maxAccountGen))).Bytes())
+		secretKey := bls.SecretKey{}
+		secretKey.Deserialize(big.NewInt(int64(keyGen.Int63n(maxKeyGen))).Bytes())
+		key := shard.BlsPublicKey{}
+		key.FromLibBLSPublicKey(secretKey.GetPublicKey())
+		stake := numeric.NewDecFromBigInt(big.NewInt(int64(stakeGen.Int63n(maxStakeGen))))
+		randomSlots = append(randomSlots, SlotPurchase{addr, key, stake})
+	}
+	return randomSlots
 }
 
 func TestMedian(t *testing.T) {
-	//
+	copyPurchases := append([]SlotPurchase{}, testingPurchases...)
+	sort.SliceStable(copyPurchases,
+		func(i, j int) bool { return copyPurchases[i].Dec.LTE(copyPurchases[j].Dec) })
+	numPurchases := len(copyPurchases) / 2
+	expectedResult := numeric.ZeroDec()
+	if len(copyPurchases)%2 == 0 {
+		expectedResult = copyPurchases[numPurchases-1].Dec.Add(copyPurchases[numPurchases].Dec).Quo(two)
+	} else {
+		expectedResult = copyPurchases[numPurchases].Dec
+	}
+	med := median(testingPurchases)
+	if !med.Equal(expectedResult) {
+		t.Errorf("Expected: %s, Got: %s", expectedResult.String(), med.String())
+	}
 }
 
 func TestEffectiveStake(t *testing.T) {
 	//
 }
+
 func TestApply(t *testing.T) {
 	//
 }
