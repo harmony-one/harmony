88dbc40437c4a315f91d6076c227eb3dbc7d1861
Edgar Aroutiounian <edgar.factorial@gmail.com>
2019-12-01 17:09:34 -0800

    [reward][schedule] Make denom a function of time block rather than fixed 12.6 billion

--- consensus/reward/schedule.go
+++ consensus/reward/schedule.go
@@ -1,91 +1,142 @@
 package reward
 
-import "github.com/harmony-one/harmony/numeric"
+import (
+	"sort"
+	"time"
 
-var (
-	// Schedule is the Token Release Schedule of Harmony
-	Schedule = map[int]numeric.Dec{
-		0:   numeric.ZeroDec(),
-		1:   numeric.MustNewDecFromStr("24.29"),
-		2:   numeric.MustNewDecFromStr("24.88"),
-		3:   numeric.MustNewDecFromStr("25.47"),
-		4:   numeric.MustNewDecFromStr("26.07"),
-		5:   numeric.MustNewDecFromStr("26.66"),
-		6:   numeric.MustNewDecFromStr("27.26"),
-		7:   numeric.MustNewDecFromStr("37.85"),
-		8:   numeric.MustNewDecFromStr("38.44"),
-		9:   numeric.MustNewDecFromStr("39.51"),
-		10:  numeric.MustNewDecFromStr("40.47"),
-		11:  numeric.MustNewDecFromStr("41.43"),
-		12:  numeric.MustNewDecFromStr("42.39"),
-		14:  numeric.MustNewDecFromStr("53.35"),
-		15:  numeric.MustNewDecFromStr("54.31"),
-		100: numeric.MustNewDecFromStr("55.27"),
-		100: numeric.MustNewDecFromStr("56.23"),
-		100: numeric.MustNewDecFromStr("57.19"),
-		100: numeric.MustNewDecFromStr("58.15"),
-		100: numeric.MustNewDecFromStr("68.82"),
-		100: numeric.MustNewDecFromStr("69.48"),
-		100: numeric.MustNewDecFromStr("70.15"),
-		100: numeric.MustNewDecFromStr("70.82"),
-		100: numeric.MustNewDecFromStr("71.48"),
-		100: numeric.MustNewDecFromStr("72.15"),
-		100: numeric.MustNewDecFromStr("77.21"),
-		100: numeric.MustNewDecFromStr("77.88"),
-		100: numeric.MustNewDecFromStr("78.55"),
-		100: numeric.MustNewDecFromStr("79.22"),
-		100: numeric.MustNewDecFromStr("79.88"),
-		100: numeric.MustNewDecFromStr("80.55"),
-		100: numeric.MustNewDecFromStr("85.61"),
-		100: numeric.MustNewDecFromStr("86.28"),
-		100: numeric.MustNewDecFromStr("86.95"),
-		100: numeric.MustNewDecFromStr("87.61"),
-		100: numeric.MustNewDecFromStr("88.28"),
-		100: numeric.MustNewDecFromStr("88.95"),
-		100: numeric.MustNewDecFromStr("89.62"),
-		100: numeric.MustNewDecFromStr("90.28"),
-		100: numeric.MustNewDecFromStr("90.95"),
-		100: numeric.MustNewDecFromStr("91.62"),
-		100: numeric.MustNewDecFromStr("92.28"),
-		100: numeric.MustNewDecFromStr("92.95"),
-		100: numeric.MustNewDecFromStr("93.62"),
-		100: numeric.MustNewDecFromStr("94.29"),
-		100: numeric.MustNewDecFromStr("94.48"),
-		100: numeric.MustNewDecFromStr("94.68"),
-		100: numeric.MustNewDecFromStr("94.88"),
-		100: numeric.MustNewDecFromStr("95.07"),
-		100: numeric.MustNewDecFromStr("95.27"),
-		100: numeric.MustNewDecFromStr("95.47"),
-		100: numeric.MustNewDecFromStr("95.67"),
-		100: numeric.MustNewDecFromStr("95.86"),
-		100: numeric.MustNewDecFromStr("96.06"),
-		100: numeric.MustNewDecFromStr("96.26"),
-		100: numeric.MustNewDecFromStr("96.45"),
-		100: numeric.MustNewDecFromStr("96.65"),
-		100: numeric.MustNewDecFromStr("96.85"),
-		100: numeric.MustNewDecFromStr("97.04"),
-		100: numeric.MustNewDecFromStr("97.24"),
-		100: numeric.MustNewDecFromStr("97.44"),
-		100: numeric.MustNewDecFromStr("97.64"),
-		100: numeric.MustNewDecFromStr("97.83"),
-		100: numeric.MustNewDecFromStr("98.03"),
-		100: numeric.MustNewDecFromStr("98.23"),
-		100: numeric.MustNewDecFromStr("98.42"),
-		100: numeric.MustNewDecFromStr("98.62"),
-		100: numeric.MustNewDecFromStr("98.82"),
-		100: numeric.MustNewDecFromStr("99.01"),
-		100: numeric.MustNewDecFromStr("99.21"),
-		100: numeric.MustNewDecFromStr("99.41"),
-		100: numeric.MustNewDecFromStr("99.61"),
-		100: numeric.MustNewDecFromStr("99.80"),
-		100: numeric.MustNewDecFromStr("100.00"),
-	}
+	"github.com/harmony-one/harmony/internal/utils"
+	"github.com/harmony-one/harmony/numeric"
 )
 
-// PercentageForTimeStamp ..
-func PercentageForTimeStamp(uint32 timestamp) numeric.Dec {
-	for key, _ := range Schedule {
-		//
+type pair struct {
+	int64
+	numeric.Dec
+}
+
+var (
+
+	// schedule is the Token Release Schedule of Harmony
+	releasePlan = map[int64]numeric.Dec{
+		//2019
+		mustParse("2019-May-31"): numeric.MustNewDecFromStr("0.2429"),
+		mustParse("2019-Jun-30"): numeric.MustNewDecFromStr("0.2488"),
+		mustParse("2019-Jul-31"): numeric.MustNewDecFromStr("0.2547"),
+		mustParse("2019-Aug-31"): numeric.MustNewDecFromStr("0.2607"),
+		mustParse("2019-Sep-30"): numeric.MustNewDecFromStr("0.2666"),
+		mustParse("2019-Oct-31"): numeric.MustNewDecFromStr("0.2726"),
+		mustParse("2019-Nov-30"): numeric.MustNewDecFromStr("0.3785"),
+		mustParse("2019-Dec-31"): numeric.MustNewDecFromStr("0.3844"),
+		// 2020
+		mustParse("2020-Jan-31"): numeric.MustNewDecFromStr("0.3951"),
+		mustParse("2020-Feb-29"): numeric.MustNewDecFromStr("0.4047"),
+		mustParse("2020-Mar-31"): numeric.MustNewDecFromStr("0.4143"),
+		mustParse("2020-Apr-30"): numeric.MustNewDecFromStr("0.4239"),
+		mustParse("2020-May-31"): numeric.MustNewDecFromStr("0.5335"),
+		mustParse("2020-Jun-30"): numeric.MustNewDecFromStr("0.5431"),
+		mustParse("2020-Jul-31"): numeric.MustNewDecFromStr("0.5527"),
+		mustParse("2020-Aug-31"): numeric.MustNewDecFromStr("0.5623"),
+		mustParse("2020-Sep-30"): numeric.MustNewDecFromStr("0.5719"),
+		mustParse("2020-Oct-31"): numeric.MustNewDecFromStr("0.5815"),
+		mustParse("2020-Nov-30"): numeric.MustNewDecFromStr("0.6882"),
+		mustParse("2020-Dec-31"): numeric.MustNewDecFromStr("0.6948"),
+		// 2021
+		mustParse("2021-Jan-31"): numeric.MustNewDecFromStr("0.7015"),
+		mustParse("2021-Feb-28"): numeric.MustNewDecFromStr("0.7082"),
+		mustParse("2021-Mar-31"): numeric.MustNewDecFromStr("0.7148"),
+		mustParse("2021-Apr-30"): numeric.MustNewDecFromStr("0.7215"),
+		mustParse("2021-May-31"): numeric.MustNewDecFromStr("0.7721"),
+		mustParse("2021-Jun-30"): numeric.MustNewDecFromStr("0.7788"),
+		mustParse("2021-Jul-31"): numeric.MustNewDecFromStr("0.7855"),
+		mustParse("2021-Aug-31"): numeric.MustNewDecFromStr("0.7922"),
+		mustParse("2021-Sep-30"): numeric.MustNewDecFromStr("0.7988"),
+		mustParse("2021-Oct-31"): numeric.MustNewDecFromStr("0.8055"),
+		mustParse("2021-Nov-30"): numeric.MustNewDecFromStr("0.8561"),
+		mustParse("2021-Dec-31"): numeric.MustNewDecFromStr("0.8628"),
+		// 2022
+		mustParse("2022-Jan-31"): numeric.MustNewDecFromStr("0.8695"),
+		mustParse("2022-Feb-28"): numeric.MustNewDecFromStr("0.8761"),
+		mustParse("2022-Mar-31"): numeric.MustNewDecFromStr("0.8828"),
+		mustParse("2022-Apr-30"): numeric.MustNewDecFromStr("0.8895"),
+		mustParse("2022-May-31"): numeric.MustNewDecFromStr("0.8962"),
+		mustParse("2022-Jun-30"): numeric.MustNewDecFromStr("0.9028"),
+		mustParse("2022-Jul-31"): numeric.MustNewDecFromStr("0.9095"),
+		mustParse("2022-Aug-31"): numeric.MustNewDecFromStr("0.9162"),
+		mustParse("2022-Sep-30"): numeric.MustNewDecFromStr("0.9228"),
+		mustParse("2022-Oct-31"): numeric.MustNewDecFromStr("0.9295"),
+		mustParse("2022-Nov-30"): numeric.MustNewDecFromStr("0.9362"),
+		mustParse("2022-Dec-31"): numeric.MustNewDecFromStr("0.9429"),
+		// 2023
+		mustParse("2023-Jan-31"): numeric.MustNewDecFromStr("0.9448"),
+		mustParse("2023-Feb-28"): numeric.MustNewDecFromStr("0.9468"),
+		mustParse("2023-Mar-31"): numeric.MustNewDecFromStr("0.9488"),
+		mustParse("2023-Apr-30"): numeric.MustNewDecFromStr("0.9507"),
+		mustParse("2023-May-31"): numeric.MustNewDecFromStr("0.9527"),
+		mustParse("2023-Jun-30"): numeric.MustNewDecFromStr("0.9547"),
+		mustParse("2023-Jul-31"): numeric.MustNewDecFromStr("0.9567"),
+		mustParse("2023-Aug-31"): numeric.MustNewDecFromStr("0.9586"),
+		mustParse("2023-Sep-30"): numeric.MustNewDecFromStr("0.9606"),
+		mustParse("2023-Oct-31"): numeric.MustNewDecFromStr("0.9626"),
+		mustParse("2023-Nov-30"): numeric.MustNewDecFromStr("0.9645"),
+		mustParse("2023-Dec-31"): numeric.MustNewDecFromStr("0.9665"),
+		// 2024
+		mustParse("2024-Jan-31"): numeric.MustNewDecFromStr("0.9685"),
+		mustParse("2024-Feb-29"): numeric.MustNewDecFromStr("0.9704"),
+		mustParse("2024-Mar-31"): numeric.MustNewDecFromStr("0.9724"),
+		mustParse("2024-Apr-30"): numeric.MustNewDecFromStr("0.9744"),
+		mustParse("2024-May-31"): numeric.MustNewDecFromStr("0.9764"),
+		mustParse("2024-Jun-30"): numeric.MustNewDecFromStr("0.9783"),
+		mustParse("2024-Jul-31"): numeric.MustNewDecFromStr("0.9803"),
+		mustParse("2024-Aug-31"): numeric.MustNewDecFromStr("0.9823"),
+		mustParse("2024-Sep-30"): numeric.MustNewDecFromStr("0.9842"),
+		mustParse("2024-Oct-31"): numeric.MustNewDecFromStr("0.9862"),
+		mustParse("2024-Nov-30"): numeric.MustNewDecFromStr("0.9882"),
+		mustParse("2024-Dec-31"): numeric.MustNewDecFromStr("0.9901"),
+		// 2025
+		mustParse("2025-Jan-31"): numeric.MustNewDecFromStr("0.9921"),
+		mustParse("2025-Feb-28"): numeric.MustNewDecFromStr("0.9941"),
+		mustParse("2025-Mar-31"): numeric.MustNewDecFromStr("0.9961"),
+		mustParse("2025-Apr-30"): numeric.MustNewDecFromStr("0.9980"),
+		mustParse("2025-May-30"): numeric.MustNewDecFromStr("1.0000"),
 	}
-	return Schedule[0]
+	sorted = func() []pair {
+		s := []pair{}
+		for k, v := range releasePlan {
+			s = append(s, pair{k, v})
+		}
+		sort.SliceStable(
+			s,
+			func(i, j int) bool { return s[i].int64 < s[j].int64 },
+		)
+		return s
+	}()
+)
+
+func mustParse(ts string) int64 {
+	const shortForm = "2006-Jan-02"
+	t, err := time.Parse(shortForm, ts)
+	if err != nil {
+		panic("could not parse timestamp")
+	}
+	return t.UnixNano()
+}
+
+// PercentageForTimeStamp ..
+func PercentageForTimeStamp(ts int64) numeric.Dec {
+	bucket := numeric.ZeroDec()
+
+	for i := range sorted {
+		if value := time.Unix(
+			sorted[i].int64, 0,
+		); value.After(time.Unix(ts, 0)) {
+			bucket = sorted[i].Dec
+			break
+		}
+	}
+
+	utils.Logger().Info().
+		Str("total-supply", bucket.Mul(numeric.NewDec(100)).String()).
+		Str("time", time.Unix(ts, 0).String()).
+		Msg("Picked Percentage for timestmap")
+
+	return bucket
 }
