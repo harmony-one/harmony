harmony-one/harmony

commit e79ba5fe880b56973a50a80d918b9145fce07cf0

Make shard chain follow beacon chain's epoch after stakingEpoch

block/factory/factory.go
@@ -30,11 +30,11 @@ func NewFactory(chainConfig *params.ChainConfig) Factory {
 func (f *factory) NewHeader(epoch *big.Int) *block.Header {
 	var impl blockif.Header
 	switch {
-	case epoch.Cmp(f.chainConfig.StakingEpoch) >= 0:
+	case f.chainConfig.IsPreStaking(epoch):
 		impl = v3.NewHeader()
-	case epoch.Cmp(f.chainConfig.CrossLinkEpoch) >= 0:
+	case f.chainConfig.IsCrossLink(epoch):
 		impl = v2.NewHeader()
-	case epoch.Cmp(f.chainConfig.CrossTxEpoch) >= 0:
+	case f.chainConfig.IsCrossTx(epoch):
 		impl = v1.NewHeader()
 	default:
 		impl = v0.NewHeader()

internal/params/config.go
@@ -23,33 +23,36 @@ var EpochTBD = big.NewInt(10000000)
 var (
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
-		ChainID:        MainnetChainID,
-		CrossTxEpoch:   big.NewInt(28),
-		CrossLinkEpoch: EpochTBD,
-		StakingEpoch:   EpochTBD,
-		EIP155Epoch:    big.NewInt(28),
-		S3Epoch:        big.NewInt(28),
+		ChainID:         MainnetChainID,
+		CrossTxEpoch:    big.NewInt(28),
+		CrossLinkEpoch:  EpochTBD,
+		StakingEpoch:    EpochTBD,
+		PreStakingEpoch: EpochTBD,
+		EIP155Epoch:     big.NewInt(28),
+		S3Epoch:         big.NewInt(28),
 	}
 
 	// TestnetChainConfig contains the chain parameters to run a node on the harmony test network.
 	TestnetChainConfig = &ChainConfig{
-		ChainID:        TestnetChainID,
-		CrossTxEpoch:   big.NewInt(0),
-		CrossLinkEpoch: big.NewInt(0),
-		StakingEpoch:   big.NewInt(3),
-		EIP155Epoch:    big.NewInt(0),
-		S3Epoch:        big.NewInt(0),
+		ChainID:         TestnetChainID,
+		CrossTxEpoch:    big.NewInt(0),
+		CrossLinkEpoch:  big.NewInt(2),
+		StakingEpoch:    big.NewInt(3),
+		PreStakingEpoch: big.NewInt(0),
+		EIP155Epoch:     big.NewInt(0),
+		S3Epoch:         big.NewInt(0),
 	}
 
 	// PangaeaChainConfig contains the chain parameters for the Pangaea network.
 	// All features except for CrossLink are enabled at launch.
 	PangaeaChainConfig = &ChainConfig{
-		ChainID:        PangaeaChainID,
-		CrossTxEpoch:   big.NewInt(0),
-		CrossLinkEpoch: EpochTBD,
-		StakingEpoch:   EpochTBD,
-		EIP155Epoch:    big.NewInt(0),
-		S3Epoch:        big.NewInt(0),
+		ChainID:         PangaeaChainID,
+		CrossTxEpoch:    big.NewInt(0),
+		CrossLinkEpoch:  EpochTBD,
+		StakingEpoch:    EpochTBD,
+		PreStakingEpoch: EpochTBD,
+		EIP155Epoch:     big.NewInt(0),
+		S3Epoch:         big.NewInt(0),
 	}
 
 	// AllProtocolChanges ...
@@ -60,6 +63,7 @@ var (
 		big.NewInt(0),             // CrossTxEpoch
 		big.NewInt(0),             // CrossLinkEpoch
 		big.NewInt(0),             // StakingEpoch
+		big.NewInt(0),             // PreStakingEpoch
 		big.NewInt(0),             // EIP155Epoch
 		big.NewInt(0),             // S3Epoch
 	}
@@ -72,6 +76,7 @@ var (
 		big.NewInt(0), // CrossTxEpoch
 		big.NewInt(0), // CrossLinkEpoch
 		big.NewInt(0), // StakingEpoch
+		big.NewInt(0), // PreStakingEpoch
 		big.NewInt(0), // EIP155Epoch
 		big.NewInt(0), // S3Epoch
 	}
@@ -108,8 +113,11 @@ type ChainConfig struct {
 	// cross-shard links.
 	CrossLinkEpoch *big.Int `json:"crossLinkEpoch,omitempty"`
 
-	// StakingEpoch is the epoch we allow staking transactions
+	// StakingEpoch is the epoch when shard assign takes staking into account
 	StakingEpoch *big.Int `json:"stakingEpoch,omitempty"`
+
+	// PreStakingEpoch is the epoch we allow staking transactions
+	PreStakingEpoch *big.Int `json:"preStakingEpoch,omitempty"`
 
 	EIP155Epoch *big.Int `json:"eip155Epoch,omitempty"` // EIP155 hard fork epoch (include EIP158 too)
 	S3Epoch     *big.Int `json:"s3Epoch,omitempty"`     // S3 epoch is the first epoch containing S3 mainnet and all ethereum update up to Constantinople
@@ -149,6 +157,11 @@ func (c *ChainConfig) IsCrossTx(epoch *big.Int) bool {
 // IsStaking determines whether it is staking epoch
 func (c *ChainConfig) IsStaking(epoch *big.Int) bool {
 	return isForked(c.StakingEpoch, epoch)
+}
+
+// IsPreStaking determines whether staking transactions are allowed
+func (c *ChainConfig) IsPreStaking(epoch *big.Int) bool {
+	return isForked(c.PreStakingEpoch, epoch)
 }
 
 // IsCrossLink returns whether epoch is either equal to the CrossLink fork epoch or greater.

consensus/consensus_service.go
@@ -465,7 +465,7 @@ func (consensus *Consensus) UpdateConsensusInformation() Mode {
 		consensus.Decider.SetShardIDProvider(func() (uint32, error) {
 			return consensus.ShardID, nil
 		})
-		s, err := committee.WithStakingEnabled.Compute(
+		s, err := committee.WithStakingEnabled.ReadFromDB(
 			next, consensus.ChainReader,
 		)
 
@@ -473,12 +473,10 @@ func (consensus *Consensus) UpdateConsensusInformation() Mode {
 			utils.Logger().Error().
 				Err(err).
 				Uint32("shard", consensus.ShardID).
-				Msg("Error when computing committee with staking")
+				Msg("Error when reading staking committee")
 			return Syncing
 		}
 
-		utils.Logger().Print("XXXXXXXX")
-		utils.Logger().Print(s.FindCommitteeByID(consensus.ShardID).Slots)
 		if _, err := consensus.Decider.SetVoters(
 			s.FindCommitteeByID(consensus.ShardID).Slots,
 		); err != nil {
@@ -504,7 +502,7 @@ func (consensus *Consensus) UpdateConsensusInformation() Mode {
 	epoch := header.Epoch()
 
 	// TODO: change GetCommitteePublicKeys to read from DB
-	curShardState, err := committee.WithStakingEnabled.Compute(
+	curShardState, err := committee.WithStakingEnabled.ReadFromDB(
 		epoch, consensus.ChainReader,
 	)
 	if err != nil {
@@ -514,9 +512,11 @@ func (consensus *Consensus) UpdateConsensusInformation() Mode {
 			Msg("Error retrieving current shard state")
 		return Syncing
 	}
+
+	curCommittee := curShardState.FindCommitteeByID(header.ShardID())
 	curPubKeys := committee.WithStakingEnabled.GetCommitteePublicKeys(
-		curShardState,
-	)[int(header.ShardID())]
+		curCommittee,
+	)
 	consensus.numPrevPubKeys = len(curPubKeys)
 	consensus.getLogger().Info().Msg("[UpdateConsensusInformation] Updating.....")
 	if shard.Schedule.IsLastBlock(header.Number().Uint64()) {
@@ -525,7 +525,7 @@ func (consensus *Consensus) UpdateConsensusInformation() Mode {
 		consensus.getLogger().Info().Uint64("headerNum", header.Number().Uint64()).
 			Msg("[UpdateConsensusInformation] Epoch updated for next epoch")
 
-		nextShardState, err := committee.WithStakingEnabled.Compute(
+		nextShardState, err := committee.WithStakingEnabled.ReadFromDB(
 			new(big.Int).Add(epoch, common.Big1), consensus.ChainReader,
 		)
 		if err != nil {
@@ -536,9 +536,10 @@ func (consensus *Consensus) UpdateConsensusInformation() Mode {
 			return Syncing
 		}
 
+		nextCommittee := nextShardState.FindCommitteeByID(header.ShardID())
 		pubKeys = committee.WithStakingEnabled.GetCommitteePublicKeys(
-			nextShardState,
-		)[int(header.ShardID())]
+			nextCommittee,
+		)
 	} else {
 		consensus.SetEpochNum(epoch.Uint64())
 		pubKeys = curPubKeys

consensus/consensus_v2.go
@@ -830,6 +830,7 @@ func (consensus *Consensus) finalizeCommits() {
 
 	utils.Logger().Info().
 		Uint64("blockNum", block.NumberU64()).
+		Uint64("epochNum", block.Epoch().Uint64()).
 		Uint64("ViewId", block.Header().ViewID().Uint64()).
 		Str("blockHash", block.Hash().String()).
 		Int("index", consensus.Decider.IndexOf(consensus.PubKey)).

core/blockchain.go
@@ -28,6 +28,7 @@ import (
 	"time"
 
 	"github.com/harmony-one/harmony/crypto/bls"
+	lru "github.com/hashicorp/golang-lru"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/mclock"
@@ -48,9 +49,7 @@ import (
 	"github.com/harmony-one/harmony/internal/params"
 	"github.com/harmony-one/harmony/internal/utils"
 	"github.com/harmony-one/harmony/shard"
-	"github.com/harmony-one/harmony/shard/committee"
 	staking "github.com/harmony-one/harmony/staking/types"
-	lru "github.com/hashicorp/golang-lru"
 )
 
 var (
@@ -1959,37 +1958,6 @@ func (bc *BlockChain) GetVrfByNumber(number uint64) []byte {
 		return []byte{}
 	}
 	return header.Vrf()
-}
-
-// GetShardState returns the shard state for the given epoch,
-// creating one if needed.
-// TODO: [STAKING]
-func (bc *BlockChain) GetShardState(epoch *big.Int) (shard.State, error) {
-	shardState, err := bc.ReadShardState(epoch)
-	if err == nil { // TODO ek – distinguish ErrNotFound
-		return shardState, err
-	}
-
-	if epoch.Cmp(big.NewInt(GenesisEpoch)) == 0 {
-		shardState, err = committee.WithStakingEnabled.Compute(
-			big.NewInt(GenesisEpoch), nil,
-		)
-	} else {
-		prevEpoch := new(big.Int).Sub(epoch, common.Big1)
-		shardState, err = committee.WithStakingEnabled.ReadFromDB(
-			prevEpoch, bc,
-		)
-	}
-
-	if err != nil {
-		return nil, err
-	}
-	err = bc.WriteShardState(epoch, shardState)
-	if err != nil {
-		return nil, err
-	}
-	utils.Logger().Debug().Str("epoch", epoch.String()).Msg("saved new shard state")
-	return shardState, nil
 }
 
 // ChainDb returns the database

node/node.go
@@ -278,17 +278,20 @@ func (node *Node) addPendingTransactions(newTxs types.Transactions) {
 // Add new staking transactions to the pending staking transaction list.
 func (node *Node) addPendingStakingTransactions(newStakingTxs staking.StakingTransactions) {
 	txPoolLimit := 1000 // TODO: incorporate staking txn into TxPool
-	node.pendingStakingTxMutex.Lock()
-	for _, tx := range newStakingTxs {
-		if _, ok := node.pendingStakingTransactions[tx.Hash()]; !ok {
-			node.pendingStakingTransactions[tx.Hash()] = tx
+
+	if node.Blockchain().Config().IsPreStaking(node.Worker.GetNewEpoch()) {
+		node.pendingStakingTxMutex.Lock()
+		for _, tx := range newStakingTxs {
+			if _, ok := node.pendingStakingTransactions[tx.Hash()]; !ok {
+				node.pendingStakingTransactions[tx.Hash()] = tx
+			}
+			if len(node.pendingStakingTransactions) > txPoolLimit {
+				break
+			}
 		}
-		if len(node.pendingStakingTransactions) > txPoolLimit {
-			break
-		}
+		utils.Logger().Info().Int("length of newStakingTxs", len(newStakingTxs)).Int("totalPending", len(node.pendingStakingTransactions)).Msg("Got more staking transactions")
+		node.pendingStakingTxMutex.Unlock()
 	}
-	utils.Logger().Info().Int("length of newStakingTxs", len(newStakingTxs)).Int("totalPending", len(node.pendingStakingTransactions)).Msg("Got more staking transactions")
-	node.pendingStakingTxMutex.Unlock()
 }
 
 // AddPendingStakingTransaction staking transactions
@@ -490,8 +493,8 @@ func (node *Node) InitConsensusWithValidators() (err error) {
 		epoch, node.Consensus.ChainReader,
 	)
 	pubKeys := committee.WithStakingEnabled.GetCommitteePublicKeys(
-		shardState,
-	)[int(shardID)]
+		shardState.FindCommitteeByID(shardID),
+	)
 	if len(pubKeys) == 0 {
 		utils.Logger().Error().
 			Uint32("shardID", shardID).

node/node_handler.go
@@ -342,7 +342,12 @@ func (node *Node) PostConsensusProcessing(newBlock *types.Block, commitSigAndBit
 		node.BroadcastCXReceipts(newBlock, commitSigAndBitmap)
 	} else {
 		utils.Logger().Info().
-			Uint64("BlockNum", newBlock.NumberU64()).
+			Uint64("blockNum", newBlock.NumberU64()).
+			Uint64("epochNum", newBlock.Epoch().Uint64()).
+			Uint64("ViewId", newBlock.Header().ViewID().Uint64()).
+			Str("blockHash", newBlock.Hash().String()).
+			Int("numTxns", len(newBlock.Transactions())).
+			Int("numStakingTxns", len(newBlock.StakingTransactions())).
 			Msg("BINGO !!! Reached Consensus")
 		// 15% of the validator also need to do broadcasting
 		rand.Seed(time.Now().UTC().UnixNano())

node/node_newblock.go
@@ -78,6 +78,7 @@ func (node *Node) WaitForConsensusReadyV2(readySignal chan struct{}, stopChan ch
 func (node *Node) proposeNewBlock() (*types.Block, error) {
 	// Update worker's current header and state data in preparation to propose/process new transactions
 	coinbase := node.Consensus.SelfAddress
+	node.Worker.UpdateCurrent(coinbase)
 
 	// Prepare transactions including staking transactions
 	pending, err := node.TxPool.Pending()
@@ -88,14 +89,16 @@ func (node *Node) proposeNewBlock() (*types.Block, error) {
 
 	// TODO: integrate staking transaction into tx pool
 	pendingStakingTransactions := types2.StakingTransactions{}
-	node.pendingStakingTxMutex.Lock()
-	for _, tx := range node.pendingStakingTransactions {
-		pendingStakingTransactions = append(pendingStakingTransactions, tx)
+	// Only process staking transactions after pre-staking epoch happened.
+	if node.Blockchain().Config().IsPreStaking(node.Worker.GetNewEpoch()) {
+		node.pendingStakingTxMutex.Lock()
+		for _, tx := range node.pendingStakingTransactions {
+			pendingStakingTransactions = append(pendingStakingTransactions, tx)
+		}
+		node.pendingStakingTransactions = make(map[common.Hash]*types2.StakingTransaction)
+		node.pendingStakingTxMutex.Unlock()
 	}
-	node.pendingStakingTransactions = make(map[common.Hash]*types2.StakingTransaction)
-	node.pendingStakingTxMutex.Unlock()
 
-	node.Worker.UpdateCurrent(coinbase)
 	if err := node.Worker.CommitTransactions(pending, pendingStakingTransactions, coinbase); err != nil {
 		utils.Logger().Error().Err(err).Msg("cannot commit transactions")
 		return nil, err
@@ -119,8 +122,9 @@ func (node *Node) proposeNewBlock() (*types.Block, error) {
 	}
 
 	// Prepare shard state
+	// NOTE: this will potentially override shard chain's epoch to beacon chain's epoch during staking migration period.
 	shardState, err := node.Worker.SuperCommitteeForNextEpoch(
-		node.Consensus.ShardID, node.Blockchain(),
+		node.Consensus.ShardID, node.Beaconchain(),
 	)
 
 	if err != nil {

commit 02ba0b309ce666c62bb47cbba5500fcbcb5fae94

Fix cross-TX header introduction glitch
Commit e79ba5fe880b56973a50a80d918b9145fce07cf0 introduced an off-by-one
error which caused CX-enabled header to be introduced one epoch later
than expected, i.e. at CrossTxEpoch + 1, where it should be
CrossTxEpoch.  This was caused by a vague, confusing naming of
IsCrossTx(epoch) function of ChainConfig: The name did not clarify
whether it meant introduction of CX-enabled header and processing of
ingress receipts (which occurs at CrossTxEpoch) or acceptance of
cross-shard transactions (which occurs at CrossTxEpoch+1).

Introduce and use params.(*ChainConfig).HasCrossTxFields which handles
the former case.

block/factory/factory.go
@@ -34,7 +34,7 @@ func (f *factory) NewHeader(epoch *big.Int) *block.Header {
 		impl = v3.NewHeader()
 	case f.chainConfig.IsCrossLink(epoch):
 		impl = v2.NewHeader()
-	case f.chainConfig.IsCrossTx(epoch):
+	case f.chainConfig.HasCrossTxFields(epoch):
 		impl = v1.NewHeader()
 	default:
 		impl = v0.NewHeader()

internal/params/config.go
@@ -139,19 +139,26 @@ func (c *ChainConfig) IsEIP155(epoch *big.Int) bool {
 	return isForked(c.EIP155Epoch, epoch)
 }
 
-// IsCrossTx returns whether cross-shard transaction is enabled in the given
+// IsCrossTx returns whether cross-shard transaction is accepted in the given
 // epoch.
 //
 // Note that this is different from comparing epoch against CrossTxEpoch.
-// Cross-shard transaction is enabled from CrossTxEpoch+1 and on, in order to
+// Cross-shard transaction is accepted from CrossTxEpoch+1 and on, in order to
 // allow for all shards to roll into CrossTxEpoch and become able to handle
 // ingress receipts.  In other words, cross-shard transaction fields are
-// introduced at CrossTxEpoch, but these fields are not used until
-// CrossTxEpoch+1, when cross-shard transactions are actually accepted by the
-// network.
+// introduced and ingress receipts are processed at CrossTxEpoch, but the shard
+// does not accept cross-shard transactions from clients until CrossTxEpoch+1.
+//
+// TODO ek – rename to AcceptsCrossTx to clarify
 func (c *ChainConfig) IsCrossTx(epoch *big.Int) bool {
 	crossTxEpoch := new(big.Int).Add(c.CrossTxEpoch, common.Big1)
 	return isForked(crossTxEpoch, epoch)
+}
+
+// HasCrossTxFields returns whether blocks in the given epoch includes
+// cross-shard transaction fields.
+func (c *ChainConfig) HasCrossTxFields(epoch *big.Int) bool {
+	return isForked(c.CrossTxEpoch, epoch)
 }
 
 // IsStaking determines whether it is staking epoch