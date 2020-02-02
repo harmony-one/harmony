cbe4ad2ef253211aa62e4f1b68cb2bb40e0f13a9
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-02-01 18:59:56 -0800

    [consensus] Fix travis tests

--- consensus/construct_test.go
+++ consensus/construct_test.go
@@ -29,7 +29,7 @@ func TestConstructAnnounceMessage(test *testing.T) {
 	}
 	consensus.blockHash = [32]byte{}
 
-	_, err = consensus.construct(msg_pb.MessageType_ANNOUNCE)
+	_, err = consensus.construct(msg_pb.MessageType_ANNOUNCE, nil)
 	if err != nil {
 		test.Fatalf("could not construct announce: %v", err)
 	}
@@ -73,7 +73,7 @@ func TestConstructPreparedMessage(test *testing.T) {
 		test.Log(ctxerror.New("prepareBitmap.SetKey").WithCause(err))
 	}
 
-	network, err := consensus.construct(msg_pb.MessageType_PREPARED)
+	network, err := consensus.construct(msg_pb.MessageType_PREPARED, nil)
 	if err != nil {
 		test.Errorf("Error when creating prepared message")
 	}
