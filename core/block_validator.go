d98267c113045340657e47a83a96436ecef06a37
Rongjian Lan <rongjian.lan@gmail.com>
2020-02-04 16:15:32 -0800

    Add one-off block verification rule for beacon chain bad block (#2180)
    * Add one-off block verification rule for beacon chain bad block
    
    * Add more comment

--- core/block_validator.go
+++ core/block_validator.go
@@ -91,7 +91,9 @@ func (v *BlockValidator) ValidateState(block, parent *types.Block, statedb *stat
 	// Validate the received block's bloom with the one derived from the generated receipts.
 	// For valid blocks this should always validate to true.
 	rbloom := types.CreateBloom(receipts)
-	if rbloom != header.Bloom() {
+	// Beacon chain block 1213181 is a one-off block with empty bloom which is expected to be non-empty.
+	// Skip the validation for it to avoid failure.
+	if rbloom != header.Bloom() && (block.NumberU64() != 1213181 || block.ShardID() != 0) {
 		return fmt.Errorf("invalid bloom (remote: %x  local: %x)", header.Bloom(), rbloom)
 	}
 	// Tre receipt Trie's root (R = (Tr [[H1, R1], ... [Hn, R1]]))
