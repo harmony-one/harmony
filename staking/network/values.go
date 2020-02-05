9e377ccca79c0493f3adff0c4715294b6995fdfc
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-02-04 21:43:59 -0800

    [network] Fill out record intended for RPC

--- staking/network/values.go
+++ staking/network/values.go
@@ -1,9 +1,15 @@
 package network
 
-import "math/big"
+import (
+	"math/big"
 
-// Reward ..
-type Reward struct {
-	AccumulatorSnapshot     *big.Int `json:"block-reward-accumulator"`
-	CurrentStakedPercentage *big.Int `json:"block-reward-accumulator"`
+	"github.com/harmony-one/harmony/numeric"
+)
+
+// UtilityMetric ..
+type UtilityMetric struct {
+	AccumulatorSnapshot     *big.Int
+	CurrentStakedPercentage *big.Int
+	Deviation               numeric.Dec
+	Adjustment              numeric.Dec
 }
