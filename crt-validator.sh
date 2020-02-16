46afbbc4f0f4ce0aa8458fbbe440f692a4852805
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-02-16 11:02:22 -0800

    [slash] Add delegator to double-sign testing script

--- crt-validator.sh
+++ crt-validator.sh
@@ -3,6 +3,7 @@
 # this is existing account that generates funds in test/debug.sh
 from='one1spshr72utf6rwxseaz339j09ed8p6f8ke370zj'
 to='one1zyxauxquys60dk824p532jjdq753pnsenrgmef'
+someRando='one1nqevvacj3y5ltuef05my4scwy5wuqteur72jk5'
 
 blsKey='be23bc3c93fe14a25f3533feee1cff1c60706845a4907c5df58bc19f5d1760bfff06fe7c9d1f596b18fdf529e0508e0a'
 
@@ -12,6 +13,11 @@ set -eu
 ./hmy-cli transfer --from "${from}" \
 	  --from-shard 0 --to-shard 0 \
 	  --to "${to}" --amount 5 --wait-for-confirm 30
+
+# transfer for our rando delegator 
+./hmy-cli transfer --from "${from}" \
+	  --from-shard 0 --to-shard 0 \
+	  --to "${someRando}" --amount 5 --wait-for-confirm 30
 
 ./hmy-cli balances "${to}"
 
@@ -26,3 +32,10 @@ set -eu
 	  --amount 3 \
 	  --bls-pubkeys "${blsKey}" \
 	  --chain-id testnet --wait-for-confirm 30
+
+# Need to do a delegator test
+./hmy-cli staking delegate \
+	  --delegator-addr "${someRando}" --validator-addr "${to}" \
+	  --amount 3 --passphrase
+
+./bin/trigger-double-sign
