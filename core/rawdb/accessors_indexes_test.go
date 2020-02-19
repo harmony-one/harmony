4d403a38ea8ddc3a27977408cf37089846f76e26
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-02-18 17:59:27 -0800

    [core] Add unit test to ensure tx fetch is correct

--- core/rawdb/accessors_indexes_test.go
+++ core/rawdb/accessors_indexes_test.go
@@ -21,10 +21,21 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethdb"
 
+	"github.com/harmony-one/bls/ffi/go/bls"
 	blockfactory "github.com/harmony-one/harmony/block/factory"
 	"github.com/harmony-one/harmony/core/types"
+	"github.com/harmony-one/harmony/crypto/hash"
+	"github.com/harmony-one/harmony/numeric"
+	"github.com/harmony-one/harmony/shard"
+	staking "github.com/harmony-one/harmony/staking/types"
+)
+
+var (
+	testBLSPubKey = "30b2c38b1316da91e068ac3bd8751c0901ef6c02a1d58bc712104918302c6ed03d5894671d0c816dad2b4d303320f202"
+	testBLSPrvKey = "c6d7603520311f7a4e6aac0b26701fc433b75b38df504cd416ef2b900cd66205"
 )
 
 // Tests that positional lookup metadata can be stored and retrieved.
@@ -66,5 +77,63 @@ func TestLookupStorage(t *testing.T) {
 		if txn, _, _, _ := ReadTransaction(db, tx.Hash()); txn != nil {
 			t.Fatalf("tx #%d [%x]: deleted transaction returned: %v", i, tx.Hash(), txn)
 		}
+	}
+}
+
+// Test that staking tx hash does not find a plain tx hash (and visa versa) within the same block
+func TestMixedLookupStorage(t *testing.T) {
+	db := ethdb.NewMemDatabase()
+	tx := types.NewTransaction(1, common.BytesToAddress([]byte{0x11}), 0, big.NewInt(111), 1111, big.NewInt(11111), []byte{0x11, 0x11, 0x11})
+	key, _ := crypto.GenerateKey()
+	stakePayloadMaker := func() (staking.Directive, interface{}) {
+		p := &bls.PublicKey{}
+		p.DeserializeHexStr(testBLSPubKey)
+		pub := shard.BlsPublicKey{}
+		pub.FromLibBLSPublicKey(p)
+		messageBytes := []byte(staking.BlsVerificationStr)
+		privateKey := &bls.SecretKey{}
+		privateKey.DeserializeHexStr(testBLSPrvKey)
+		msgHash := hash.Keccak256(messageBytes)
+		signature := privateKey.SignHash(msgHash[:])
+		var sig shard.BLSSignature
+		copy(sig[:], signature.Serialize())
+
+		ra, _ := numeric.NewDecFromStr("0.7")
+		maxRate, _ := numeric.NewDecFromStr("1")
+		maxChangeRate, _ := numeric.NewDecFromStr("0.5")
+		return staking.DirectiveCreateValidator, staking.CreateValidator{
+			Description: staking.Description{
+				Name:            "SuperHero",
+				Identity:        "YouWouldNotKnow",
+				Website:         "Secret Website",
+				SecurityContact: "LicenseToKill",
+				Details:         "blah blah blah",
+			},
+			CommissionRates: staking.CommissionRates{
+				Rate:          ra,
+				MaxRate:       maxRate,
+				MaxChangeRate: maxChangeRate,
+			},
+			MinSelfDelegation:  big.NewInt(1e18),
+			MaxTotalDelegation: big.NewInt(3e18),
+			ValidatorAddress:   crypto.PubkeyToAddress(key.PublicKey),
+			SlotPubKeys:        []shard.BlsPublicKey{pub},
+			SlotKeySigs:        []shard.BLSSignature{sig},
+			Amount:             big.NewInt(1e18),
+		}
+	}
+	stx, _ := staking.NewStakingTransaction(0, 1e10, big.NewInt(10000), stakePayloadMaker)
+	txs := []*types.Transaction{tx}
+	stxs := []*staking.StakingTransaction{stx}
+	header := blockfactory.NewTestHeader().With().Number(big.NewInt(314)).Header()
+	block := types.NewBlock(header, txs, types.Receipts{&types.Receipt{}, &types.Receipt{}}, nil, nil, stxs)
+	WriteBlock(db, block)
+	WriteTxLookupEntries(db, block)
+
+	if recTx, _, _, _ := ReadStakingTransaction(db, tx.Hash()); recTx != nil {
+		t.Fatal("got staking transactions with plain tx hash")
+	}
+	if recTx, _, _, _ := ReadTransaction(db, stx.Hash()); recTx != nil {
+		t.Fatal("got plain transactions with staking tx hash")
 	}
 }
