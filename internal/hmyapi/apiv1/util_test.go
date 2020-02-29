37d8ff3285c2f0ea35e1356a44b4b2a4cf449ba5
flicker-harmony <vlad@harmony.one>
2020-02-29 10:48:21 +0300

    Fix test

--- internal/hmyapi/apiv1/util_test.go
+++ internal/hmyapi/apiv1/util_test.go
@@ -2,15 +2,17 @@ package apiv1
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestPagination(t *testing.T) {
 	txHistoryArgs := TxHistoryArgs{PageIndex: 1, PageSize: 100}
 	length, txHistoryArgs := ReturnPagination(10, txHistoryArgs)
-	assert.Equal(t, 0, length, "should be equal to 0")
+	if length != 0 {
+		t.Errorf("expected pagination length %d; got %d", 0, length)
+	}
 	txHistoryArgs.PageIndex = 0
 	length, txHistoryArgs = ReturnPagination(10, txHistoryArgs)
-	assert.Equa	l(t, 10, length, "should be equal to 10")
+	if length != 10 {
+		t.Errorf("expected pagination length %d; got %d", 10, length)
+	}
 }
