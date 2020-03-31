f08b02c3f3fc6ef48407fe3bb66591c164ac80a4
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-03-31 09:12:23 -0700

    [p2p] Make error explicit, do not panic for log

--- p2p/p2pimpl/p2pimpl.go
+++ p2p/p2pimpl/p2pimpl.go
@@ -3,19 +3,20 @@ package p2pimpl
 import (
 	"net"
 
-	libp2p_crypto "github.com/libp2p/go-libp2p-crypto"
-
 	"github.com/harmony-one/harmony/internal/utils"
 	"github.com/harmony-one/harmony/p2p"
 	"github.com/harmony-one/harmony/p2p/host/hostv2"
+	libp2p_crypto "github.com/libp2p/go-libp2p-crypto"
 )
 
 // NewHost starts the host for p2p
 // for hostv2, it generates multiaddress, keypair and add PeerID to peer, add priKey to host
 // TODO (leo) The peerstore has to be persisted on disk.
 func NewHost(self *p2p.Peer, key libp2p_crypto.PrivKey) (p2p.Host, error) {
-	h := hostv2.New(self, key)
-
+	h, err := hostv2.New(self, key)
+	if err != nil {
+		return nil, err
+	}
 	utils.Logger().Info().
 		Str("self", net.JoinHostPort(self.IP, self.Port)).
 		Interface("PeerID", self.PeerID).
