554c65c635cb3913bbc18606122fca9cfc81e1f6
Leo Chen <leo@harmony.one>
2020-04-01 22:47:08 +0000

    [build] remove double / in FOLDER
    Signed-off-by: Leo Chen <leo@harmony.one>

--- scripts/go_executable_build.sh
+++ scripts/go_executable_build.sh
@@ -13,7 +13,7 @@ PUBBUCKET=pub.harmony.one
 REL=
 GOOS=linux
 GOARCH=amd64
-FOLDER=/${WHOAMI:-$USER}
+FOLDER=${WHOAMI:-$USER}
 RACE=
 VERBOSE=
 GO_GCFLAGS="all=-c 2"
@@ -170,7 +170,7 @@ function upload
    if [ "$STATIC" != "true" ]; then
       for lib in "${!LIB[@]}"; do
          if [ -e ${LIB[$lib]} ]; then
-            $AWSCLI s3 cp ${LIB[$lib]} s3://${BUCKET}$FOLDER/$lib --acl public-read
+            $AWSCLI s3 cp ${LIB[$lib]} s3://${BUCKET}/$FOLDER/$lib --acl public-read
          else
             echo "!! MISSING ${LIB[$lib]} !!"
          fi
@@ -180,11 +180,11 @@ function upload
    fi
 
    for bin in "${!SRC[@]}"; do
-      [ -e $BINDIR/$bin ] && $AWSCLI s3 cp $BINDIR/$bin s3://${BUCKET}$FOLDER/$bin --acl public-read
+      [ -e $BINDIR/$bin ] && $AWSCLI s3 cp $BINDIR/$bin s3://${BUCKET}/$FOLDER/$bin --acl public-read
    done
 
 
-   [ -e $BINDIR/md5sum.txt ] && $AWSCLI s3 cp $BINDIR/md5sum.txt s3://${BUCKET}$FOLDER/md5sum.txt --acl public-read
+   [ -e $BINDIR/md5sum.txt ] && $AWSCLI s3 cp $BINDIR/md5sum.txt s3://${BUCKET}/$FOLDER/md5sum.txt --acl public-read
 }
 
 function release
