be3ca8407b192ce218b30c0edbded41a68627877
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-04-02 12:35:32 -0700

    [project] More leftover wallet support scripts, update README

--- scripts/wallet_embed_ini_files.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/harmony-one/harmony/internal/utils"
-
-// Embed the default wallet.ini file into defaultWalletIni string literal constant
-func main() {
-	utils.EmbedFile("../../../.hmy/wallet.ini", "defaultWalletIni")
-}
