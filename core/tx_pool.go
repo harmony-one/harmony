bab74f93e368e9908236b77fa592415ff9959198
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-04-02 18:55:57 -0700

    [txn-pool] Closes 2690

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -776,11 +776,11 @@ func (pool *TxPool) add(tx *types.Transaction, local bool) (bool, error) {
 	}
 	pool.journalTx(from, tx)
 
-	logger.Warn().
+	logger.Info().
 		Str("hash", hash.Hex()).
 		Interface("from", from).
 		Interface("to", tx.To()).
-		Msg("Pooled new future transaction")
+		Msg("pooled new future transaction")
 	return replace, nil
 }
 
