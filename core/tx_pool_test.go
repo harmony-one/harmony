a876df67bdfe982c52256c7c11c897c97a0d6412
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-04-06 00:05:47 -0700

    Squashed commit of the following: (#2750)
    commit ba1c0cd0a3237dcc094266c30253e401b65e3552
    Author: Edgar Aroutiounian <edgar.factorial@gmail.com>
    Date:   Sun Apr 5 23:53:03 2020 -0700
    
        [blockchain] Create fresh big int, remove more dead code from static … (#2749)
    
        * [blockchain] Create fresh big int, remove more dead code from static analysis
    
        * [measure] Remove IzZero/IsNil checks
    
    commit 191f1d5ee266d32cf08f243f953925718fdb61c1
    Author: Rongjian Lan <rongjian.lan@gmail.com>
    Date:   Sun Apr 5 21:11:21 2020 -0700
    
        Final attempt: Don't div by 0; print out debug info (#2746)
    
        * Revert "Revert "fix earned-reward by writing the stats only once, remove from UpdateValdiatorVotingPower (#2737)" (#2742)"
    
        This reverts commit 832b01dfff0b2fec3cc647f507a8d105a8dbf4a7.
    
        * Don't div by 0; print out debug info
    
    commit 832b01dfff0b2fec3cc647f507a8d105a8dbf4a7
    Author: Edgar Aroutiounian <edgar.factorial@gmail.com>
    Date:   Sun Apr 5 19:40:37 2020 -0700
    
        Revert "fix earned-reward by writing the stats only once, remove from UpdateValdiatorVotingPower (#2737)" (#2742)
    
        This reverts commit dc036e64c7fa9b1bc0e34c0da7c67c541b0317fa.
    
    commit dc036e64c7fa9b1bc0e34c0da7c67c541b0317fa
    Author: Ganesha Upadhyaya <ganeshrvce@gmail.com>
    Date:   Sun Apr 5 17:18:36 2020 -0700
    
        fix earned-reward by writing the stats only once, remove from UpdateValdiatorVotingPower (#2737)
    
    commit 66f26e85084d2e5b8385c5e4b9888171c93e664e
    Author: Rongjian Lan <rongjian.lan@gmail.com>
    Date:   Sun Apr 5 17:06:50 2020 -0700
    
        do snapshot for validators at last block of epoch (#2736)
    
    commit f2524a888d6133e7d729ee22031d198695e8ce5f
    Author: Rongjian Lan <rongjian.lan@gmail.com>
    Date:   Sun Apr 5 12:33:35 2020 -0700
    
        Make validator snapshot on new validator too (#2733)
    
    commit 350b7a0bb45c68889547ac5b9521f46dcd97ced8
    Author: Rongjian Lan <rongjian.lan@gmail.com>
    Date:   Sat Apr 4 23:41:19 2020 -0700
    
        fix validator snapshot cache
    
    commit 8f5f287c749c62209f2dd08bfc0dbfc88df7b225
    Author: Leo Chen <leo@harmony.one>
    Date:   Sat Apr 4 19:09:38 2020 -0700
    
        [node.sh] use rclone to fast sync harmony_db_0 (#2709)
    
        Signed-off-by: Leo Chen <leo@harmony.one>
    
    commit 05f2e20a39e3472ee1ff506aa8b974a988a9a970
    Author: Rongjian Lan <rongjian.lan@gmail.com>
    Date:   Sat Apr 4 19:00:28 2020 -0700
    
        make validator snapshot consistent with election (#2726)
    
        * make validator snapshot consistent with election
    
        * fix condition with only beacon chain
    
        * Fix epoch number
    
        * Fix stats update nil epoch issue
    
    commit 24a17feecdbd91b8bc3c8a758be6e2f0c2e2b973
    Author: Edgar Aroutiounian <edgar.factorial@gmail.com>
    Date:   Sat Apr 4 16:38:12 2020 -0700
    
        [state] ValidatorWrapper div zero panic fix -  (#2724)
    
        * [state] Error on div zero
    
        * [state] Do not pay out commit when commit is 0%
    
        * [state] Put whole commission in guarded block
    
        * [core] Reward cannot be zero
    
        * [state] return nil, not hard error
    
    commit 1f83ebf6a6f2853d9df2b18fd658a82a32c4f530
    Author: Minh Doan <40258599+mikedoan@users.noreply.github.com>
    Date:   Sat Apr 4 14:38:55 2020 -0700
    
        change explorer node storage folder (#2720)
    
    commit 3aa08e9e3b4f17423eda01138b2900e0abf6f0c0
    Author: Edgar Aroutiounian <edgar.factorial@gmail.com>
    Date:   Sat Apr 4 10:21:51 2020 -0700
    
        [rpc] Show both latest header of beacon chain and shard chain (#2714)
    
    commit 1a2c23ada524e4cadc89001bec60f9b5147885b8
    Author: Edgar Aroutiounian <edgar.factorial@gmail.com>
    Date:   Fri Apr 3 17:49:24 2020 -0700
    
        [validator] Hide one field from JSON (#2705)

--- core/tx_pool_test.go
+++ core/tx_pool_test.go
@@ -176,34 +176,6 @@ func validateTxPoolInternals(pool *TxPool) error {
 	return nil
 }
 
-// validateEvents checks that the correct number of transaction addition events
-// were fired on the pool's event feed.
-func validateEvents(events chan NewTxsEvent, count int) error {
-	var received []*types.Transaction
-
-	for len(received) < count {
-		select {
-		case ev := <-events:
-			received = append(received, ev.Txs...)
-		case <-time.After(time.Second):
-			return fmt.Errorf("event #%d not fired", len(received))
-		}
-	}
-	if len(received) > count {
-		return fmt.Errorf("more than %d events fired: %v", count, received[count:])
-	}
-	select {
-	case ev := <-events:
-		return fmt.Errorf("more than %d events fired: %v", count, ev.Txs)
-
-	case <-time.After(50 * time.Millisecond):
-		// This branch should be "default", but it's a data race between goroutines,
-		// reading the event channel and pushing into it, so better wait a bit ensuring
-		// really nothing gets injected.
-	}
-	return nil
-}
-
 func deriveSender(tx types.PoolTransaction) (common.Address, error) {
 	return types.PoolTransactionSender(types.HomesteadSigner{}, tx)
 }
