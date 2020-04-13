a096f734d7dd059b98c775f5a90bce237b389194
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-04-13 11:59:44 -0700

    [project] Dead fields, staticcheck, update deprecated libp2p usages

--- internal/genesis/genesis_test.go
+++ internal/genesis/genesis_test.go
@@ -1,9 +1,6 @@
 package genesis
 
 import (
-	"bufio"
-	"fmt"
-	"os"
 	"strconv"
 	"strings"
 	"testing"
@@ -11,45 +8,11 @@ import (
 	"github.com/btcsuite/btcutil/bech32"
 	ethCommon "github.com/ethereum/go-ethereum/common"
 	"github.com/harmony-one/bls/ffi/go/bls"
-
 	"github.com/harmony-one/harmony/internal/common"
 )
 
 func TestString(t *testing.T) {
 	_ = BeaconAccountPriKey()
-}
-
-func fileToLines(filePath string) (lines []string, err error) {
-	f, err := os.Open(filePath)
-	if err != nil {
-		return
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	err = scanner.Err()
-	return
-}
-
-func testGenesisccounts(t *testing.T) {
-	ones, err := fileToLines("one-acc.txt")
-	if err != nil {
-		t.Fatal("ReadFile failed", err)
-	}
-
-	bls, err := fileToLines("bls-pub.txt")
-	if err != nil {
-		t.Fatal("ReadFile failed", err)
-	}
-
-	index := 404
-	for i, one := range ones {
-		fmt.Printf("   {Index: \" %v \", Address: \"%v\", BLSPublicKey: \"%v\"},\n", index, one, bls[i])
-		index++
-	}
 }
 
 func TestCommitteeAccounts(test *testing.T) {
