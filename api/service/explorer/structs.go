a5039ec5d80a753202b1a7a565a317034598d336
Dennis Won <jhwon0820@gmail.com>
2020-04-13 18:58:19 -0700

    make staking txn field values to be decoded on explorer node ldb storage, and set to address correctly for staking txns

--- api/service/explorer/structs.go
+++ api/service/explorer/structs.go
@@ -1,15 +1,22 @@
 package explorer
 
 import (
+	"github.com/ethereum/go-ethereum/common"
+
+	"bytes"
 	"encoding/hex"
 	"math/big"
 	"strconv"
 
 	core2 "github.com/harmony-one/harmony/core"
 	"github.com/harmony-one/harmony/core/types"
-	"github.com/harmony-one/harmony/internal/common"
+	common2 "github.com/harmony-one/harmony/internal/common"
 	"github.com/harmony-one/harmony/internal/utils"
+	"github.com/harmony-one/harmony/numeric"
+	"github.com/harmony-one/harmony/shard"
+	"github.com/harmony-one/harmony/staking/effective"
 	staking "github.com/harmony-one/harmony/staking/types"
+	"github.com/pkg/errors"
 )
 
 /*
@@ -20,6 +27,15 @@ import (
 const (
 	Received = "RECEIVED"
 	Sent     = "SENT"
+)
+
+// Errors ...
+var (
+	// ErrInvalidMsgForStakingDirective is returned if a staking message does not
+	// match the related directive
+	ErrInvalidMsgForStakingDirective = errors.New("staking message does not match directive message")
+	// ErrInvalidSender is returned if a txn or staking txn message has incorrect from address
+	ErrInvalidSender = errors.New("staking message does not match directive message")
 )
 
 // Data ...
@@ -60,12 +76,12 @@ func GetTransaction(tx *types.Transaction, addressBlock *types.Block) *Transacti
 	gasFee = gasFee.Mul(tx.GasPrice(), new(big.Int).SetUint64(tx.Gas()))
 	to := ""
 	if msg.To() != nil {
-		if to, err = common.AddressToBech32(*msg.To()); err != nil {
+		if to, err = common2.AddressToBech32(*msg.To()); err != nil {
 			return nil
 		}
 	}
 	from := ""
-	if from, err = common.AddressToBech32(msg.From()); err != nil {
+	if from, err = common2.AddressToBech32(msg.From()); err != nil {
 		return nil
 	}
 	return &Transaction{
@@ -85,38 +101,63 @@ func GetTransaction(tx *types.Transaction, addressBlock *types.Block) *Transacti
 
 // StakingTransaction ...
 type StakingTransaction struct {
-	ID        string   `json:"id"`
-	Timestamp string   `json:"timestamp"`
-	From      string   `json:"from"`
-	To        string   `json:"to"`
-	Value     *big.Int `json:"value"`
-	Bytes     string   `json:"bytes"`
-	Data      string   `json:"data"`
-	GasFee    *big.Int `json:"gasFee"`
-	FromShard uint32   `json:"fromShard"`
-	ToShard   uint32   `json:"toShard"`
-	Type      string   `json:"type"`
+	Transaction
+
+	// CreateValidator
+
+	ValidatorAddress   common.Address          `json:"validator-address"`
+	Description        staking.Description     `json:"description"`
+	CommissionRates    staking.CommissionRates `json:"commission"`
+	MinSelfDelegation  *big.Int                `json:"min-self-delegation"`
+	MaxTotalDelegation *big.Int                `json:"max-total-delegation"`
+	SlotPubKeys        []shard.BLSPublicKey    `json:"slot-pub-keys"`
+	SlotKeySigs        []shard.BLSSignature    `json:"slot-key-sigs"`
+
+	// EditValidator
+
+	// ValidatorAddress common.Address        `json:"validator-address"`
+	// Description      staking.Description   `json:"description"`
+	Amount         *big.Int     `json:"amount"`
+	CommissionRate *numeric.Dec `json:"commission-rate" rlp:"nil"`
+	// MinSelfDelegation  *big.Int              `json:"min-self-delegation" rlp:"nil"`
+	// MaxTotalDelegation *big.Int              `json:"max-total-delegation" rlp:"nil"`
+	SlotKeyToRemove *shard.BLSPublicKey   `json:"slot-key-to_remove" rlp:"nil"`
+	SlotKeyToAdd    *shard.BLSPublicKey   `json:"slot-key-to_add" rlp:"nil"`
+	SlotKeyToAddSig *shard.BLSSignature   `json:"slot-key-to-add-sig" rlp:"nil"`
+	EPOSStatus      effective.Eligibility `json:"epos-eligibility-status" rlp:"nil"`
+
+	// Delegate/Undelegate
+
+	DelegatorAddress common.Address `json:"delegator_address"`
+	// ValidatorAddress common.Address `json:"validator_address"`
+	// Amount           *big.Int       `json:"amount"`
+
+	// CollectRewards
+	// DelegatorAddress common.Address `json:"delegator_address"`
 }
 
 // GetStakingTransaction ...
-func GetStakingTransaction(tx *staking.StakingTransaction, addressBlock *types.Block) *StakingTransaction {
+func GetStakingTransaction(tx *staking.StakingTransaction, addressBlock *types.Block) (*StakingTransaction, error) {
+	// TODO: legacy. To prevent breaking current clients of explorer staking txns rpc apis, keep unnecessary rlp encoding here
+	// Will be removed when the current client (only ostn block explorer) migrates
 	msg, err := core2.StakingToMessage(tx, addressBlock.Header().Number())
 	if err != nil {
 		utils.Logger().Error().Err(err).Msg("Error when parsing tx into message")
 	}
+
 	gasFee := big.NewInt(0)
 	gasFee = gasFee.Mul(tx.GasPrice(), new(big.Int).SetUint64(tx.Gas()))
 	to := ""
 	if msg.To() != nil {
-		if to, err = common.AddressToBech32(*msg.To()); err != nil {
-			return nil
+		if to, err = common2.AddressToBech32(*msg.To()); err != nil {
+			return nil, err
 		}
 	}
 	from := ""
-	if from, err = common.AddressToBech32(msg.From()); err != nil {
-		return nil
+	if from, err = common2.AddressToBech32(msg.From()); err != nil {
+		return nil, err
 	}
-	return &StakingTransaction{
+	txn := Transaction{
 		ID:        tx.Hash().Hex(),
 		Timestamp: strconv.Itoa(int(addressBlock.Time().Int64() * 1000)),
 		From:      from,
@@ -128,5 +169,157 @@ func GetStakingTransaction(tx *staking.StakingTransaction, addressBlock *types.B
 		FromShard: tx.ShardID(),
 		ToShard:   0,
 		Type:      string(msg.Type()),
+	}
+
+	switch tx.StakingType() {
+	case staking.DirectiveCreateValidator:
+		stkMsg, err := staking.RLPDecodeStakeMsg(tx.Data(), staking.DirectiveCreateValidator)
+		if err != nil {
+			return nil, err
+		}
+
+		if _, ok := stkMsg.(*staking.CreateValidator); !ok {
+			return nil, ErrInvalidMsgForStakingDirective
+		}
+
+		createValidatorMsg := stkMsg.(*staking.CreateValidator)
+		if bytes.Compare(msg.From().Bytes(), createValidatorMsg.ValidatorAddress.Bytes()) != 0 {
+			return nil, ErrInvalidSender
+		}
+
+		// for fetching sent/received txns history, create validator txn treated as a self transaction
+		validatorAddress := ""
+		if validatorAddress, err = common2.AddressToBech32(createValidatorMsg.ValidatorAddress); err != nil {
+			return nil, err
+		}
+		txn.To = validatorAddress
+
+		return &StakingTransaction{
+			Transaction:        txn,
+			ValidatorAddress:   createValidatorMsg.ValidatorAddress,
+			Description:        createValidatorMsg.Description,
+			CommissionRates:    createValidatorMsg.CommissionRates,
+			MinSelfDelegation:  createValidatorMsg.MinSelfDelegation,
+			MaxTotalDelegation: createValidatorMsg.MaxTotalDelegation,
+			SlotPubKeys:        createValidatorMsg.SlotPubKeys,
+			SlotKeySigs:        createValidatorMsg.SlotKeySigs,
+			Amount:             createValidatorMsg.Amount,
+		}, nil
+	case staking.DirectiveEditValidator:
+		stkMsg, err := staking.RLPDecodeStakeMsg(tx.Data(), staking.DirectiveEditValidator)
+		if err != nil {
+			return nil, err
+		}
+
+		if _, ok := stkMsg.(*staking.EditValidator); !ok {
+			return nil, ErrInvalidMsgForStakingDirective
+		}
+
+		editValidatorMsg := stkMsg.(*staking.EditValidator)
+		if bytes.Compare(msg.From().Bytes(), editValidatorMsg.ValidatorAddress.Bytes()) != 0 {
+			return nil, ErrInvalidSender
+		}
+
+		// for fetching sent/received txns history, edit validator txn treated as a self transaction
+		validatorAddress := ""
+		if validatorAddress, err = common2.AddressToBech32(editValidatorMsg.ValidatorAddress); err != nil {
+			return nil, err
+		}
+		txn.To = validatorAddress
+
+		return &StakingTransaction{
+			Transaction:        txn,
+			ValidatorAddress:   editValidatorMsg.ValidatorAddress,
+			Description:        editValidatorMsg.Description,
+			CommissionRate:     editValidatorMsg.CommissionRate,
+			MinSelfDelegation:  editValidatorMsg.MinSelfDelegation,
+			MaxTotalDelegation: editValidatorMsg.MaxTotalDelegation,
+			SlotKeyToRemove:    editValidatorMsg.SlotKeyToRemove,
+			SlotKeyToAdd:       editValidatorMsg.SlotKeyToAdd,
+			SlotKeyToAddSig:    editValidatorMsg.SlotKeyToAddSig,
+			EPOSStatus:         editValidatorMsg.EPOSStatus,
+		}, nil
+	case staking.DirectiveDelegate:
+		stkMsg, err := staking.RLPDecodeStakeMsg(tx.Data(), staking.DirectiveDelegate)
+		if err != nil {
+			return nil, err
+		}
+
+		if _, ok := stkMsg.(*staking.Delegate); !ok {
+			return nil, ErrInvalidMsgForStakingDirective
+		}
+
+		delegateMsg := stkMsg.(*staking.Delegate)
+		if bytes.Compare(msg.From().Bytes(), delegateMsg.DelegatorAddress.Bytes()) != 0 {
+			return nil, ErrInvalidSender
+		}
+
+		validatorAddress := ""
+		if validatorAddress, err = common2.AddressToBech32(delegateMsg.ValidatorAddress); err != nil {
+			return nil, err
+		}
+		txn.To = validatorAddress
+
+		return &StakingTransaction{
+			Transaction:      txn,
+			ValidatorAddress: delegateMsg.ValidatorAddress,
+			DelegatorAddress: delegateMsg.DelegatorAddress,
+			Amount:           delegateMsg.Amount,
+		}, nil
+	case staking.DirectiveUndelegate:
+		stkMsg, err := staking.RLPDecodeStakeMsg(tx.Data(), staking.DirectiveUndelegate)
+		if err != nil {
+			return nil, err
+		}
+
+		if _, ok := stkMsg.(*staking.Undelegate); !ok {
+			return nil, ErrInvalidMsgForStakingDirective
+		}
+
+		undelegateMsg := stkMsg.(*staking.Undelegate)
+		if bytes.Compare(msg.From().Bytes(), undelegateMsg.DelegatorAddress.Bytes()) != 0 {
+			return nil, ErrInvalidSender
+		}
+
+		validatorAddress := ""
+		if validatorAddress, err = common2.AddressToBech32(undelegateMsg.ValidatorAddress); err != nil {
+			return nil, err
+		}
+		txn.To = validatorAddress
+
+		return &StakingTransaction{
+			Transaction:      txn,
+			ValidatorAddress: undelegateMsg.ValidatorAddress,
+			DelegatorAddress: undelegateMsg.DelegatorAddress,
+			Amount:           undelegateMsg.Amount,
+		}, nil
+	case staking.DirectiveCollectRewards:
+		stkMsg, err := staking.RLPDecodeStakeMsg(tx.Data(), staking.DirectiveCollectRewards)
+		if err != nil {
+			return nil, err
+		}
+
+		if _, ok := stkMsg.(*staking.CollectRewards); !ok {
+			return nil, ErrInvalidMsgForStakingDirective
+		}
+
+		collectRewardsMsg := stkMsg.(*staking.CollectRewards)
+		if bytes.Compare(msg.From().Bytes(), collectRewardsMsg.DelegatorAddress.Bytes()) != 0 {
+			return nil, ErrInvalidSender
+		}
+
+		// for fetching sent/received txns history, collect reward txn treated as a self transaction
+		delegatorAddress := ""
+		if delegatorAddress, err = common2.AddressToBech32(collectRewardsMsg.DelegatorAddress); err != nil {
+			return nil, err
+		}
+		txn.To = delegatorAddress
+
+		return &StakingTransaction{
+			Transaction:      txn,
+			DelegatorAddress: collectRewardsMsg.DelegatorAddress,
+		}, nil
+	default:
+		return nil, staking.ErrInvalidStakingKind
 	}
 }
