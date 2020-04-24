201f55e3b72be70fd4dd00eead56de20466b02f0
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-04-24 11:42:42 -0700

    [p2p][project] Take p2p dir as is from ipfs-node branch

--- p2p/host.go
+++ p2p/host.go
@@ -4,250 +4,236 @@ import (
 	"context"
 	"encoding/binary"
 	"fmt"
-	"net"
-	"os"
 	"strings"
 	"sync"
 
 	"github.com/harmony-one/bls/ffi/go/bls"
 	nodeconfig "github.com/harmony-one/harmony/internal/configs/node"
-	"github.com/harmony-one/harmony/internal/utils"
-	libp2p "github.com/libp2p/go-libp2p"
-	libp2p_crypto "github.com/libp2p/go-libp2p-core/crypto"
-	libp2p_host "github.com/libp2p/go-libp2p-core/host"
+	"github.com/harmony-one/harmony/p2p/ipfsutil"
+	"github.com/ipfs/go-datastore"
+	sync_ds "github.com/ipfs/go-datastore/sync"
+	ipfs_cfg "github.com/ipfs/go-ipfs-config"
+	ipfs_core "github.com/ipfs/go-ipfs/core"
+	ipfs_interface "github.com/ipfs/interface-go-ipfs-core"
+	"github.com/juju/fslock"
 	libp2p_peer "github.com/libp2p/go-libp2p-core/peer"
-	libp2p_peerstore "github.com/libp2p/go-libp2p-core/peerstore"
-	libp2p_pubsub "github.com/libp2p/go-libp2p-pubsub"
-	ma "github.com/multiformats/go-multiaddr"
+	"github.com/multiformats/go-multiaddr"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
+	"golang.org/x/sync/errgroup"
 )
-
-// Host is the client + server in p2p network.
-type Host interface {
-	GetSelfPeer() Peer
-	AddPeer(*Peer) error
-	GetID() libp2p_peer.ID
-	GetP2PHost() libp2p_host.Host
-	GetPeerCount() int
-	ConnectHostPeer(Peer) error
-	// SendMessageToGroups sends a message to one or more multicast groups.
-	SendMessageToGroups(groups []nodeconfig.GroupID, msg []byte) error
-	AllTopics() []*libp2p_pubsub.Topic
-}
 
 // Peer is the object for a p2p peer (node)
 type Peer struct {
-	IP              string         // IP address of the peer
+	IP string // IP address of the peer
+	// TODO this should not be string
 	Port            string         // Port number of the peer
 	ConsensusPubKey *bls.PublicKey // Public key of the peer, used for consensus signing
-	Addrs           []ma.Multiaddr // MultiAddress of the peer
 	PeerID          libp2p_peer.ID // PeerID, the pubkey for communication
 }
 
-func (p Peer) String() string {
-	BLSPubKey := "nil"
-	if p.ConsensusPubKey != nil {
-		BLSPubKey = p.ConsensusPubKey.SerializeToHexStr()
+// Host is the client + server in p2p network.
+type Host interface {
+	GetSelfPeer() *Peer
+	SendMessageToGroups(groups []nodeconfig.GroupID, msg []byte) error
+	GetPeerCount() int
+	AllSubscriptions() []ipfs_interface.PubSubSubscription
+}
+
+var DefaultBootstrap = ipfs_cfg.DefaultBootstrapAddresses
+
+type Opts struct {
+	Bootstrap             []string
+	RendezVousServerMAddr string
+	Port                  uint
+	RootDS                datastore.Batching
+	Logger                *zerolog.Logger
+}
+
+type hmyHost struct {
+	// Temp hack to satisfy all methods
+	Host
+	coreAPI ipfs_interface.CoreAPI
+	node    *ipfs_core.IpfsNode
+	log     *zerolog.Logger
+	ownPeer *Peer
+	lock    sync.Mutex
+	joined  map[string]ipfs_interface.PubSubSubscription
+}
+
+func unlockFS(l *fslock.Lock) {
+	if l == nil {
+		return
 	}
-	return fmt.Sprintf(
-		"BLSPubKey:%s-%s/%s[%d]", BLSPubKey,
-		net.JoinHostPort(p.IP, p.Port), p.PeerID, len(p.Addrs),
-	)
+
+	err := l.Unlock()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func panicUnlockFS(err error, l *fslock.Lock) {
+	unlockFS(l)
+	panic(err)
+}
+
+func fatal(err error) {
+	fmt.Println("died, why", err.Error())
+	panic("end")
+}
+
+func (h *hmyHost) GetSelfPeer() *Peer {
+	return h.ownPeer
+}
+
+func (h *hmyHost) SendMessageToGroups(groups []nodeconfig.GroupID, msg []byte) error {
+	ctx := context.Background()
+	var g errgroup.Group
+
+	for _, group := range groups {
+		top := string(group)
+		_, err := h.getTopic(top)
+		if err != nil {
+			return err
+		}
+		g.Go(func() error {
+			return h.coreAPI.PubSub().Publish(ctx, top, msg)
+		})
+	}
+
+	return g.Wait()
+}
+
+func (h *hmyHost) getTopic(topic string) (ipfs_interface.PubSubSubscription, error) {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+	if t, ok := h.joined[topic]; ok {
+		return t, nil
+	}
+
+	sub, err := h.coreAPI.PubSub().Subscribe(context.Background(), topic)
+
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot join pubsub topic %x", topic)
+	}
+
+	h.joined[topic] = sub
+	return sub, nil
+}
+
+func (h *hmyHost) AllSubscriptions() []ipfs_interface.PubSubSubscription {
+	subs := []ipfs_interface.PubSubSubscription{}
+	h.lock.Lock()
+	defer h.lock.Unlock()
+	for _, g := range h.joined {
+		subs = append(subs, g)
+	}
+	return subs
+}
+
+func (h *hmyHost) GetPeerCount() int {
+	conns, _ := h.coreAPI.Swarm().Peers(context.Background())
+	return len(conns)
 }
 
 // NewHost ..
-func NewHost(self *Peer, key libp2p_crypto.PrivKey) (Host, error) {
-	listenAddr, err := ma.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%s", self.Port))
-	if err != nil {
-		return nil, errors.Wrapf(err,
-			"cannot create listen multiaddr from port %#v", self.Port)
+func NewHost(opts *Opts, own *Peer) (Host, error) {
+	var swarmAddresses []string
+
+	if opts.Port != 0 {
+		swarmAddresses = []string{
+			fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", opts.Port),
+			fmt.Sprintf("/ip6/127.0.0.1/tcp/%d", opts.Port),
+			// TODO Hold up, need httpu
+			// fmt.Sprintf("/ip4/0.0.0.0/udp/%d/quic", opts.Port+1),
+			// fmt.Sprintf("/ip6/0.0.0.0/udp/%d/quic", opts.Port+1),
+		}
+	} else {
+		swarmAddresses = []string{
+			"/ip4/127.0.0.1/tcp/0",
+			"/ip6/127.0.0.1/tcp/0",
+			// "/ip4/0.0.0.0/udp/0/quic",
+			// "/ip6/0.0.0.0/udp/0/quic",
+		}
 	}
+
+	mardv, err := multiaddr.NewMultiaddr(opts.RendezVousServerMAddr)
+	if err != nil {
+		fatal(err)
+	}
+
+	rdvpeer, err := libp2p_peer.AddrInfoFromP2pAddr(mardv)
+	if err != nil {
+		fatal(err)
+	}
+
 	ctx := context.Background()
-	p2pHost, err := libp2p.New(ctx,
-		libp2p.ListenAddrs(listenAddr), libp2p.Identity(key),
+	rootDS := sync_ds.MutexWrap(opts.RootDS)
+	ipfsDS := ipfsutil.NewNamespacedDatastore(rootDS, datastore.NewKey("ipfs"))
+	cfg, err := ipfsutil.CreateBuildConfigWithDatastore(&ipfsutil.BuildOpts{
+		SwarmAddresses: swarmAddresses,
+	}, ipfsDS)
+	if err != nil {
+		fatal(err)
+	}
+
+	routingOpt, crouting := ipfsutil.NewTinderRouting(
+		opts.Logger, rdvpeer.ID, false,
+	)
+	cfg.Routing = routingOpt
+
+	ipfsConfig, err := cfg.Repo.Config()
+	if err != nil {
+		fatal(err)
+	}
+
+	ipfsConfig.Bootstrap = append(opts.Bootstrap, opts.RendezVousServerMAddr)
+	if err := cfg.Repo.SetConfig(ipfsConfig); err != nil {
+		fatal(err)
+	}
+
+	api, node, err := ipfsutil.NewConfigurableCoreAPI(
+		ctx,
+		cfg,
+		ipfsutil.OptionMDNSDiscovery,
 	)
 	if err != nil {
-		return nil, errors.Wrapf(err, "cannot initialize libp2p host")
+		fatal(err)
 	}
-	traceFile := os.Getenv("P2P_TRACEFILE")
+	// wait to get routing
+	<-crouting
+	// routing.RoutingTable().Print()
 
-	const MaxSize = 2_145_728
-	options := []libp2p_pubsub.Option{
-		libp2p_pubsub.WithPeerOutboundQueueSize(64),
-		libp2p_pubsub.WithMaxMessageSize(MaxSize),
-	}
-	if len(traceFile) > 0 {
-		tracer, _ := libp2p_pubsub.NewJSONTracer(traceFile)
-		options = append(options, libp2p_pubsub.WithEventTracer(tracer))
-	}
-	pubsub, err := libp2p_pubsub.NewGossipSub(ctx, p2pHost, options...)
-	if err != nil {
-		return nil, errors.Wrapf(err, "cannot initialize libp2p pubsub")
-	}
+	// fmt.Println("node->",
+	// 	node.Identity,
+	// 	node.PeerHost.Addrs(),
+	// 	routing,
+	// )
 
-	self.PeerID = p2pHost.ID()
-	subLogger := utils.Logger().With().Str("hostID", p2pHost.ID().Pretty()).Logger()
-	// has to save the private key for host
-	h := &HostV2{
-		h:      p2pHost,
-		joiner: topicJoiner{pubsub},
-		joined: map[string]*libp2p_pubsub.Topic{},
-		self:   *self,
-		priKey: key,
-		logger: &subLogger,
-	}
-
-	if err != nil {
-		return nil, err
-	}
-	utils.Logger().Info().
-		Str("self", net.JoinHostPort(self.IP, self.Port)).
-		Interface("PeerID", self.PeerID).
-		Str("PubKey", self.ConsensusPubKey.SerializeToHexStr()).
-		Msg("libp2p host ready")
-	return h, nil
-}
-
-type topicJoiner struct {
-	pubsub *libp2p_pubsub.PubSub
-}
-
-func (tj topicJoiner) JoinTopic(topic string) (*libp2p_pubsub.Topic, error) {
-	th, err := tj.pubsub.Join(topic)
-	if err != nil {
-		return nil, err
-	}
-	return th, nil
-}
-
-// HostV2 is the version 2 p2p host
-type HostV2 struct {
-	h      libp2p_host.Host
-	joiner topicJoiner
-	joined map[string]*libp2p_pubsub.Topic
-	self   Peer
-	priKey libp2p_crypto.PrivKey
-	lock   sync.Mutex
-	// logger
-	logger *zerolog.Logger
-}
-
-func (host *HostV2) getTopic(topic string) (*libp2p_pubsub.Topic, error) {
-	host.lock.Lock()
-	defer host.lock.Unlock()
-	if t, ok := host.joined[topic]; ok {
-		return t, nil
-	} else if t, err := host.joiner.JoinTopic(topic); err != nil {
-		return nil, errors.Wrapf(err, "cannot join pubsub topic %x", topic)
-	} else {
-		host.joined[topic] = t
-		return t, nil
-	}
-}
-
-// SendMessageToGroups sends a message to one or more multicast groups.
-// It returns a nil error if and only if it has succeeded to schedule the given
-// message for sending.
-func (host *HostV2) SendMessageToGroups(groups []nodeconfig.GroupID, msg []byte) (err error) {
-	for _, group := range groups {
-		t, e := host.getTopic(string(group))
-		if e != nil {
-			err = e
-			continue
-		}
-		e = t.Publish(context.Background(), msg)
-		if e != nil {
-			err = e
-			continue
+	for {
+		if err := node.PeerHost.Connect(ctx, *rdvpeer); err != nil {
+			fmt.Println("something busted, why", err.Error())
+			opts.Logger.Error().Err(err).Msg("cannot dial rendezvous point")
+		} else {
+			// fmt.Println(
+			// 	"was able to connect to the rendevous peer",
+			// 	rdvpeer.Loggable(),
+			// 	rdvpeer.String(),
+			// )
+			break
 		}
 	}
-	return err
-}
 
-// AddPeer add p2p.Peer into Peerstore
-func (host *HostV2) AddPeer(p *Peer) error {
-	if p.PeerID != "" && len(p.Addrs) != 0 {
-		host.Peerstore().AddAddrs(p.PeerID, p.Addrs, libp2p_peerstore.PermanentAddrTTL)
-		return nil
-	}
+	own.PeerID = node.PeerHost.ID()
 
-	if p.PeerID == "" {
-		host.logger.Error().Msg("AddPeer PeerID is EMPTY")
-		return fmt.Errorf("AddPeer error: peerID is empty")
-	}
-
-	// reconstruct the multiaddress based on ip/port
-	// PeerID has to be known for the ip/port
-	addr := fmt.Sprintf("/ip4/%s/tcp/%s", p.IP, p.Port)
-	targetAddr, err := ma.NewMultiaddr(addr)
-	if err != nil {
-		host.logger.Error().Err(err).Msg("AddPeer NewMultiaddr error")
-		return err
-	}
-
-	p.Addrs = append(p.Addrs, targetAddr)
-	host.Peerstore().AddAddrs(p.PeerID, p.Addrs, libp2p_peerstore.PermanentAddrTTL)
-	host.logger.Info().Interface("peer", *p).Msg("AddPeer add to libp2p_peerstore")
-	return nil
-}
-
-// Peerstore returns the peer store
-func (host *HostV2) Peerstore() libp2p_peerstore.Peerstore {
-	return host.h.Peerstore()
-}
-
-// GetID returns ID.Pretty
-func (host *HostV2) GetID() libp2p_peer.ID {
-	return host.h.ID()
-}
-
-// GetSelfPeer gets self peer
-func (host *HostV2) GetSelfPeer() Peer {
-	return host.self
-}
-
-// GetP2PHost returns the p2p.Host
-func (host *HostV2) GetP2PHost() libp2p_host.Host {
-	return host.h
-}
-
-// GetPeerCount ...
-func (host *HostV2) GetPeerCount() int {
-	return host.h.Peerstore().Peers().Len()
-}
-
-// ConnectHostPeer connects to peer host
-func (host *HostV2) ConnectHostPeer(peer Peer) error {
-	ctx := context.Background()
-	addr := fmt.Sprintf("/ip4/%s/tcp/%s/ipfs/%s", peer.IP, peer.Port, peer.PeerID.Pretty())
-	peerAddr, err := ma.NewMultiaddr(addr)
-	if err != nil {
-		host.logger.Error().Err(err).Interface("peer", peer).Msg("ConnectHostPeer")
-		return err
-	}
-	peerInfo, err := libp2p_peer.AddrInfoFromP2pAddr(peerAddr)
-	if err != nil {
-		host.logger.Error().Err(err).Interface("peer", peer).Msg("ConnectHostPeer")
-		return err
-	}
-	if err := host.h.Connect(ctx, *peerInfo); err != nil {
-		host.logger.Warn().Err(err).Interface("peer", peer).Msg("can't connect to peer")
-		return err
-	}
-	host.logger.Info().Interface("node", *peerInfo).Msg("connected to peer host")
-	return nil
-}
-
-// AllTopics ..
-func (host *HostV2) AllTopics() []*libp2p_pubsub.Topic {
-	host.lock.Lock()
-	defer host.lock.Unlock()
-	topics := []*libp2p_pubsub.Topic{}
-	for _, g := range host.joined {
-		topics = append(topics, g)
-	}
-	return topics
+	return &hmyHost{
+		coreAPI: api,
+		node:    node,
+		log:     opts.Logger,
+		ownPeer: own,
+		lock:    sync.Mutex{},
+		joined:  map[string]ipfs_interface.PubSubSubscription{},
+	}, nil
 }
 
 // ConstructMessage constructs the p2p message as [messageType, contentSize, content]
@@ -260,7 +246,7 @@ func ConstructMessage(content []byte) []byte {
 }
 
 // AddrList is a list of multiaddress
-type AddrList []ma.Multiaddr
+type AddrList []multiaddr.Multiaddr
 
 // String is a function to print a string representation of the AddrList
 func (al *AddrList) String() string {
@@ -277,7 +263,7 @@ func (al *AddrList) Set(value string) error {
 		return fmt.Errorf("AddrList is already set")
 	}
 	for _, a := range strings.Split(value, ",") {
-		addr, err := ma.NewMultiaddr(a)
+		addr, err := multiaddr.NewMultiaddr(a)
 		if err != nil {
 			return err
 		}
@@ -285,25 +271,3 @@ func (al *AddrList) Set(value string) error {
 	}
 	return nil
 }
-
-// StringsToAddrs convert a list of strings to a list of multiaddresses
-func StringsToAddrs(addrStrings []string) (maddrs []ma.Multiaddr, err error) {
-	for _, addrString := range addrStrings {
-		addr, err := ma.NewMultiaddr(addrString)
-		if err != nil {
-			return maddrs, err
-		}
-		maddrs = append(maddrs, addr)
-	}
-	return
-}
-
-// DefaultBootNodeAddrStrings is a list of Harmony
-// bootnodes address. Used to find other peers in the network.
-var DefaultBootNodeAddrStrings = []string{
-	"/ip4/127.0.0.1/tcp/19876/p2p/Qmc1V6W7BwX8Ugb42Ti8RnXF1rY5PF7nnZ6bKBryCgi6cv",
-}
-
-// BootNodes is a list of boot nodes.
-// It is populated either from default or from user CLI input.
-var BootNodes AddrList
