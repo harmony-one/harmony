60e9c5d7a297a30bb3f489311eca7441a7ba99dc
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-04-30 14:54:06 -0700

    [main] Start all threads

--- cmd/harmony/main.go
+++ cmd/harmony/main.go
@@ -779,31 +779,28 @@ func main() {
 
 	var g errgroup.Group
 
-	// g.Go(currentNode.HandleIncomingBlock)
+	g.Go(currentNode.HandleIncomingBlock)
 	g.Go(currentNode.StartP2PMessageHandling)
 
 	if currentNode.NodeConfig.Role() == nodeconfig.Validator {
-		// g.Go(currentNode.HandleConsensusBlockProcessing)
-		// g.Go(currentNode.HandleConsensusMessageProcessing)
-		// currentNode.Consensus.SetMode(consensus.Normal)
+		g.Go(currentNode.HandleConsensusBlockProcessing)
+		g.Go(currentNode.HandleConsensusMessageProcessing)
 
-		// g.Go(currentNode.StartLeaderWork)
+		g.Go(currentNode.StartLeaderWork)
 		go func() {
 			time.Sleep(time.Second * 3)
 			currentNode.Consensus.ProposalNewBlock <- struct{}{}
 			currentNode.Consensus.SetNextBlockDue(time.Now().Add(consensus.BlockTime))
 		}()
 
-		// g.Go(currentNode.BootstrapConsensus)
-		// g.Go(currentNode.EnsureConsensusLiviness)
+		g.Go(currentNode.BootstrapConsensus)
+		g.Go(currentNode.EnsureConsensusLiviness)
 		// g.Go(currentNode.EnsureConsensusInSync)
 
 	}
 
 	// if currentNode.NodeConfig.ShardID != shard.BeaconChainShardID &&
 	// 	currentNode.NodeConfig.Role() != nodeconfig.ExplorerNode {
-	// g.Go(currentNode.StartBeaconBlockStateSync)
-
 	g.Go(currentNode.StartStateSyncStreams)
 
 	// }
