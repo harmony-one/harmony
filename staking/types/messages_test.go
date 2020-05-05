e2c34b5e8391c5f1d0d906d2f710606f3c9d12b1
Jacky Wang <jackyw.se@gmail.com>
2020-05-04 18:04:09 -0700

    [staking] implemented Delegate.Copy and related unit test case

--- staking/types/messages_test.go
+++ staking/types/messages_test.go
@@ -28,7 +28,7 @@ func TestCreateValidator_Copy(t *testing.T) {
 	tests := []struct {
 		cv CreateValidator
 	}{
-		{testCreateValidator}, // normal values
+		{testCreateValidator}, // non-zero values
 		{zeroCreateValidator}, // zero values
 		{CreateValidator{}},   // empty values
 	}
@@ -45,7 +45,7 @@ func TestEditValidator_Copy(t *testing.T) {
 	tests := []struct {
 		ev EditValidator
 	}{
-		{testEditValidator}, // normal values
+		{testEditValidator}, // non-zero values
 		{zeroEditValidator}, // zero values
 		{EditValidator{}},   // empty values
 	}
@@ -56,6 +56,132 @@ func TestEditValidator_Copy(t *testing.T) {
 			t.Errorf("Test %v: %v", i, err)
 		}
 	}
+}
+
+func TestDelegate_Copy(t *testing.T) {
+	tests := []struct {
+		d Delegate
+	}{
+		{testDelegate}, // non-zero values
+		{zeroDelegate}, // zero values
+		{Delegate{}},   // empty values
+	}
+	for i, test := range tests {
+		cp := test.d.Copy().(Delegate)
+
+		if err := assertDelegateDeepEqual(test.d, cp); err != nil {
+			t.Errorf("Test %v: %v", i, err)
+		}
+	}
+}
+
+func assertCreateValidatorDeepCopy(cv1, cv2 CreateValidator) error {
+	if !reflect.DeepEqual(cv1, cv2) {
+		return fmt.Errorf("not deep equal")
+	}
+	if &cv1.Description == &cv2.Description {
+		return fmt.Errorf("description not copy")
+	}
+	if err := assertCommissionRatesCopy(cv1.CommissionRates, cv2.CommissionRates); err != nil {
+		return fmt.Errorf("commissionRate %v", err)
+	}
+	if err := assertBigIntCopy(cv1.MinSelfDelegation, cv2.MinSelfDelegation); err != nil {
+		return fmt.Errorf("MinSelfDelegation %v", err)
+	}
+	if err := assertBigIntCopy(cv1.MaxTotalDelegation, cv2.MaxTotalDelegation); err != nil {
+		return fmt.Errorf("MaxTotalDelegation %v", err)
+	}
+	if err := assertPubsCopy(cv1.SlotPubKeys, cv2.SlotPubKeys); err != nil {
+		return fmt.Errorf("SlotPubKeys %v", err)
+	}
+	if err := assertSigsCopy(cv1.SlotKeySigs, cv2.SlotKeySigs); err != nil {
+		return fmt.Errorf("SlotKeySigs %v", err)
+	}
+	if err := assertBigIntCopy(cv1.Amount, cv2.Amount); err != nil {
+		return fmt.Errorf("amount %v", err)
+	}
+	return nil
+}
+
+func assertEditValidatorDeepCopy(ev1, ev2 EditValidator) error {
+	if !reflect.DeepEqual(ev1, ev2) {
+		return errors.New("not deep equal")
+	}
+	if &ev1.ValidatorAddress == &ev2.ValidatorAddress {
+		return fmt.Errorf("validator address same pointer")
+	}
+	if &ev1.Description == &ev2.Description {
+		return fmt.Errorf("description same pointer")
+	}
+	if ev1.CommissionRate != nil && ev1.CommissionRate == ev2.CommissionRate {
+		return fmt.Errorf("CommissionRate same pointer")
+	}
+	if err := assertBigIntCopy(ev1.MinSelfDelegation, ev2.MinSelfDelegation); err != nil {
+		return fmt.Errorf("MinSelfDelegation %v", err)
+	}
+	if err := assertBigIntCopy(ev1.MaxTotalDelegation, ev2.MaxTotalDelegation); err != nil {
+		return fmt.Errorf("MaxTotalDelegation %v", err)
+	}
+	if ev1.SlotKeyToRemove != nil && ev1.SlotKeyToRemove == ev2.SlotKeyToRemove {
+		return fmt.Errorf("SlotKeyToRemove same pointer")
+	}
+	if ev1.SlotKeyToAdd != nil && ev1.SlotKeyToAdd == ev2.SlotKeyToAdd {
+		return fmt.Errorf("SlotKeyToAdd same pointer")
+	}
+	if ev1.SlotKeyToAddSig != nil && ev1.SlotKeyToAddSig == ev2.SlotKeyToAddSig {
+		return fmt.Errorf("SlotKeyToAddSig same pointer")
+	}
+	return nil
+}
+
+func assertDelegateDeepEqual(d1, d2 Delegate) error {
+	if !reflect.DeepEqual(d1, d2) {
+		return fmt.Errorf("not deep equal")
+	}
+	if &d1.DelegatorAddress == &d2.DelegatorAddress {
+		return fmt.Errorf("DelegatorAddress same pointer")
+	}
+	if &d1.ValidatorAddress == &d2.ValidatorAddress {
+		return fmt.Errorf("ValidatorAddress same pointer")
+	}
+	if d1.Amount != nil && d1.Amount == d2.Amount {
+		return fmt.Errorf("amount same pointer")
+	}
+	return nil
+}
+
+func assertBigIntCopy(i1, i2 *big.Int) error {
+	if (i1 == nil) != (i2 == nil) {
+		return errors.New("is nil not equal")
+	}
+	if i1 != nil && i1 == i2 {
+		return errors.New("not copy")
+	}
+	return nil
+}
+
+func assertPubsCopy(s1, s2 []shard.BLSPublicKey) error {
+	if len(s1) != len(s2) {
+		return fmt.Errorf("size not equal")
+	}
+	for i := range s1 {
+		if &s1[i] == &s2[i] {
+			return fmt.Errorf("[%v] same address", i)
+		}
+	}
+	return nil
+}
+
+func assertSigsCopy(s1, s2 []shard.BLSSignature) error {
+	if len(s1) != len(s2) {
+		return fmt.Errorf("size not equal")
+	}
+	for i := range s1 {
+		if &s1[i] == &s2[i] {
+			return fmt.Errorf("[%v] same address", i)
+		}
+	}
+	return nil
 }
 
 func cpTestDataSetup() {
@@ -140,103 +266,6 @@ func cpTestDataSetup() {
 		DelegatorAddress: common.BigToAddress(common.Big1),
 	}
 	zeroCollectReward = CollectRewards{}
-}
-
-func assertCreateValidatorDeepCopy(cv1, cv2 CreateValidator) error {
-	if !reflect.DeepEqual(cv1, cv2) {
-		return fmt.Errorf("not deep equal")
-	}
-	if &cv1.Description == &cv2.Description {
-		return fmt.Errorf("description not copy")
-	}
-	if err := assertCommissionRatesCopy(cv1.CommissionRates, cv2.CommissionRates); err != nil {
-		return fmt.Errorf("commissionRate %v", err)
-	}
-	if err := assertBigIntCopy(cv1.MinSelfDelegation, cv2.MinSelfDelegation); err != nil {
-		return fmt.Errorf("MinSelfDelegation %v", err)
-	}
-	if err := assertBigIntCopy(cv1.MaxTotalDelegation, cv2.MaxTotalDelegation); err != nil {
-		return fmt.Errorf("MaxTotalDelegation %v", err)
-	}
-	if err := assertPubsCopy(cv1.SlotPubKeys, cv2.SlotPubKeys); err != nil {
-		return fmt.Errorf("SlotPubKeys %v", err)
-	}
-	if err := assertSigsCopy(cv1.SlotKeySigs, cv2.SlotKeySigs); err != nil {
-		return fmt.Errorf("SlotKeySigs %v", err)
-	}
-	if err := assertBigIntCopy(cv1.Amount, cv2.Amount); err != nil {
-		return fmt.Errorf("amount %v", err)
-	}
-	return nil
-}
-
-func assertEditValidatorDeepCopy(ev1, ev2 EditValidator) error {
-	if !reflect.DeepEqual(ev1, ev2) {
-		return errors.New("not deep equal")
-	}
-	if &ev1.ValidatorAddress == &ev2.ValidatorAddress {
-		return fmt.Errorf("validator address same pointer")
-	}
-	if &ev1.Description == &ev2.Description {
-		return fmt.Errorf("description same pointer")
-	}
-	if ev1.CommissionRate != nil && ev1.CommissionRate == ev2.CommissionRate {
-		return fmt.Errorf("CommissionRate same pointer")
-	}
-	if err := assertBigIntCopy(ev1.MinSelfDelegation, ev2.MinSelfDelegation); err != nil {
-		return fmt.Errorf("MinSelfDelegation %v", err)
-	}
-	if err := assertBigIntCopy(ev1.MaxTotalDelegation, ev2.MaxTotalDelegation); err != nil {
-		return fmt.Errorf("MaxTotalDelegation %v", err)
-	}
-	if ev1.SlotKeyToRemove != nil && ev1.SlotKeyToRemove == ev2.SlotKeyToRemove {
-		return fmt.Errorf("SlotKeyToRemove same pointer")
-	}
-	if ev1.SlotKeyToAdd != nil && ev1.SlotKeyToAdd == ev2.SlotKeyToAdd {
-		return fmt.Errorf("SlotKeyToAdd same pointer")
-	}
-	if ev1.SlotKeyToAddSig != nil && ev1.SlotKeyToAddSig == ev2.SlotKeyToAddSig {
-		return fmt.Errorf("SlotKeyToAddSig same pointer")
-	}
-	return nil
-}
-
-func assertDelegateDeepEqual(d1, d2 Delegate) error {
-	return nil
-}
-
-func assertBigIntCopy(i1, i2 *big.Int) error {
-	if (i1 == nil) != (i2 == nil) {
-		return errors.New("is nil not equal")
-	}
-	if i1 != nil && i1 == i2 {
-		return errors.New("not copy")
-	}
-	return nil
-}
-
-func assertPubsCopy(s1, s2 []shard.BLSPublicKey) error {
-	if len(s1) != len(s2) {
-		return fmt.Errorf("size not equal")
-	}
-	for i := range s1 {
-		if &s1[i] == &s2[i] {
-			return fmt.Errorf("[%v] same address", i)
-		}
-	}
-	return nil
-}
-
-func assertSigsCopy(s1, s2 []shard.BLSSignature) error {
-	if len(s1) != len(s2) {
-		return fmt.Errorf("size not equal")
-	}
-	for i := range s1 {
-		if &s1[i] == &s2[i] {
-			return fmt.Errorf("[%v] same address", i)
-		}
-	}
-	return nil
 }
 
 // var (
