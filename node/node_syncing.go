8122c101ea950837af8b3a0150de6a6f7929fd74
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-05-10 23:08:15 -0700

    [node] Sync race condition

--- node/node_syncing.go
+++ node/node_syncing.go
@@ -410,6 +410,8 @@ func (node *Node) CalculateResponse(request *downloader_pb.DownloaderRequest, in
 
 	// this is the out of sync node acts as grpc server side
 	case downloader_pb.DownloaderRequest_NEWBLOCK:
+		node.stateMutex.Lock()
+		defer node.stateMutex.Unlock()
 		if node.State != NodeNotInSync {
 			utils.Logger().Debug().
 				Str("state", node.State.String()).
