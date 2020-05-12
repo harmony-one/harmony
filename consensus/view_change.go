0dc5ca007019d5e9e70a71440759ebade1e61f2c
Rongjian Lan <rongjian.lan@gmail.com>
2020-05-12 16:01:29 -0700

    fix crash on parsed block

--- consensus/view_change.go
+++ consensus/view_change.go
@@ -226,6 +226,7 @@ func (consensus *Consensus) onViewChange(msg *msg_pb.Message) {
 	}
 
 	preparedBlock := &types.Block{}
+	hasBlock := false
 	if len(recvMsg.Payload) != 0 && len(recvMsg.Block) != 0 {
 		if err := rlp.DecodeBytes(recvMsg.Block, preparedBlock); err != nil {
 			consensus.getLogger().Warn().
@@ -234,10 +235,11 @@ func (consensus *Consensus) onViewChange(msg *msg_pb.Message) {
 				Msg("[onViewChange] Unparseable prepared block data")
 			return
 		}
+		hasBlock = true
 	}
 
 	// m2 type message
-	if preparedBlock == nil {
+	if !hasBlock {
 		_, ok := consensus.nilSigs[recvMsg.ViewID][senderKey.SerializeToHexStr()]
 		if ok {
 			consensus.getLogger().Debug().
@@ -505,6 +507,7 @@ func (consensus *Consensus) onNewView(msg *msg_pb.Message) {
 
 	// check when M3 sigs > M2 sigs, then M1 (recvMsg.Payload) should not be empty
 	preparedBlock := &types.Block{}
+	hasBlock := false
 	if len(recvMsg.Payload) != 0 && len(recvMsg.Block) != 0 {
 		if err := rlp.DecodeBytes(recvMsg.Block, preparedBlock); err != nil {
 			consensus.getLogger().Warn().
@@ -524,6 +527,7 @@ func (consensus *Consensus) onNewView(msg *msg_pb.Message) {
 				Msg("[onNewView] Prepared block hash doesn't match msg block hash.")
 			return
 		}
+		hasBlock = true
 	}
 
 	if m2Mask == nil || m2Mask.Bitmap == nil ||
@@ -563,7 +567,9 @@ func (consensus *Consensus) onNewView(msg *msg_pb.Message) {
 		preparedMsg.SenderPubkey = senderKey
 		consensus.FBFTLog.AddMessage(&preparedMsg)
 
-		consensus.FBFTLog.AddBlock(preparedBlock)
+		if hasBlock {
+			consensus.FBFTLog.AddBlock(preparedBlock)
+		}
 	}
 
 	// newView message verified success, override my state
@@ -582,7 +588,7 @@ func (consensus *Consensus) onNewView(msg *msg_pb.Message) {
 	}
 
 	// NewView message is verified, change state to normal consensus
-	if preparedBlock != nil {
+	if hasBlock {
 		// Construct and send the commit message
 		if consensus.BlockVerifier(preparedBlock); err != nil {
 			consensus.getLogger().Error().Err(err).Msg("[onNewView] Prepared block verification failed")
