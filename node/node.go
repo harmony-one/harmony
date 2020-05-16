54a504d5414ee1ca8813dc7668736423079d7dbd
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-05-16 10:48:10 -0700

    [node] Bump handlers by order of magnitude

--- node/node.go
+++ node/node.go
@@ -357,7 +357,7 @@ func (node *Node) Start() error {
 		return errors.New("have no topics to listen to")
 	}
 	weighted := make([]*semaphore.Weighted, len(allTopics))
-	const maxMessageHandlers = 200
+	const maxMessageHandlers = 2000
 	ctx := context.Background()
 	ownID := node.host.GetID()
 	errChan := make(chan error)
