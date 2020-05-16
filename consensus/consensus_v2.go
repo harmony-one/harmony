3807b40dceb5dc41eafb1be85b1f0997c1c73d1a
Rongjian Lan <rongjian.lan@gmail.com>
2020-05-16 14:07:12 -0700

    add nil check on getHeader

--- consensus/consensus_v2.go
+++ consensus/consensus_v2.go
@@ -531,6 +531,9 @@ func (consensus *Consensus) GenerateVrfAndProof(newBlock *types.Block, vrfBlockN
 	previousHeader := consensus.ChainReader.GetHeaderByNumber(
 		newBlock.NumberU64() - 1,
 	)
+	if previousHeader == nil {
+		return vrfBlockNumbers
+	}
 	previousHash := previousHeader.Hash()
 	copy(blockHash[:], previousHash[:])
 
@@ -553,6 +556,10 @@ func (consensus *Consensus) ValidateVrfAndProof(headerObj *block.Header) bool {
 	previousHeader := consensus.ChainReader.GetHeaderByNumber(
 		headerObj.Number().Uint64() - 1,
 	)
+	if previousHeader == nil {
+		return false
+	}
+
 	previousHash := previousHeader.Hash()
 	copy(blockHash[:], previousHash[:])
 	vrfProof := [96]byte{}
