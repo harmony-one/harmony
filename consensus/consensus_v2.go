f2427d47f042ee44656c419d4ceedb1e0d579e9e
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-05-16 18:52:21 -0700

    [consensus] Try with lower tick

--- consensus/consensus_v2.go
+++ consensus/consensus_v2.go
@@ -349,7 +349,7 @@ func (consensus *Consensus) Start(
 		}
 		consensus.getLogger().Info().Time("time", time.Now()).Msg("[ConsensusMainLoop] Consensus started")
 		defer close(stoppedChan)
-		ticker := time.NewTicker(3 * time.Second)
+		ticker := time.NewTicker(time.Second)
 		defer ticker.Stop()
 		consensus.consensusTimeout[timeoutBootstrap].Start()
 		consensus.getLogger().Debug().
