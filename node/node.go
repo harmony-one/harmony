02469a4b9cced65175060b428e60c2b2cb79128e
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-05-16 18:23:57 -0700

    [node] Bump timeout on message handle since now block

--- node/node.go
+++ node/node.go
@@ -513,7 +513,7 @@ func (node *Node) Start() error {
 		go func() {
 
 			for msg := range msgChan {
-				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 				msg := msg
 
 				go func() {
