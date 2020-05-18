ba1daa26d21b5e7f65875c150c88b48cd742eccc
Rongjian Lan <rongjian.lan@gmail.com>
2020-05-18 11:09:40 -0700

    adjust catch up speed to be a sane number

--- node/node_cross_link.go
+++ node/node_cross_link.go
@@ -91,7 +91,7 @@ func (node *Node) ProcessCrossLinkMessage(msgPayload []byte) {
 			Msgf("[ProcessingCrossLink] Received crosslinks: %d", len(crosslinks))
 
 		for i, cl := range crosslinks {
-			if i > crossLinkBatchSize*10 { // A sanity check to prevent spamming
+			if i > crossLinkBatchSize*2 { // A sanity check to prevent spamming
 				break
 			}
 
