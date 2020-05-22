1810142077cfa29e01c59485a83fd2bc66124fc0
Jacky Wang <jackyw.se@gmail.com>
2020-05-22 00:32:30 -0700

    [network] Limit client sync connection (#3071)
    * [network] limit client connected peers
    * Changed signature and added tests

--- api/service/syncing/syncing_test.go
+++ api/service/syncing/syncing_test.go
@@ -1,9 +1,12 @@
 package syncing
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/harmony-one/harmony/api/service/syncing/downloader"
+	"github.com/harmony-one/harmony/p2p"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -48,4 +51,70 @@ func TestCreateStateSync(t *testing.T) {
 	if stateSync == nil {
 		t.Error("Unable to create stateSync")
 	}
+}
+
+func TestLimitPeersWithBound(t *testing.T) {
+	tests := []struct {
+		size    int
+		expSize int
+	}{
+		{0, 0},
+		{1, 1},
+		{3, 3},
+		{4, 3},
+		{7, 3},
+		{8, 4},
+		{10, 5},
+		{11, 5},
+		{100, 5},
+	}
+	for _, test := range tests {
+		ps := makePeersForTest(test.size)
+
+		res := limitNumPeers(ps, 1)
+
+		if len(res) != test.expSize {
+			t.Errorf("result size unexpected: %v / %v", len(res), test.expSize)
+		}
+		if err := checkTestPeerDuplicity(res); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestLimitPeersWithBound_random(t *testing.T) {
+	ps1 := makePeersForTest(100)
+	ps2 := makePeersForTest(100)
+	s1, s2 := int64(1), int64(2)
+
+	res1 := limitNumPeers(ps1, s1)
+	res2 := limitNumPeers(ps2, s2)
+	if reflect.DeepEqual(res1, res2) {
+		t.Fatal("not randomized limit peer")
+	}
+}
+
+func makePeersForTest(size int) []p2p.Peer {
+	ps := make([]p2p.Peer, 0, size)
+	for i := 0; i != size; i++ {
+		ps = append(ps, p2p.Peer{
+			IP: makeTestPeerIP(i),
+		})
+	}
+	return ps
+}
+
+func checkTestPeerDuplicity(ps []p2p.Peer) error {
+	m := make(map[string]struct{})
+	for _, p := range ps {
+		if _, ok := m[p.IP]; ok {
+			return fmt.Errorf("duplicate ip")
+		}
+		m[p.IP] = struct{}{}
+	}
+	return nil
+}
+
+func makeTestPeerIP(i interface{}) string {
+	return fmt.Sprintf("%v", i)
 }
