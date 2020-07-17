5fda02771d035574d366b7d292b46ecbcb39444f
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-07-17 07:22:30 -0700

    [test] Fix merge conflict for deploy.sh

--- test/deploy.sh
+++ test/deploy.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 unset -v progdir
 case "${0}" in
 */*) progdir="${0%/*}" ;;
-*) progdir=.  ;;
+*) progdir=. ;;
 esac
 
 ROOT="${progdir}/.."
@@ -14,122 +14,122 @@ OS=$(uname -s)
 . "${ROOT}/scripts/setup_bls_build_flags.sh"
 
 function cleanup() {
-   "${progdir}/kill_node.sh"
+  "${progdir}/kill_node.sh"
 }
 
 function build() {
-    pushd ${ROOT}
-    export GO111MODULE=on
-    if [[ "$OS" = "Darwin" ]]; then
-        # MacOS doesn't support static build
-        scripts/go_executable_build.sh -S
-    else
-        # Static build on Linux platform
-        scripts/go_executable_build.sh -s
-    fi
-    popd
+  pushd ${ROOT}
+  export GO111MODULE=on
+  if [[ "$OS" == "Darwin" ]]; then
+    # MacOS doesn't support static build
+    scripts/go_executable_build.sh -S
+  else
+    # Static build on Linux platform
+    scripts/go_executable_build.sh -s
+  fi
+  popd
 }
 
 function setup() {
-    # Setup blspass file
-    mkdir -p ${ROOT}/.hmy
-    if [[ ! -f "${ROOT}/.hmy/blspass.txt" ]]; then
-        touch "${ROOT}/.hmy/blspass.txt"
-    fi
+  # Setup blspass file
+  mkdir -p ${ROOT}/.hmy
+  if [[ ! -f "${ROOT}/.hmy/blspass.txt" ]]; then
+    touch "${ROOT}/.hmy/blspass.txt"
+  fi
 
-    # Kill nodes if any
-    cleanup
+  # Kill nodes if any
+  cleanup
 
-    # Note that the binarys only works on MacOS & Linux
-    if [[ "${NOBUILD}" != "true" ]]; then
-        build
-    fi
+  # Note that the binarys only works on MacOS & Linux
+  if [[ "${NOBUILD}" != "true" ]]; then
+    build
+  fi
 
-    # Create a tmp folder for logs
-    t=`date +"%Y%m%d-%H%M%S"`
-    log_folder="${ROOT}/tmp_log/log-$t"
-    mkdir -p ${log_folder}
-    LOG_FILE=${log_folder}/r.log
+  # Create a tmp folder for logs
+  t=$(date +"%Y%m%d-%H%M%S")
+  log_folder="${ROOT}/tmp_log/log-$t"
+  mkdir -p "${log_folder}"
+  LOG_FILE=${log_folder}/r.log
 }
 
-function launch_bootnode(){
-    echo "launching boot node ..."
-    ${DRYRUN} ${ROOT}/bin/bootnode -port 19876 > ${log_folder}/bootnode.log 2>&1 | tee -a ${LOG_FILE} &
-    sleep 1
-    BN_MA=$(grep "BN_MA" ${log_folder}/bootnode.log | awk -F\= ' { print $2 } ')
-    echo "bootnode launched." + " $BN_MA"
+function launch_bootnode() {
+  echo "launching boot node ..."
+  ${DRYRUN} ${ROOT}/bin/bootnode -port 19876 >"${log_folder}"/bootnode.log 2>&1 | tee -a "${LOG_FILE}" &
+  sleep 1
+  BN_MA=$(grep "BN_MA" "${log_folder}"/bootnode.log | awk -F\= ' { print $2 } ')
+  echo "bootnode launched." + " $BN_MA"
 }
 
-function launch_localnet(){
-    launch_bootnode
+function launch_localnet() {
+  launch_bootnode
 
-    unset -v base_args
-    declare -a base_args args
+  unset -v base_args
+  declare -a base_args args
 
-    if ${VERBOSE}; then
-       verbosity=5
+  if ${VERBOSE}; then
+    verbosity=5
+  else
+    verbosity=3
+  fi
+
+  base_args=(-log_folder "${log_folder}" -min_peers "${MIN}" -bootnodes "${BN_MA}" "-network_type=$NETWORK" -blspass file:"${ROOT}/.hmy/blspass.txt" "-dns=false" "-verbosity=${verbosity}")
+  sleep 2
+
+  # Start nodes
+  i=-1
+  while IFS='' read -r line || [[ -n "$line" ]]; do
+    i=$((i + 1))
+
+    # Read config for i-th node form config file
+    IFS=' ' read -r ip port mode bls_key <<<"${line}"
+    args=("${base_args[@]}" -ip "${ip}" -port "${port}" -key "/tmp/${ip}-${port}.key" -db_dir "${ROOT}/db-${ip}-${port}" "-broadcast_invalid_tx=true")
+    if [[ -z "$ip" || -z "$port" ]]; then
+      echo "skip empty node"
+      continue
+    fi
+
+    # Setup BLS key for i-th localnet node
+    if [[ ! -e "$bls_key" ]]; then
+      args=("${args[@]}" -blskey_file "BLSKEY")
+    elif [[ -f "$bls_key" ]]; then
+      args=("${args[@]}" -blskey_file "${ROOT}/${bls_key}")
+    elif [[ -d "$bls_key" ]]; then
+      args=("${args[@]}" -blsfolder "${ROOT}/${bls_key}")
     else
-       verbosity=3
+      echo "skipping unknown node"
+      continue
     fi
 
-    base_args=(-log_folder "${log_folder}" -min_peers "${MIN}" -bootnodes "${BN_MA}" -network_type="$NETWORK" -blspass file:"${ROOT}/.hmy/blspass.txt" -dns=false -verbosity="${verbosity}")
-    sleep 2
+    # Setup flags for i-th node based on config
+    case "${mode}" in
+    explorer)
+      args=("${args[@]}" "-node_type=explorer" "-shard_id=0")
+      ;;
+    archival)
+      args=("${args[@]}" -is_archival)
+      ;;
+    leader)
+      args=("${args[@]}" -is_leader)
+      ;;
+    external)
+      args=("${args[@]}" "-staking=true")
+      ;;
+    client)
+      continue
+      ;;
+    esac
 
-    # Start nodes
-    i=-1
-    while IFS='' read -r line || [[ -n "$line" ]]; do
-        i=$((i+1))
-
-        # Read config for i-th node form config file
-        IFS=' ' read ip port mode bls_key account <<< ${line}
-            args=("${base_args[@]}" -ip "${ip}" -port "${port}" -key "/tmp/${ip}-${port}.key" -db_dir "${ROOT}/db-${ip}-${port}" -broadcast_invalid_tx=true)
-        if [[ -z "$ip" || -z "$port" ]]; then
-            echo "skip empty node"
-            continue
-        fi
-
-        # Setup BLS key for i-th localnet node
-        if [[ ! -e "$bls_key" ]]; then
-            args=("${args[@]}" -blskey_file "BLSKEY")
-        elif [[ -f "$bls_key" ]]; then
-            args=("${args[@]}" -blskey_file "${ROOT}/${bls_key}")
-        elif [[ -d "$bls_key" ]]; then
-            args=("${args[@]}" -blsfolder "${ROOT}/${bls_key}")
-        else
-            echo "skipping unknown node"
-            continue
-        fi
-
-        # Setup flags for i-th node based on config
-        case "${mode}" in
-        explorer)
-            args=("${args[@]}" -node_type=explorer -shard_id=0)
-            ;;
-        archival|archival)
-            args=("${args[@]}" -is_archival)
-            ;;
-        leader)
-            args=("${args[@]}" -is_leader)
-            ;;
-        external)
-            args=("${args[@]}" -staking=true)
-            ;;
-        client)
-            continue
-            ;;
-        esac
-
-        # Start the node
-        ${DRYRUN} "${ROOT}/bin/harmony" "${args[@]}" "${extra_args[@]}" 2>&1 | tee -a "${LOG_FILE}" &
-    done < ${config}
+    # Start the node
+    ${DRYRUN} "${ROOT}/bin/harmony" "${args[@]}" "${extra_args[@]}" 2>&1 | tee -a "${LOG_FILE}" &
+  done <"${config}"
 }
 
-trap cleanup_and_result SIGINT SIGTERM
+trap cleanup SIGINT SIGTERM
 
-function usage {
-   local ME=$(basename $0)
+function usage() {
+  local ME=$(basename $0)
 
-   echo "
+  echo "
 USAGE: $ME [OPTIONS] config_file_name [extra args to node]
 
    -h             print this help message
@@ -147,31 +147,31 @@ EXAMPLES:
 
    $ME local_config.txt
 "
-   exit 0
+  exit 0
 }
 
 DURATION=60000
 MIN=3
 SHARDS=2
 DRYRUN=
-SYNC=true
 NETWORK=localnet
 VERBOSE=false
 
 while getopts "hD:m:s:nBN:v" option; do
-   case $option in
-      h) usage ;;
-      D) DURATION=$OPTARG ;;
-      m) MIN=$OPTARG ;;
-      s) SHARDS=$OPTARG ;;
-      n) DRYRUN=echo ;;
-      B) NOBUILD=true ;;
-      N) NETWORK=$OPTARG ;;
-      v) VERBOSE=true ;;
-   esac
+  case ${option} in
+  h) usage ;;
+  D) DURATION=$OPTARG ;;
+  m) MIN=$OPTARG ;;
+  s) SHARDS=$OPTARG ;;
+  n) DRYRUN=echo ;;
+  B) NOBUILD=true ;;
+  N) NETWORK=$OPTARG ;;
+  v) VERBOSE=true ;;
+  *) usage ;;
+  esac
 done
 
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 
 config=$1
 shift 1 || usage
@@ -181,5 +181,5 @@ extra_args=("$@")
 
 setup
 launch_localnet
-sleep ${DURATION}
-"${ROOT}/test/kill_node.sh" 2> /dev/null || true
+sleep "${DURATION}"
+cleanup || true
