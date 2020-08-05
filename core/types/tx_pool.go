ef8cb98d1c3ac90a0a42dd41a88923506c461248
Rongjian Lan <rongjian.lan@gmail.com>
2020-08-05 12:36:15 -0700

    raize txn size limit to 128kb (#3283)

--- core/types/tx_pool.go
+++ core/types/tx_pool.go
@@ -11,8 +11,8 @@ import (
 )
 
 const (
-	//MaxPoolTransactionDataSize is a 32KB heuristic data limit for DOS prevention
-	MaxPoolTransactionDataSize = 32 * 1024
+	//MaxPoolTransactionDataSize is a 128KB heuristic data limit for DOS prevention
+	MaxPoolTransactionDataSize = 128 * 1024
 	//MaxEncodedPoolTransactionSize is a heuristic raw/encoded data size limit. It has an additional 10KB for metadata
 	MaxEncodedPoolTransactionSize = MaxPoolTransactionDataSize + (10 * 1024)
 )
