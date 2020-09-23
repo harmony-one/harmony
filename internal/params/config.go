28c93b5427c7e1bfb180b5259b15234a97ff48b7
Rongjian Lan <rongjian.lan@gmail.com>
2020-09-23 11:10:36 -0700

    Postpone redelegation to epoch 290 (#3362)

--- internal/params/config.go
+++ internal/params/config.go
@@ -32,7 +32,7 @@ var (
 		PreStakingEpoch:   big.NewInt(185),
 		QuickUnlockEpoch:  big.NewInt(191),
 		FiveSecondsEpoch:  big.NewInt(230),
-		RedelegationEpoch: big.NewInt(289),
+		RedelegationEpoch: big.NewInt(290),
 		EIP155Epoch:       big.NewInt(28),
 		S3Epoch:           big.NewInt(28),
 		ReceiptLogEpoch:   big.NewInt(101),
