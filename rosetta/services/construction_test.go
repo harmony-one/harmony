95aab05314d9b0986f98aae664336e285697c050
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-09-30 23:53:00 -0700

    [rosetta] Add TestGetAddressFromKnownPublicKey
    Signed-off-by: Daniel Van Der Maden <dvandermaden0@berkeley.edu>

--- rosetta/services/construction_test.go
+++ rosetta/services/construction_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/coinbase/rosetta-sdk-go/types"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
@@ -37,5 +38,25 @@ func TestGetAddressFromPublicKey(t *testing.T) {
 	_, rosettaError = getAddressFromPublicKey(nil)
 	if rosettaError == nil {
 		t.Error("expected error")
+	}
+}
+
+func TestGetAddressFromKnownPublicKey(t *testing.T) {
+	refCompressKey := "0x033e4c030253cd932a73e24f1a852de98b67647e0e96c5a3aba905a26d1c09bd2a"
+	compressedPublicKey, _ := hexutil.Decode(refCompressKey)
+	addr, rosettaError := getAddressFromPublicKey(&types.PublicKey{
+		Bytes:     compressedPublicKey,
+		CurveType: types.Secp256k1,
+	})
+	if rosettaError != nil {
+		t.Fatal(rosettaError)
+	}
+	addrID, rosettaError := newAccountIdentifier(*addr)
+	if rosettaError != nil {
+		t.Fatal(rosettaError)
+	}
+	refB32Addr := "one1aaw9mcd8hcwela748rl3mn9c7phe7ujzdls5rg"
+	if addrID.Address != refB32Addr {
+		t.Error("account ID from key is incorrect")
 	}
 }
