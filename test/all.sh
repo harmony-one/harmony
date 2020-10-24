28a72f4aeeaddcfb196d3e6e6621340ed3e5ac2e
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-10-23 21:14:25 -0700

    [test] Add rosetta.sh & change api.sh to rpc.sh
    * Update Makefile for new tests
    
    Signed-off-by: Daniel Van Der Maden <dvandermaden0@berkeley.edu>

--- test/all.sh
+++ test/all.sh
@@ -2,4 +2,6 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 bash "$DIR/kill_node.sh"
 docker pull harmonyone/localnet-test
-docker run -it --expose 9000-9999 -v "$DIR/../:/go/src/github.com/harmony-one/harmony" harmonyone/localnet-test
+docker run -it \
+  -p 9500:9500 -p 9501:9501 -p 9599:9599 -p 9598:9598 -p 9799:9799 -p 9798:9798 -p 9899:9899 -p 9898:9898 \
+  -v "$DIR/../:/go/src/github.com/harmony-one/harmony" harmonyone/localnet-test
---
+++ test/rpc.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+bash "$DIR/kill_node.sh"
+
+docker_name="harmony-localnet-test"
+
+case ${1} in
+run)
+    docker pull harmonyone/localnet-test
+    docker rm "$docker_name"
+    docker run -it --name "$docker_name" \
+      -p 9500:9500 -p 9501:9501 -p 9599:9599 -p 9598:9598 -p 9799:9799 -p 9798:9798 -p 9899:9899 -p 9898:9898 \
+      -v "$DIR/../:/go/src/github.com/harmony-one/harmony" harmonyone/localnet-test -n -k
+    ;;
+attach)
+    docker exec -it "$docker_name" /bin/bash
+    ;;
+*)
+    echo "
+Node API tests
+
+Param:     Help:
+run        Run the Node API tests
+attach     Attach onto the Node API testing docker image for inspection
+"
+    exit 0
+    ;;
+esac
+
