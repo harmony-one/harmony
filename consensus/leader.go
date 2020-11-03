a34cd1ac4776663ca426ddfbee39130e2caaffaf
Rongjian Lan <rongjian.lan@gmail.com>
2020-11-02 16:43:18 -0800

    code cleanup

--- consensus/leader.go
+++ consensus/leader.go
@@ -3,10 +3,8 @@ package consensus
 import (
 	"time"
 
-	"github.com/harmony-one/harmony/internal/utils"
-
 	"github.com/harmony-one/harmony/crypto/bls"
-	nodeconfig "github.com/harmony-one/harmony/internal/configs/node"
+	"github.com/harmony-one/harmony/internal/configs/node"
 
 	"github.com/harmony-one/harmony/consensus/signature"
 
@@ -201,10 +199,6 @@ func (consensus *Consensus) onPrepare(msg *msg_pb.Message) {
 }
 
 func (consensus *Consensus) onCommit(msg *msg_pb.Message) {
-	utils.Logger().Info().Msgf("ViewChanging %d %d", consensus.GetCurBlockViewID(), consensus.GetViewChangingID())
-	if consensus.GetCurBlockViewID()%7 == 0 {
-		return
-	}
 	recvMsg, err := consensus.ParseFBFTMessage(msg)
 	if err != nil {
 		consensus.getLogger().Debug().Err(err).Msg("[OnCommit] Parse pbft message failed")
@@ -307,7 +301,7 @@ func (consensus *Consensus) onCommit(msg *msg_pb.Message) {
 		logger.Info().Msg("[OnCommit] 2/3 Enough commits received")
 
 		go func() {
-			// TODO: make it a channel
+			// TODO: make it synchronized with commitFinishChan
 			consensus.preCommitAndPropose(blockObj)
 		}()
 
