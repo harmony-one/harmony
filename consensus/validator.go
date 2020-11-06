8ca08d3ac37ec12cdcc154eb773c4941726ee167
Rongjian Lan <rongjian.lan@gmail.com>
2020-11-06 11:26:36 -0800

    revert import change

--- consensus/validator.go
+++ consensus/validator.go
@@ -5,7 +5,7 @@ import (
 	"time"
 
 	"github.com/harmony-one/harmony/crypto/bls"
-	"github.com/harmony-one/harmony/internal/configs/node"
+	nodeconfig "github.com/harmony-one/harmony/internal/configs/node"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/rlp"
