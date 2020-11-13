36fd9dcb1e3c2e0940cb17fae3f92b37aa09ab57
Rongjian Lan <rongjian.lan@gmail.com>
2020-11-13 00:22:49 -0800

    make commit sig timeout longer, also return early for onCommit

--- consensus/consensus_v2.go
+++ consensus/consensus_v2.go
@@ -32,10 +32,10 @@ var (
 // timeout constant
 const (
 	// CommitSigSenderTimeout is the timeout for sending the commit sig to finish block proposal
-	CommitSigSenderTimeout = 6 * time.Second
+	CommitSigSenderTimeout = 10 * time.Second
 	// CommitSigReceiverTimeout is the timeout for the receiving side of the commit sig
 	// if timeout, the receiver should instead ready directly from db for the commit sig
-	CommitSigReceiverTimeout = 4 * time.Second
+	CommitSigReceiverTimeout = 8 * time.Second
 )
 
 // IsViewChangingMode return true if curernt mode is viewchanging
