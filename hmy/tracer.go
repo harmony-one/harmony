d4823ffc99a974f6588d5bd54a9cdb0f0556be8b
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-11-20 17:55:16 -0800

    [tracer] Remove ptr to slice & map in StructLogRes
    Signed-off-by: Daniel Van Der Maden <dvandermaden0@berkeley.edu>

--- hmy/tracer.go
+++ hmy/tracer.go
@@ -719,17 +719,17 @@ type ExecutionResult struct {
 // StructLogRes stores a structured log emitted by the EVM while replaying a
 // transaction in debug mode
 type StructLogRes struct {
-	Pc            uint64             `json:"pc"`
-	Op            string             `json:"op"`
-	CallerAddress common.Address     `json:"callerAddress"`
-	CodeAddress   *common.Address    `json:"codeAddress"`
-	Gas           uint64             `json:"gas"`
-	GasCost       uint64             `json:"gasCost"`
-	Depth         int                `json:"depth"`
-	Error         error              `json:"error,omitempty"`
-	Stack         *[]string          `json:"stack,omitempty"`
-	Memory        *[]string          `json:"memory,omitempty"`
-	Storage       *map[string]string `json:"storage,omitempty"`
+	Pc            uint64            `json:"pc"`
+	Op            string            `json:"op"`
+	CallerAddress common.Address    `json:"callerAddress"`
+	CodeAddress   *common.Address   `json:"codeAddress"`
+	Gas           uint64            `json:"gas"`
+	GasCost       uint64            `json:"gasCost"`
+	Depth         int               `json:"depth"`
+	Error         error             `json:"error,omitempty"`
+	Stack         []string          `json:"stack,omitempty"`
+	Memory        []string          `json:"memory,omitempty"`
+	Storage       map[string]string `json:"storage,omitempty"`
 }
 
 // FormatLogs formats EVM returned structured logs for json output
@@ -751,21 +751,21 @@ func FormatLogs(logs []vm.StructLog) []StructLogRes {
 			for i, stackValue := range trace.Stack {
 				stack[i] = fmt.Sprintf("%x", math.PaddedBigBytes(stackValue, 32))
 			}
-			formatted[index].Stack = &stack
+			formatted[index].Stack = stack
 		}
 		if trace.Memory != nil {
 			memory := make([]string, 0, (len(trace.Memory)+31)/32)
 			for i := 0; i+32 <= len(trace.Memory); i += 32 {
 				memory = append(memory, fmt.Sprintf("%x", trace.Memory[i:i+32]))
 			}
-			formatted[index].Memory = &memory
+			formatted[index].Memory = memory
 		}
 		if trace.Storage != nil {
 			storage := make(map[string]string)
 			for i, storageValue := range trace.Storage {
 				storage[fmt.Sprintf("%x", i)] = fmt.Sprintf("%x", storageValue)
 			}
-			formatted[index].Storage = &storage
+			formatted[index].Storage = storage
 		}
 	}
 	return formatted
