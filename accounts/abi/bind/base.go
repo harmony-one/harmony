af48b05019d8ba196d8ae3042133017a07c11485
Leo Chen <leo@harmony.one>
2021-01-16 07:23:42 +0000

    [abi] hardcoded abi/bind to use shard0
    Signed-off-by: Leo Chen <leo@harmony.one>

--- accounts/abi/bind/base.go
+++ accounts/abi/bind/base.go
@@ -248,10 +248,12 @@ func (c *BoundContract) transact(opts *TransactOpts, contract *common.Address, i
 	}
 	// Create the transaction, sign it and schedule it for execution
 	var rawTx *types.Transaction
+	shardID := uint32(0)
+
 	if contract == nil {
-		rawTx = types.NewContractCreation(nonce, value, gasLimit, gasPrice, input)
+		rawTx = types.NewContractCreation(nonce, shardID, value, gasLimit, gasPrice, input) //FIXME: hardcoded shardID 0
 	} else {
-		rawTx = types.NewTransaction(nonce, c.address, value, gasLimit, gasPrice, input)
+		rawTx = types.NewTransaction(nonce, c.address, shardID, value, gasLimit, gasPrice, input) //FIXME: hardcoded shardID 0
 	}
 	if opts.Signer == nil {
 		return nil, errors.New("no signer to authorize the transaction with")
