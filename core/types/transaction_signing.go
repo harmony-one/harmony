5f9c4468e4214de5cc0ed0f4840e607cac4caff6
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2021-01-21 20:39:09 +0100

    Add comparisons for params.EthMainnetChainID - stops panics

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -122,7 +122,7 @@ func NewEIP155Signer(chainID *big.Int) EIP155Signer {
 // Equal checks if the given EIP155Signer is equal to another Signer.
 func (s EIP155Signer) Equal(s2 Signer) bool {
 	eip155, ok := s2.(EIP155Signer)
-	return ok && eip155.chainID.Cmp(s.chainID) == 0
+	return ok && (eip155.chainID.Cmp(params.EthMainnetChainID) >= 0 || eip155.chainID.Cmp(s.chainID) == 0)
 }
 
 var big8 = big.NewInt(8)
@@ -133,7 +133,7 @@ func (s EIP155Signer) Sender(tx InternalTransaction) (common.Address, error) {
 		return HomesteadSigner{}.Sender(tx)
 	}
 
-	if tx.ChainID().Cmp(s.chainID) != 0 {
+	if tx.ChainID().Cmp(params.EthMainnetChainID) < 0 && tx.ChainID().Cmp(s.chainID) != 0 {
 		return common.Address{}, ErrInvalidChainID
 	}
 	V := new(big.Int).Sub(tx.V(), s.chainIDMul)
