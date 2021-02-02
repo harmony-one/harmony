61f404f4b457f542c021f2aac7b6cb3dd8535cc4
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2021-02-02 15:15:29 -0800

    [travis] Cache build img
    Signed-off-by: Daniel Van Der Maden <dvandermaden0@berkeley.edu>

--- scripts/travis_rosetta_checker.sh
+++ scripts/travis_rosetta_checker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+CACHE_DIR="docker_images"
+docker load -i $CACHE_DIR/images.tar || true
 docker pull harmonyone/localnet-test
+docker save -o $CACHE_DIR/images.tar harmonyone/localnet-test
 docker run -v "$DIR/../:/go/src/github.com/harmony-one/harmony" harmonyone/localnet-test -r
