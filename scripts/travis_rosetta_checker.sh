0b6a90a28cbce69ba2e07c48f29d798eb5af5035
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2021-02-02 17:11:09 -0800

    [travis] Cache docker imgs (#3523)
    * [travis] Cache build img
    
    Signed-off-by: Daniel Van Der Maden <dvandermaden0@berkeley.edu>
    
    * [travis] Safely cache imgs
    
    Signed-off-by: Daniel Van Der Maden <dvandermaden0@berkeley.edu>

--- scripts/travis_rosetta_checker.sh
+++ scripts/travis_rosetta_checker.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+CACHE_DIR="docker_images"
+mkdir -p $CACHE_DIR
+echo "pulling cached docker img"
+docker load -i $CACHE_DIR/images.tar || true
 docker pull harmonyone/localnet-test
+echo "saving cached docker img"
+docker save -o $CACHE_DIR/images.tar harmonyone/localnet-test
 docker run -v "$DIR/../:/go/src/github.com/harmony-one/harmony" harmonyone/localnet-test -r
