d194244428b0d898ca9b46822a61cbeb2e154ea7
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2021-02-05 12:49:09 +0100

    Enable filter API:s for the eth_ namespace

--- node/api.go
+++ node/api.go
@@ -117,6 +117,12 @@ func (node *Node) APIs(harmony *hmy.Harmony) []rpc.API {
 			Service:   filters.NewPublicFilterAPI(harmony, false),
 			Public:    true,
 		},
+		{
+			Namespace: "eth",
+			Version:   hmy_rpc.APIVersion,
+			Service:   filters.NewPublicFilterAPI(harmony, false),
+			Public:    true,
+		},
 		hmy_rpc.NewPublicNetAPI(node.host, harmony.ChainID, hmy_rpc.Eth),
 		hmy_rpc.NewPublicWeb3API(),
 	}
