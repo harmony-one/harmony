e1d072b3ac2c92d616a0ca8829fd0a47f5d7af5b
Rongjian Lan <rongjian.lan@gmail.com>
2021-03-25 16:51:03 -0700

    revert change on insertHook

--- hmy/downloader/beaconhelper.go
+++ hmy/downloader/beaconhelper.go
@@ -128,12 +128,10 @@ func (bh *beaconHelper) insertLastMileBlocks() (inserted int, bn uint64, err err
 			return
 		}
 		bh.logger.Info().Uint64("number", b.NumberU64()).Msg("Inserted block from beacon pub-sub")
-		// TODO: clean up insertHook since for beacon chain sync, we shouldn't broadcast crosslinks for all sync nodes.
-		// Crosslink broadcasting is a live operation related to the beacon consensus, which should only
-		// be triggered when there is a new beacon block produced.
-		//if bh.insertHook != nil {
-		//	bh.insertHook()
-		//}
+
+		if bh.insertHook != nil {
+			bh.insertHook()
+		}
 		inserted++
 		bn++
 	}
