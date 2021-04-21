6705e233eedc387ccc5ca44525834b268def4ddc
Jacky Wang <jackyw.se@gmail.com>
2021-04-21 00:46:23 -0700

    [rpc] added current block number in utility metadata

--- rpc/common/types.go
+++ rpc/common/types.go
@@ -49,22 +49,23 @@ type ConsensusInternal struct {
 
 // NodeMetadata captures select metadata of the RPC answering node
 type NodeMetadata struct {
-	BLSPublicKey   []string           `json:"blskey"`
-	Version        string             `json:"version"`
-	NetworkType    string             `json:"network"`
-	ChainConfig    params.ChainConfig `json:"chain-config"`
-	IsLeader       bool               `json:"is-leader"`
-	ShardID        uint32             `json:"shard-id"`
-	CurrentEpoch   uint64             `json:"current-epoch"`
-	BlocksPerEpoch *uint64            `json:"blocks-per-epoch,omitempty"`
-	Role           string             `json:"role"`
-	DNSZone        string             `json:"dns-zone"`
-	Archival       bool               `json:"is-archival"`
-	NodeBootTime   int64              `json:"node-unix-start-time"`
-	PeerID         peer.ID            `json:"peerid"`
-	Consensus      ConsensusInternal  `json:"consensus"`
-	C              C                  `json:"p2p-connectivity"`
-	SyncPeers      map[string]int     `json:"sync-peers",omitempty`
+	BLSPublicKey    []string           `json:"blskey"`
+	Version         string             `json:"version"`
+	NetworkType     string             `json:"network"`
+	ChainConfig     params.ChainConfig `json:"chain-config"`
+	IsLeader        bool               `json:"is-leader"`
+	ShardID         uint32             `json:"shard-id"`
+	CurrentBlockNum uint64             `json:"current-number"`
+	CurrentEpoch    uint64             `json:"current-epoch"`
+	BlocksPerEpoch  *uint64            `json:"blocks-per-epoch,omitempty"`
+	Role            string             `json:"role"`
+	DNSZone         string             `json:"dns-zone"`
+	Archival        bool               `json:"is-archival"`
+	NodeBootTime    int64              `json:"node-unix-start-time"`
+	PeerID          peer.ID            `json:"peerid"`
+	Consensus       ConsensusInternal  `json:"consensus"`
+	C               C                  `json:"p2p-connectivity"`
+	SyncPeers       map[string]int     `json:"sync-peers",omitempty`
 }
 
 // P captures the connected peers per topic
