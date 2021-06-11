a270e8b1a7764192e7f1857e8ad6ed62a8e33f62
Jacky Wang <jackyw.se@gmail.com>
2021-06-10 18:48:06 -0700

    [p2p] small adjustment at code order

--- node/node.go
+++ node/node.go
@@ -804,9 +804,6 @@ func (node *Node) StartPubSub() error {
 			return err
 		}
 
-		semConsensus := semaphore.NewWeighted(p2p.SetAsideForConsensus)
-		msgChanConsensus := make(chan validated, MsgChanBuffer)
-
 		go func() {
 			select {
 			case <-node.psCtx.Done():
@@ -814,6 +811,9 @@ func (node *Node) StartPubSub() error {
 				rateLimiter.Start()
 			}
 		}()
+
+		semConsensus := semaphore.NewWeighted(p2p.SetAsideForConsensus)
+		msgChanConsensus := make(chan validated, MsgChanBuffer)
 
 		// goroutine to handle consensus messages
 		go func() {
