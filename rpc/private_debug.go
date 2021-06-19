1ef982a7313906c806968a461603f8f85b572930
Lutty <lutty@lutty.me>
2021-06-19 15:46:21 +0800

    add public debug api

--- rpc/private_debug.go
+++ rpc/private_debug.go
@@ -3,10 +3,8 @@ package rpc
 import (
 	"context"
 
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/harmony-one/harmony/hmy"
-	"github.com/harmony-one/harmony/internal/utils"
 )
 
 // PrivateDebugService Internal JSON RPC for debugging purpose
@@ -24,17 +22,6 @@ func NewPrivateDebugAPI(hmy *hmy.Harmony, version Version) rpc.API {
 		Service:   &PrivateDebugService{hmy, version},
 		Public:    false,
 	}
-}
-
-// SetLogVerbosity Sets log verbosity on runtime
-func (s *PrivateDebugService) SetLogVerbosity(ctx context.Context, level int) (map[string]interface{}, error) {
-	if level < int(log.LvlCrit) || level > int(log.LvlTrace) {
-		return nil, ErrInvalidLogLevel
-	}
-
-	verbosity := log.Lvl(level)
-	utils.SetLogVerbosity(verbosity)
-	return map[string]interface{}{"verbosity": verbosity.String()}, nil
 }
 
 // ConsensusViewChangingID return the current view changing ID to RPC
