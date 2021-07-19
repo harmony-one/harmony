cb273387a71c62660d7391cdda070dfae90ef7f3
niklr <r.niklaus@gmx.ch>
2021-07-19 23:44:47 +0200

    Fix pprof service race condition

--- api/service/pprof/service_test.go
+++ api/service/pprof/service_test.go
@@ -3,10 +3,14 @@ package pprof
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"os"
+	"path/filepath"
 	"reflect"
 	"runtime/pprof"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestUnpackProfilesIntoMap(t *testing.T) {
@@ -70,6 +74,29 @@ func TestUnpackProfilesIntoMap(t *testing.T) {
 			t.Errorf("Test %v: unexpected map\n\t%+v\n\t%+v", i, actual, test.expMap)
 		}
 	}
+}
+
+func TestStart(t *testing.T) {
+	input := &Config{
+		Enabled:          true,
+		Folder:           tempTestDir(),
+		ProfileNames:     []string{"cpu"},
+		ProfileIntervals: []int{1},
+	}
+	defer os.RemoveAll(input.Folder)
+	s := NewService(*input)
+	err := s.Start()
+	if assErr := assertError(err, nil); assErr != nil {
+		t.Fatal(assErr)
+	}
+	time.Sleep(1 * time.Second)
+}
+
+func tempTestDir() string {
+	tempDir := os.TempDir()
+	testDir := filepath.Join(tempDir, fmt.Sprintf("pprof-service-test-%d-%d", os.Getpid(), rand.Int()))
+	os.RemoveAll(testDir)
+	return testDir
 }
 
 func assertError(gotErr, expErr error) error {
