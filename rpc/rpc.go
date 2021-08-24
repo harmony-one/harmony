d58243b6301698eaf672c505723bcd144c2ad867
haodi <82733821@qq.com>
2021-08-25 00:42:40 +0800

    split API which need be authed

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -156,6 +156,8 @@ func getAPIs(hmy *hmy.Harmony, debugEnable bool, rateLimiterEnable bool, ratelim
 		NewPublicStakingAPI(hmy, V2),
 		NewPublicDebugAPI(hmy, V1),
 		NewPublicDebugAPI(hmy, V2),
+		NewPublicTraceAPI(hmy, Debug), // Debug version means geth trace rpc
+		NewPublicTraceAPI(hmy, Trace), // Trace version means parity trace rpc
 		// Legacy methods (subject to removal)
 		v1.NewPublicLegacyAPI(hmy, "hmy"),
 		eth.NewPublicEthService(hmy, "eth"),
