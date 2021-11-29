bb9811f36a4bb013337f69a88ed5f8c2c76fd2a5
徐志强 <652732310@qq.com>
2021-11-29 14:49:00 +0800

    fix compile

--- rpc/blockchain.go
+++ rpc/blockchain.go
@@ -6,8 +6,10 @@ import (
 	"math/big"
 	"time"
 
+	"encoding/hex"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/rpc"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/pkg/errors"
