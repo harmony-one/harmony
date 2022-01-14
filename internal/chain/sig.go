8f8b6293e0ea54b270ced8db631666cae4e6cffd
Leo Chen <leo@harmony.one>
2022-01-14 04:46:19 +0000

    Revert "fix DecodeSigBitmap (#3949)"
    This reverts commit 83f26ded15d5c7259112d3c800c8f85ca9e64f15.

--- internal/chain/sig.go
+++ internal/chain/sig.go
@@ -47,7 +47,6 @@ func DecodeSigBitmap(sigBytes bls.SerializedSignature, bitmap []byte, pubKeys []
 	}
 	if err := mask.SetMask(bitmap); err != nil {
 		utils.Logger().Warn().Err(err).Msg("mask.SetMask failed")
-		return nil, nil, errors.New("mask.SetMask failed")
 	}
 	return &aggSig, mask, nil
 }
