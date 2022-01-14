5b52117f8b0b383a1f325212f015757fe329c019
Leo Chen <leo@harmony.one>
2022-01-14 11:34:39 +0000

    [log] reduce log on vc check
    Signed-off-by: Leo Chen <leo@harmony.one>

--- consensus/checks.go
+++ consensus/checks.go
@@ -164,7 +164,7 @@ func (consensus *Consensus) onViewChangeSanityCheck(recvMsg *FBFTMessage) bool {
 	}
 	if consensus.IsViewChangingMode() &&
 		consensus.GetCurBlockViewID() > recvMsg.ViewID {
-		consensus.getLogger().Warn().Uint64("curBlockViewID", consensus.GetCurBlockViewID()).
+		consensus.getLogger().Debug().Uint64("curBlockViewID", consensus.GetCurBlockViewID()).
 			Uint64("msgViewID", recvMsg.ViewID).
 			Msg("[onViewChangeSanityCheck] ViewChanging ID Is Low")
 		return false
