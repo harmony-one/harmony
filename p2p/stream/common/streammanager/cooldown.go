471bd1accc55be1963eb883501350f9e5c0e5915
Jacky Wang <jackyw.se@gmail.com>
2022-02-07 19:00:51 -0800

    [p2p] add cooldown mechanism at discover

---
+++ p2p/stream/common/streammanager/cooldown.go
@@ -0,0 +1,38 @@
+package streammanager
+
+import (
+	"container/list"
+	"time"
+
+	"github.com/libp2p/go-libp2p-core/peer"
+
+	"github.com/whyrusleeping/timecache"
+)
+
+const (
+	coolDownPeriod = 1 * time.Minute
+)
+
+type coolDownCache struct {
+	timeCache *timecache.TimeCache
+}
+
+func newCoolDownCache() *coolDownCache {
+	tl := timecache.NewTimeCache(coolDownPeriod)
+	return &coolDownCache{
+		timeCache: tl,
+	}
+}
+
+// Has check and add the peer ID to the cache
+func (cache *coolDownCache) Has(id peer.ID) bool {
+	has := cache.timeCache.Has(string(id))
+	cache.timeCache.Add(string(id))
+	return has
+}
+
+// Reset reset the cooldown cache
+func (cache *coolDownCache) Reset() {
+	cache.timeCache.Q = list.New()
+	cache.timeCache.M = make(map[string]time.Time)
+}
