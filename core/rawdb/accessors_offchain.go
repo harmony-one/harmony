7e6b16aec8dca41f7c8954fa50ce5e48c8950221
lutty <lutty@lutty.me>
2022-02-16 16:57:48 +0800

    fix bug

--- core/rawdb/accessors_offchain.go
+++ core/rawdb/accessors_offchain.go
@@ -188,7 +188,7 @@ func IteratorValidatorSnapshot(iterator DatabaseIterator, cb func(addr common.Ad
 	iter := iterator.NewIteratorWithPrefix(validatorSnapshotPrefix)
 	defer iter.Release()
 
-	minKey = headerPrefix
+	minKey = validatorSnapshotPrefix
 	for iter.Next() {
 		// validatorSnapshotKey = validatorSnapshotPrefix + addr bytes (20 bytes) + epoch bytes
 		key := iter.Key()
