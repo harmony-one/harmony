dceb40fd7f6f1671267318a1bd22fea61d64f3b3
lutty <lutty@lutty.me>
2022-03-01 14:24:29 +0800

    add rosetta RCLONE_DB_0_URL

--- rosetta/infra/run.sh
+++ rosetta/infra/run.sh
@@ -6,6 +6,10 @@ DATA="$DIR/data"
 LOGS="$DATA/logs"
 BASE_ARGS=(--http.ip "0.0.0.0" --ws.ip "0.0.0.0" --http.rosetta --node_type "explorer" --datadir "$DATA" --log.dir "$LOGS")
 
+if [ -n "$RCLONE_DB_0_URL" ]; then
+  rclone -P -L sync $RCLONE_DB_0_URL $DATA/harmony_db_0 --multi-thread-streams 4 --transfers=8
+fi
+
 mkdir -p "$LOGS"
 echo -e NODE ARGS: \" "$@"  "${BASE_ARGS[@]}" \"
 echo "NODE VERSION: $(./harmony --version)"
