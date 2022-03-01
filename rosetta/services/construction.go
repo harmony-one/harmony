9cdaef35c2c183b085376d7adab19b411e1b668c
Lutty <lutty@lutty.me>
2022-03-02 01:27:50 +0800

    DefaultGasPrice

--- rosetta/services/construction.go
+++ rosetta/services/construction.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	// DefaultGasPrice ..
-	DefaultGasPrice = denominations.Nano
+	DefaultGasPrice = 40 * denominations.Nano
 )
 
 // ConstructAPI implements the server.ConstructAPIServicer interface.
