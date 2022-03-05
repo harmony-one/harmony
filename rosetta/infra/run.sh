738570242c9ec843e6f3e4ccc2aa4f6637ad48b1
haodi <82733821@qq.com>
2022-03-05 11:39:13 +0800

    change rosetta dockerfile

--- rosetta/infra/run.sh
+++ rosetta/infra/run.sh
@@ -1,17 +1,38 @@
 #!/usr/bin/env bash
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 DATA="$DIR/data"
 LOGS="$DATA/logs"
-BASE_ARGS=(--http.ip "0.0.0.0" --ws.ip "0.0.0.0" --http.rosetta --node_type "explorer" --datadir "$DATA" --log.dir "$LOGS")
-DATA_NAME="${DATA_NAME:=harmony_db_0}"
+DATA_NAME="${DATA_NAME:=harmony_sharddb_0}"
 
-if [ -n "$RCLONE_DB_0_URL" ]; then
-  rclone -P -L sync $RCLONE_DB_0_URL $DATA/$DATA_NAME --transfers=64
+MAINNET_22816573_SNAPSHOT="release:pub.harmony.one/mainnet.min.22816573/harmony_sharddb_0"
+
+case "$NETWORK" in
+mainnet)
+  CONFIG_PATH="-c /root/harmony-mainnet.conf"
+  ;;
+mainnet-22816573)
+  CONFIG_PATH="-c /root/harmony-mainnet.conf"
+  rclone -P -L sync $MAINNET_22816573_SNAPSHOT $DATA/$DATA_NAME --transfers=64
+  ;;
+testnet)
+  CONFIG_PATH="-c /root/harmony-pstn.conf"
+  ;;
+*)
+  echo "unknown network"
+  exit 1
+  ;;
+esac
+
+if [ "$MODE" = "offline" ]; then
+  BASE_ARGS=(--datadir "$DATA" --log.dir "$LOGS" --run.offline --dns.server false --dns.client false --sync.legacy.server false --sync.legacy.client false)
+else
+  BASE_ARGS=(--datadir "$DATA" --log.dir "$LOGS")
 fi
 
 mkdir -p "$LOGS"
-echo -e NODE ARGS: \" "$@"  "${BASE_ARGS[@]}" \"
-echo "NODE VERSION: $(./harmony --version)"
-"$DIR/harmony" "$@" "${BASE_ARGS[@]}"
+echo -e NODE ARGS: \" $CONFIG_PATH "$@" "${BASE_ARGS[@]}" \"
+echo "NODE VERSION: $($DIR/harmony --version)"
+
+"$DIR/harmony" $CONFIG_PATH "$@" "${BASE_ARGS[@]}"
