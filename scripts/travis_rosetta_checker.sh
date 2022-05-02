d2c99b0e46e4a9ad5004fe23eb85baeb4f4d627b
Konstantin <k.potapov@softpro.com>
2022-05-02 18:41:48 +0300

    Fix rosetta test.

--- scripts/travis_rosetta_checker.sh
+++ scripts/travis_rosetta_checker.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
+
+echo $TRAVIS_PULL_REQUEST_BRANCH
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-CACHE_DIR="docker_images"
-mkdir -p $CACHE_DIR
-echo "pulling cached docker img"
-docker load -i $CACHE_DIR/images.tar || true
-docker pull harmonyone/localnet-test
-echo "saving cached docker img"
-docker save -o $CACHE_DIR/images.tar harmonyone/localnet-test
+echo $DIR
+cd $HOME/gopath/src/github.com/harmony-one
+cd $(go env GOPATH)/src/github.com/harmony-one/harmony-test
+git checkout $TRAVIS_PULL_REQUEST_BRANCH || true
+git branch --show-current
+cd localnet
+docker build -t harmonyone/localnet-test .
 docker run -v "$DIR/../:/go/src/github.com/harmony-one/harmony" harmonyone/localnet-test -r
