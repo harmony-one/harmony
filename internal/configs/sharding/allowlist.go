d24a0fa5713963a31ef4a639bafa9cc66c74588e
peekpi <894646171@qq.com>
2022-05-24 13:37:49 +0800

    update allowlist of testnet

--- internal/configs/sharding/allowlist.go
+++ internal/configs/sharding/allowlist.go
@@ -23,6 +23,7 @@ func _BLS(pubkeys []string) []bls_cosi.PublicKeyWrapper {
 	return blsPubkeys
 }
 
+// each time to update the allowlist, it requires a hardfork.
 var mainnetAllowlist = Allowlist{
 	MaxLimitPerShard: 0,
 	BLSPublicKeys:    _BLS([]string{}),
@@ -32,6 +33,9 @@ var testnetAllowlist = Allowlist{
 	MaxLimitPerShard: 4,
 	BLSPublicKeys: _BLS([]string{
 		"7915b9cbae9d675af510cb252362b80ae6d68a3684bbea203bc30d2f5fda25ffcedfa3cf2a6c1d3051469379920a418d",
+		"ca06101eb8aa410d53c7b9cad494f5fae32f4b2ea083b4389af629aacf0d1996e6fca43a2ea7c7c89f6e131c1c35a704",
+		"a7b563a180629a121a3f78d2864b3d5c5b76d1672a3f9de9349fde9c7a3dad0922bf9a4e68cb38d033d6a9ddef754709",
+		"0a62ca435c5e48983b6124768b383d3f0b2d358326604aec692189c7833f4a52dff865c1515c32f315eb8e78eaceec11",
 	}),
 }
 
