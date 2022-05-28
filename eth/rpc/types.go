c0b3ecddd6f20babe43cf26ae46009c7e2d8ab6c
peekpi <894646171@qq.com>
2022-05-28 21:27:12 +0800

    align eip-1898: support block number or hash on state-related methods

--- eth/rpc/types.go
+++ eth/rpc/types.go
@@ -103,6 +103,22 @@ func (bn *BlockNumber) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+// MarshalText implements encoding.TextMarshaler. It marshals:
+// - "latest", "earliest" or "pending" as strings
+// - other numbers as hex
+func (bn BlockNumber) MarshalText() ([]byte, error) {
+	switch bn {
+	case EarliestBlockNumber:
+		return []byte("earliest"), nil
+	case LatestBlockNumber:
+		return []byte("latest"), nil
+	case PendingBlockNumber:
+		return []byte("pending"), nil
+	default:
+		return hexutil.Uint64(bn).MarshalText()
+	}
+}
+
 func (bn BlockNumber) Int64() int64 {
 	return (int64)(bn)
 }
@@ -114,6 +130,7 @@ type BlockNumberOrHash struct {
 }
 
 func (bnh *BlockNumberOrHash) UnmarshalJSON(data []byte) error {
+	fmt.Println("UnmarshalJSON: bnh")
 	type erased BlockNumberOrHash
 	e := erased{}
 	err := json.Unmarshal(data, &e)
