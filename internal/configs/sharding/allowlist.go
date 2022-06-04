ac1daef58c334dbbaace57acdda8c8eb0db44565
peekpi <894646171@qq.com>
2022-06-04 16:22:32 +0800

    rename _BLS() to BLS()

--- internal/configs/sharding/allowlist.go
+++ internal/configs/sharding/allowlist.go
@@ -11,7 +11,7 @@ type Allowlist struct {
 	BLSPublicKeys    []bls_cosi.PublicKeyWrapper
 }
 
-func _BLS(pubkeys []string) []bls_cosi.PublicKeyWrapper {
+func BLS(pubkeys []string) []bls_cosi.PublicKeyWrapper {
 	blsPubkeys := make([]bls_cosi.PublicKeyWrapper, len(pubkeys))
 	for i := range pubkeys {
 		if key, err := bls_cosi.WrapperPublicKeyFromString(pubkeys[i]); err != nil {
@@ -26,12 +26,12 @@ func _BLS(pubkeys []string) []bls_cosi.PublicKeyWrapper {
 // each time to update the allowlist, it requires a hardfork.
 var mainnetAllowlist = Allowlist{
 	MaxLimitPerShard: 0,
-	BLSPublicKeys:    _BLS([]string{}),
+	BLSPublicKeys:    BLS([]string{}),
 }
 
 var testnetAllowlist = Allowlist{
 	MaxLimitPerShard: 4,
-	BLSPublicKeys: _BLS([]string{
+	BLSPublicKeys: BLS([]string{
 		"7915b9cbae9d675af510cb252362b80ae6d68a3684bbea203bc30d2f5fda25ffcedfa3cf2a6c1d3051469379920a418d",
 		"cf8dad79f5da460462b190a4996f1701589139aa0d8a2202bdd10004ad3b0d9299165278f8002bcb49599444b3607802",
 		"a7b563a180629a121a3f78d2864b3d5c5b76d1672a3f9de9349fde9c7a3dad0922bf9a4e68cb38d033d6a9ddef754709",
@@ -41,7 +41,7 @@ var testnetAllowlist = Allowlist{
 
 var localnetAllowlist = Allowlist{
 	MaxLimitPerShard: 0,
-	BLSPublicKeys:    _BLS([]string{}),
+	BLSPublicKeys:    BLS([]string{}),
 }
 
 var emptyAllowlist = Allowlist{}
