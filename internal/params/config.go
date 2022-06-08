aa78453c36d2689e06487e43015be86b81fb34f6
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2022-06-08 19:21:27 +0000

    [params] testnet hard fork for cross shard xfer

--- internal/params/config.go
+++ internal/params/config.go
@@ -104,7 +104,7 @@ var (
 		StakingPrecompileEpoch:        big.NewInt(75175),
 		ChainIdFixEpoch:               big.NewInt(75877), // around Fri, 10 Jun 2022 06:10:18 GMT with average block time 2.0065s
 		SlotsLimitedEpoch:             big.NewInt(75684), // epoch to enable HIP-16, around Mon, 02 May 2022 08:18:45 UTC with 2s block time
-		CrossShardXferPrecompileEpoch: EpochTBD,
+		CrossShardXferPrecompileEpoch: big.NewInt(75877),
 		AllowlistEpoch:                big.NewInt(75877), // around Fri, 10 Jun 2022 06:10:18 GMT with average block time 2.0065s
 	}
 
