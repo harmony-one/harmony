271681a82bcdb361d0027bbad10c5692b538dc5f
“GheisMohammadi” <Gheis.Mohammadi@gmail.com>
2022-07-18 19:36:11 +0800

    removed extra not used files

--- api/service/stagedsync/types/types.go
+++
@@ -1,8 +0,0 @@
-package stagedsync
-
-// import "github.com/ledgerwatch/erigon/turbo/stages/bodydownload"
-
-// type DownloaderGlue interface {
-// 	ExecHeaderDownloadStage([]func() error, *StageState, Unwinder) error
-// 	ExecBodyDownloadStage(string, string, *StageState, Unwinder, *bodydownload.PrefetchedBlocks) (bool, error)
-// }
