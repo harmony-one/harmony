b03bde8f46b1a46150001acf4bb7a2701ab5e1d2
frozen <355847+Frozen@users.noreply.github.com>
2022-09-10 18:02:45 +0700

    fix

--- consensus/consensus_service.go
+++ consensus/consensus_service.go
@@ -213,7 +213,6 @@ func (consensus *Consensus) checkViewID(msg *FBFTMessage) error {
 		if !msg.HasSingleSender() {
 			return errors.New("Leader message can not have multiple sender keys")
 		}
-		fmt.Println("[checkViewID] Set LEADEER PUB KEY ", msg.SenderPubkeys[0].Bytes.Hex(), utils.GetPort())
 		consensus.LeaderPubKey = msg.SenderPubkeys[0]
 		consensus.IgnoreViewIDCheck.UnSet()
 		consensus.consensusTimeout[timeoutConsensus].Start()
@@ -397,7 +396,6 @@ func (consensus *Consensus) UpdateConsensusInformation() Mode {
 				Str("leaderPubKey", leaderPubKey.Bytes.Hex()).
 				Msg("[UpdateConsensusInformation] Most Recent LeaderPubKey Updated Based on BlockChain")
 			consensus.pubKeyLock.Lock()
-			fmt.Println("[UpdateConsensusInformation] Most Recent LeaderPubKey Updated Based on BlockChain", leaderPubKey.Bytes.Hex(), utils.GetPort())
 			consensus.LeaderPubKey = leaderPubKey
 			consensus.pubKeyLock.Unlock()
 		}
@@ -483,15 +481,6 @@ func (consensus *Consensus) SetViewChangingID(viewID uint64) {
 func (consensus *Consensus) StartFinalityCount() {
 	consensus.finalityCounter.Store(time.Now().UnixNano())
 }
-
-//func (consensus *Consensus) ReshardingNextLeader(newblock *types.Block) {
-//	consensus.pubKeyLock.Lock()
-//	fmt.Println("nextBlock1 ", newblock.Header().Number().Uint64(), " ", consensus.LeaderPubKey.Bytes.Hex())
-//	consensus.LeaderPubKey = consensus.getNextLeaderKey(consensus.GetCurBlockViewID() + 1)
-//	fmt.Println("nextBlock2 ", newblock.Header().Number().Uint64(), " ", consensus.LeaderPubKey.Bytes.Hex())
-//	consensus.pubKeyLock.Unlock()
-//
-//}
 
 // FinishFinalityCount calculate the current finality
 func (consensus *Consensus) FinishFinalityCount() {
