b1513d41dcd8e1a361db9b9bf790fad156e34922
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2022-10-10 10:10:25 +0000

    [p2p] add test to check gater non blocking

--- p2p/gater_test.go
+++ p2p/gater_test.go
@@ -8,10 +8,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestEmptyGater(t *testing.T) {
-	assert.Nil(t, NewGater(false))
-}
-
 func TestGaterBlocking(t *testing.T) {
 	gater := NewGater(true)
 	require.NotNil(t, &gater, "%s", &gater)
@@ -25,4 +21,19 @@ func TestGaterBlocking(t *testing.T) {
 	assert.Nil(t, err, "%s", err)
 	allowed = gater.InterceptAddrDial("somePeer", private)
 	assert.False(t, allowed, "%b", allowed)
+}
+
+func TestGaterNotBlocking(t *testing.T) {
+	gater := NewGater(false)
+	require.NotNil(t, &gater, "%s", &gater)
+
+	public, err := ma.NewMultiaddr("/ip4/1.1.1.1/udp/53")
+	assert.Nil(t, err, "%s", err)
+	allowed := gater.InterceptAddrDial("somePeer", public)
+	assert.True(t, allowed, "%b", allowed)
+
+	private, err := ma.NewMultiaddr("/ip4/192.168.1.1/tcp/80")
+	assert.Nil(t, err, "%s", err)
+	allowed = gater.InterceptAddrDial("somePeer", private)
+	assert.True(t, allowed, "%b", allowed)
 }
