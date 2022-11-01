a92cc1bb409c9344303c0afc0b67daf7ae6b156f
frozen <355847+Frozen@users.noreply.github.com>
2022-11-02 00:33:16 +0800

    Rwlock consensus.

--- api/service/consensus/service.go
+++ api/service/consensus/service.go
@@ -3,23 +3,21 @@ package consensus
 import (
 	msg_pb "github.com/harmony-one/harmony/api/proto/message"
 	"github.com/harmony-one/harmony/consensus"
-	"github.com/harmony-one/harmony/core/types"
 	"github.com/harmony-one/harmony/internal/utils"
 )
 
 // Service is the consensus service.
 type Service struct {
-	blockChannel chan *types.Block // The channel to receive new blocks from Node
-	consensus    *consensus.Consensus
-	stopChan     chan struct{}
-	stoppedChan  chan struct{}
-	startChan    chan struct{}
-	messageChan  chan *msg_pb.Message
+	consensus   *consensus.Consensus
+	stopChan    chan struct{}
+	stoppedChan chan struct{}
+	startChan   chan struct{}
+	messageChan chan *msg_pb.Message
 }
 
 // New returns consensus service.
-func New(blockChannel chan *types.Block, consensus *consensus.Consensus, startChan chan struct{}) *Service {
-	return &Service{blockChannel: blockChannel, consensus: consensus, startChan: startChan}
+func New(consensus *consensus.Consensus, startChan chan struct{}) *Service {
+	return &Service{consensus: consensus, startChan: startChan}
 }
 
 // Start starts consensus service.
@@ -27,7 +25,7 @@ func (s *Service) Start() error {
 	utils.Logger().Info().Msg("[consensus/service] Starting consensus service.")
 	s.stopChan = make(chan struct{})
 	s.stoppedChan = make(chan struct{})
-	s.consensus.Start(s.blockChannel, s.stopChan, s.stoppedChan, s.startChan)
+	s.consensus.Start(s.stopChan, s.stoppedChan, s.startChan)
 	s.consensus.WaitForNewRandomness()
 	return nil
 }
