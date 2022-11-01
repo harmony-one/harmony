8ecadb15955a4033f217128cd1b3719247e85750
frozen <355847+Frozen@users.noreply.github.com>
2022-11-02 01:11:36 +0800

    Removed channels.

--- node/service_setup.go
+++ node/service_setup.go
@@ -14,7 +14,7 @@ func (node *Node) RegisterValidatorServices() {
 	// Register consensus service.
 	node.serviceManager.Register(
 		service.Consensus,
-		consensus.New(node.Consensus, node.startConsensus),
+		consensus.New(node.Consensus),
 	)
 	// Register new block service.
 	node.serviceManager.Register(
