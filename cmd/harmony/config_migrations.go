c0ec7ff9547db36688ac6f198a3e31802a7d322a
peekpi <894646171@qq.com>
2022-11-03 18:53:57 +0800

    fix default harmony.config

--- cmd/harmony/config_migrations.go
+++ cmd/harmony/config_migrations.go
@@ -288,6 +288,13 @@ func init() {
 		confTree.Set("Version", "2.5.7")
 		return confTree
 	}
+	migrations["2.5.7"] = func(confTree *toml.Tree) *toml.Tree {
+		if confTree.Get("General.StatePruneEnable") == nil {
+			confTree.Set("General.StatePruneEnable", defaultConfig.General.StatePruneEnable)
+		}
+		confTree.Set("Version", "2.5.8")
+		return confTree
+	}
 
 	// check that the latest version here is the same as in default.go
 	largestKey := getNextVersion(migrations)
