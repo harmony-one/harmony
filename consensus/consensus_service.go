8e55c973415c2f17d6023e06442e3b3a48aee5ae
frozen <355847+Frozen@users.noreply.github.com>
2022-11-07 20:29:44 +0700

    Fixed locks.

--- consensus/consensus_service.go
+++ consensus/consensus_service.go
@@ -470,9 +470,8 @@ func (consensus *Consensus) updateConsensusInformation() Mode {
 // IsLeader check if the node is a leader or not by comparing the public key of
 // the node with the leader public key
 func (consensus *Consensus) IsLeader() bool {
-	// TODO: if remove locks blockchain stucks.
-	//consensus.mutex.RLock()
-	//defer consensus.mutex.RUnlock()
+	consensus.mutex.RLock()
+	defer consensus.mutex.RUnlock()
 
 	return consensus.isLeader()
 }
