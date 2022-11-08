bbc565e60b6a8b0102252b6e508e022a389cf423
frozen <355847+Frozen@users.noreply.github.com>
2022-11-08 19:30:57 +0700

    Rebased over leader rotation.

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -194,6 +194,10 @@ func (consensus *Consensus) getPublicKeys() multibls.PublicKeys {
 }
 
 func (consensus *Consensus) GetLeaderPubKey() *bls_cosi.PublicKeyWrapper {
+	return consensus.getLeaderPubKey()
+}
+
+func (consensus *Consensus) getLeaderPubKey() *bls_cosi.PublicKeyWrapper {
 	return consensus.LeaderPubKey
 }
 
@@ -201,10 +205,8 @@ func (consensus *Consensus) getLeaderPubKey() *bls_cosi.PublicKeyWrapper {
 	return consensus.LeaderPubKey
 }
 
-func (consensus *Consensus) SetLeaderPubKey(pub *bls_cosi.PublicKeyWrapper) {
-	consensus.pubKeyLock.Lock()
+func (consensus *Consensus) setLeaderPubKey(pub *bls_cosi.PublicKeyWrapper) {
 	consensus.LeaderPubKey = pub
-	consensus.pubKeyLock.Unlock()
 }
 
 func (consensus *Consensus) setLeaderPubKey(pub *bls_cosi.PublicKeyWrapper) {
