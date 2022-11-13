846eb4cccd8afa9589ba469ab029dc81224b2747
peekpi <894646171@qq.com>
2022-11-13 23:41:57 +0800

    check shardID in VerifyCrossLink

--- node/node_cross_link.go
+++ node/node_cross_link.go
@@ -169,3 +169,20 @@ func (node *Node) ProcessCrossLinkMessage(msgPayload []byte) {
 			Msgf("[ProcessingCrossLink] Add pending crosslinks,  total pending: %d", Len)
 	}
 }
+
+// VerifyCrossLink verifies the header is valid
+func (node *Node) VerifyCrossLink(cl types.CrossLink) error {
+	if node.Blockchain().ShardID() != shard.BeaconChainShardID {
+		return errors.New("[VerifyCrossLink] Shard chains should not verify cross links")
+	}
+	instance := shard.Schedule.InstanceForEpoch(node.Blockchain().CurrentHeader().Epoch())
+	if cl.ShardID() >= instance.NumShards() {
+		return errors.New("[VerifyCrossLink] ShardID should less than NumShards")
+	}
+	engine := node.Blockchain().Engine()
+
+	if err := engine.VerifyCrossLink(node.Blockchain(), cl); err != nil {
+		return errors.Wrap(err, "[VerifyCrossLink]")
+	}
+	return nil
+}
