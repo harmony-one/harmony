15c77764401d350f975380ea9af20c1cb5526646
frozen <355847+Frozen@users.noreply.github.com>
2022-11-18 18:24:48 +0700

    Fix formatting.

--- internal/params/config.go
+++ internal/params/config.go
@@ -318,8 +318,6 @@ var (
 		big.NewInt(0),                      // AllowlistEpoch
 		big.NewInt(1),                      // LeaderRotationEpoch
 		64,                                 // LeaderRotationBlocksCount
-		big.NewInt(1), // LeaderRotationEpoch
-		64,            // LeaderRotationBlocksCount
 		big.NewInt(0),                      // FeeCollectEpoch
 	}
 
@@ -360,9 +358,8 @@ var (
 		big.NewInt(0),        // SlotsLimitedEpoch
 		big.NewInt(1),        // CrossShardXferPrecompileEpoch
 		big.NewInt(0),        // AllowlistEpoch
-
-		big.NewInt(1), // LeaderRotationEpoch
-		64,            // LeaderRotationBlocksCount
+		big.NewInt(1),        // LeaderRotationEpoch
+		64,                   // LeaderRotationBlocksCount
 		big.NewInt(0),        // FeeCollectEpoch
 	}
 
