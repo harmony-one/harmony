b11800fe3f813b682c22f056d5b5ed4809c0da19
Max <82761650+MaxMustermann2@users.noreply.github.com>
2022-11-25 16:18:48 +0000

    [rpc] query node for syncing calls (#4309)
    * [rpc] query node for syncing calls
    
    Resolves #3966
    
    * fix(downloader): do not underflow

--- hmy/downloader/downloader.go
+++ hmy/downloader/downloader.go
@@ -120,11 +120,13 @@ func (d *Downloader) NumPeers() int {
 
 // IsSyncing return the current sync status
 func (d *Downloader) SyncStatus() (bool, uint64, uint64) {
+	current := d.bc.CurrentBlock().NumberU64()
 	syncing, target := d.status.get()
-	if !syncing {
-		target = d.bc.CurrentBlock().NumberU64()
+	if !syncing { // means synced
+		target = current
 	}
-	return syncing, target, 0
+	// isSyncing, target, blocks to target
+	return syncing, target, target - current
 }
 
 // SubscribeDownloadStarted subscribe download started
