57108cf7bf34f4af866ed3864d7bd39e2d987b77
“GheisMohammadi” <Gheis.Mohammadi@gmail.com>
2022-12-27 18:26:22 +0800

    fix spell

--- p2p/stream/types/stream.go
+++ p2p/stream/types/stream.go
@@ -23,7 +23,7 @@ type Stream interface {
 	CloseOnExit() error
 	FailedTimes() int
 	AddFailedTimes()
-	ResetFailedRimes()
+	ResetFailedTimes()
 }
 
 // BaseStream is the wrapper around
@@ -86,7 +86,7 @@ func (st *BaseStream) AddFailedTimes() {
 	st.failedTimes++
 }
 
-func (st *BaseStream) ResetFailedRimes() {
+func (st *BaseStream) ResetFailedTimes() {
 	st.failedTimes = 0
 }
 
