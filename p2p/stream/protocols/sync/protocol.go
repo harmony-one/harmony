3a9c537aed7374c3f67bd278a59f9bfa190e7983
“GheisMohammadi” <36589218+GheisMohammadi@users.noreply.github.com>
2023-01-06 09:02:40 +0800

    refactor stream peer cooldown and fix protocol beacon node field

--- p2p/stream/protocols/sync/protocol.go
+++ p2p/stream/protocols/sync/protocol.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/harmony-one/harmony/consensus/engine"
-	"github.com/harmony-one/harmony/core"
 	nodeconfig "github.com/harmony-one/harmony/internal/configs/node"
 	shardingconfig "github.com/harmony-one/harmony/internal/configs/sharding"
 	"github.com/harmony-one/harmony/internal/utils"
@@ -17,7 +16,6 @@ import (
 	"github.com/harmony-one/harmony/p2p/stream/common/requestmanager"
 	"github.com/harmony-one/harmony/p2p/stream/common/streammanager"
 	sttypes "github.com/harmony-one/harmony/p2p/stream/types"
-	"github.com/harmony-one/harmony/shard"
 	"github.com/hashicorp/go-version"
 	libp2p_host "github.com/libp2p/go-libp2p/core/host"
 	libp2p_network "github.com/libp2p/go-libp2p/core/network"
@@ -79,10 +77,6 @@ type (
 func NewProtocol(config Config) *Protocol {
 	ctx, cancel := context.WithCancel(context.Background())
 
-	isBeaconNode := config.Chain.ShardID() == shard.BeaconChainShardID
-	if _, ok := config.Chain.(*core.EpochChain); ok {
-		isBeaconNode = false
-	}
 	sp := &Protocol{
 		chain:      config.Chain,
 		beaconNode: config.BeaconNode,
