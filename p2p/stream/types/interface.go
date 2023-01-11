6eafc0ab64efacb3a7aa06d1aa8714e7158714ab
“GheisMohammadi” <36589218+GheisMohammadi@users.noreply.github.com>
2023-01-11 22:55:15 +0800

    refactor p2p host and routing

--- p2p/stream/types/interface.go
+++ p2p/stream/types/interface.go
@@ -13,6 +13,8 @@ type Protocol interface {
 	Specifier() string
 	Version() *version.Version
 	ProtoID() ProtoID
+	// ShardProtoID() ProtoID
+	IsBeaconNode() bool
 	Match(string) bool
 	HandleStream(st libp2p_network.Stream)
 }
