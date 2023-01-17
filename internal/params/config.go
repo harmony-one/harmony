d8e535c93daa704fb5a7e1c67a3071970706d645
frozen <355847+Frozen@users.noreply.github.com>
2023-01-17 13:22:30 -0300

    Merged leader rotation.

--- internal/params/config.go
+++ internal/params/config.go
@@ -275,8 +275,6 @@ var (
 		LeaderRotationEpoch:           EpochTBD,
 		LeaderRotationBlocksCount:     5,
 		FeeCollectEpoch:               big.NewInt(5),
-		LeaderRotationEpoch:           EpochTBD,
-		LeaderRotationBlocksCount:     5,
 	}
 
 	// AllProtocolChanges ...
