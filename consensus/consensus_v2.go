65a888a5f57c3f8cc3dc79fa1d532853d22b54ff
frozen <355847+Frozen@users.noreply.github.com>
2023-01-20 08:49:08 -0300

    fixes

--- consensus/consensus_v2.go
+++ consensus/consensus_v2.go
@@ -683,11 +683,11 @@ func (consensus *Consensus) commitBlock(blk *types.Block, committedMsg *FBFTMess
 // rotateLeader rotates the leader to the next leader in the committee.
 // This function must be called with enabled leader rotation.
 func (consensus *Consensus) rotateLeader(epoch *big.Int) {
-	prev := consensus.GetLeaderPubKey()
+	prev := consensus.getLeaderPubKey()
 	bc := consensus.Blockchain()
 	curNumber := bc.CurrentHeader().Number().Uint64()
 	utils.Logger().Info().Msgf("[Rotating leader] epoch: %v rotation:%v numblocks:%d", epoch.Uint64(), bc.Config().IsLeaderRotation(epoch), bc.Config().LeaderRotationBlocksCount)
-	leader := consensus.GetLeaderPubKey()
+	leader := consensus.getLeaderPubKey()
 	for i := 0; i < bc.Config().LeaderRotationBlocksCount; i++ {
 		header := bc.GetHeaderByNumber(curNumber - uint64(i))
 		if header == nil {
@@ -714,9 +714,9 @@ func (consensus *Consensus) rotateLeader(epoch *big.Int) {
 		utils.Logger().Error().Msg("Failed to get next leader")
 		return
 	} else {
-		consensus.SetLeaderPubKey(next)
+		consensus.setLeaderPubKey(next)
 	}
-	if consensus.IsLeader() && !consensus.GetLeaderPubKey().Object.IsEqual(prev.Object) {
+	if consensus.isLeader() && !consensus.getLeaderPubKey().Object.IsEqual(prev.Object) {
 		// leader changed
 		go func() {
 			consensus.ReadySignal <- SyncProposal
