9dabe148577bdf49a948b0168308e08edd6c0cc0
frozen <355847+Frozen@users.noreply.github.com>
2023-01-27 19:14:09 -0300

    Fix usage of private methods.

--- consensus/debug.go
+++ consensus/debug.go
@@ -12,10 +12,24 @@ func (consensus *Consensus) GetConsensusMode() string {
 
 // GetCurBlockViewID returns the current view ID of the consensus
 func (consensus *Consensus) GetCurBlockViewID() uint64 {
+	consensus.mutex.RLock()
+	defer consensus.mutex.RUnlock()
+	return consensus.getCurBlockViewID()
+}
+
+// GetCurBlockViewID returns the current view ID of the consensus
+func (consensus *Consensus) getCurBlockViewID() uint64 {
 	return consensus.current.GetCurBlockViewID()
 }
 
 // GetViewChangingID returns the current view changing ID of the consensus
 func (consensus *Consensus) GetViewChangingID() uint64 {
+	consensus.mutex.RLock()
+	defer consensus.mutex.RUnlock()
+	return consensus.current.GetViewChangingID()
+}
+
+// GetViewChangingID returns the current view changing ID of the consensus
+func (consensus *Consensus) getViewChangingID() uint64 {
 	return consensus.current.GetViewChangingID()
 }
