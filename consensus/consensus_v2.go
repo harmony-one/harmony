3af8a96cb3f92b301a7331f80967e1661702d9c5
frozen <355847+Frozen@users.noreply.github.com>
2023-01-31 12:04:33 -0300

    Removed deadcode, LockedFBFTPhase.

--- consensus/consensus_v2.go
+++ consensus/consensus_v2.go
@@ -450,21 +450,6 @@ func (consensus *Consensus) BlockChannel(newBlock *types.Block) {
 	}
 }
 
-// waitForCommit wait extra 2 seconds for commit phase to finish
-func (consensus *Consensus) waitForCommit() {
-	if consensus.mode() != Normal || consensus.phase.Get() != FBFTCommit {
-		return
-	}
-	// We only need to wait consensus is in normal commit phase
-	utils.Logger().Warn().Str("phase", consensus.phase.String()).Msg("[shutdown] commit phase has to wait")
-
-	maxWait := time.Now().Add(2 * consensus.BlockPeriod)
-	for time.Now().Before(maxWait) && consensus.getConsensusPhase() == "Commit" {
-		utils.Logger().Warn().Msg("[shutdown] wait for consensus finished")
-		time.Sleep(time.Millisecond * 100)
-	}
-}
-
 // LastMileBlockIter is the iterator to iterate over the last mile blocks in consensus cache.
 // All blocks returned are guaranteed to pass the verification.
 type LastMileBlockIter struct {
