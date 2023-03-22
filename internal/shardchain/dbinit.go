5758569465f068925887617c26abf564b8fd761c
frozen <355847+Frozen@users.noreply.github.com>
2023-03-22 01:48:12 -0300

    Network type.

--- internal/shardchain/dbinit.go
+++ internal/shardchain/dbinit.go
@@ -1,8 +1,11 @@
 package shardchain
 
-import "github.com/ethereum/go-ethereum/ethdb"
+import (
+	"github.com/ethereum/go-ethereum/ethdb"
+	nodeconfig "github.com/harmony-one/harmony/internal/configs/node"
+)
 
 // DBInitializer initializes a newly created chain database.
 type DBInitializer interface {
-	InitChainDB(db ethdb.Database, shardID uint32) error
+	InitChainDB(db ethdb.Database, networkType nodeconfig.NetworkType, shardID uint32) error
 }
