45e1b32b6b64285752bec7dc14a4e151eaed354d
“GheisMohammadi” <36589218+GheisMohammadi@users.noreply.github.com>
2023-05-02 07:47:02 +0800

    integrate snapshot feature with statedb and add cache to states

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -24,7 +24,7 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/state/snapshot"
+	"github.com/harmony-one/harmony/core/state/snapshot"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/metrics"
