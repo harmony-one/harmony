107c577ce60c62b519bf0dbec60f9f991b241e16
frozen <355847+Frozen@users.noreply.github.com>
2023-05-10 22:18:44 -0500

    Buildx multiplatform.

--- api/service/legacysync/downloader/build.sh
+++ api/service/legacysync/downloader/build.sh
@@ -1,3 +1,4 @@
 
-
-docker build -t frozen621/harmony-proto:latest -f Proto.Dockerfile --progress=plain .
+# if command fails, try this
+# docker buildx create --use
+docker buildx build -t frozen621/harmony-proto:latest --platform linux/amd64,linux/arm64 -f Proto.Dockerfile --progress=plain .
