9df6215ab2db9904f8e0c07841b413004fbf89d5
Max <82761650+MaxMustermann2@users.noreply.github.com>
2023-06-28 18:49:48 +0000

    scripts: restore install_build_tools.sh (#4457)
    With #4427, the protofiles are generated using a Docker image and thus
    `protoc` related binaries are not required. However, `mockgen` and
    `golangci-lint` are still being used by our Travis build. This change
    restores the file that installs them, and removes `protoc` related files
    from the list of installations.

--- tools/tools.go
+++ tools/tools.go
@@ -11,6 +11,5 @@ package tools
 // scripts/install_build_tools.sh parses these imports to install them.
 import (
 	_ "github.com/golang/mock/mockgen"
-	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 )
