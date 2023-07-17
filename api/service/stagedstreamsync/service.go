ce2178735a8dbc89088242763aed6d5d23deadb3
“GheisMohammadi” <36589218+GheisMohammadi@users.noreply.github.com>
2023-07-17 10:23:21 +0800

    pass consensus to stream sync through downloader

--- api/service/stagedstreamsync/service.go
+++ api/service/stagedstreamsync/service.go
@@ -1,6 +1,7 @@
 package stagedstreamsync
 
 import (
+	"github.com/harmony-one/harmony/consensus"
 	"github.com/harmony-one/harmony/core"
 	"github.com/harmony-one/harmony/p2p"
 )
@@ -11,9 +12,9 @@ type StagedStreamSyncService struct {
 }
 
 // NewService creates a new downloader service
-func NewService(host p2p.Host, bcs []core.BlockChain, config Config, dbDir string) *StagedStreamSyncService {
+func NewService(host p2p.Host, bcs []core.BlockChain, consensus *consensus.Consensus, config Config, dbDir string) *StagedStreamSyncService {
 	return &StagedStreamSyncService{
-		Downloaders: NewDownloaders(host, bcs, dbDir, config),
+		Downloaders: NewDownloaders(host, bcs, consensus, dbDir, config),
 	}
 }
 
