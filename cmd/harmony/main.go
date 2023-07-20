6b97fe86f731a2f194087f78fb1c8794b7546dee
“GheisMohammadi” <36589218+GheisMohammadi@users.noreply.github.com>
2023-07-20 10:16:23 +0800

    add debug mode to stream sync help debugging syncing issues

--- cmd/harmony/main.go
+++ cmd/harmony/main.go
@@ -606,6 +606,7 @@ func createGlobalConfig(hc harmonyconfig.HarmonyConfig) (*nodeconfig.ConfigType,
 	nodeConfig.VerifyHeaderBatchSize = hc.Sync.StagedSyncCfg.VerifyHeaderBatchSize
 	nodeConfig.InsertChainBatchSize = hc.Sync.StagedSyncCfg.InsertChainBatchSize
 	nodeConfig.LogProgress = hc.Sync.StagedSyncCfg.LogProgress
+	nodeConfig.DebugMode = hc.Sync.StagedSyncCfg.DebugMode
 	// P2P private key is used for secure message transfer between p2p nodes.
 	nodeConfig.P2PPriKey, _, err = utils.LoadKeyFromFile(hc.P2P.KeyFile)
 	if err != nil {
@@ -943,6 +944,7 @@ func setupStagedSyncService(node *node.Node, host p2p.Host, hc harmonyconfig.Har
 		SmHiCap:              hc.Sync.DiscHighCap,
 		SmDiscBatch:          hc.Sync.DiscBatch,
 		LogProgress:          node.NodeConfig.LogProgress,
+		DebugMode:            node.NodeConfig.DebugMode,			
 	}
 
 	// If we are running side chain, we will need to do some extra works for beacon
