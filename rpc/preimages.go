5f609f769203c2594fb2f86d5de04507df795dd7
Diego Nava <diego.nava77@hotmail.com>
2023-09-08 19:17:03 +0100

    add the generate method

--- rpc/preimages.go
+++ rpc/preimages.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/harmony-one/harmony/core"
 	"github.com/harmony-one/harmony/eth/rpc"
@@ -26,4 +27,15 @@ func NewPreimagesAPI(hmy *hmy.Harmony, version string) rpc.API {
 func (s *PreimagesService) Export(ctx context.Context, path string) error {
 	// these are by default not blocking
 	return core.ExportPreimages(s.hmy.BlockChain, path)
+}
+func (s *PreimagesService) Generate(ctx context.Context, start, end uint64) error {
+	if number := s.hmy.CurrentBlock().NumberU64(); number > end {
+		fmt.Printf(
+			"Cropping generate endpoint from %d to %d\n",
+			number, end,
+		)
+		end = number
+	}
+	// these are by default not blocking
+	return core.GeneratePreimages(s.hmy.BlockChain, start, end)
 }
