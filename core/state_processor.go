edb9c865ff4ea32d91a69e0de3e166d4c5b9271a
Diego Nava <diego.nava77@hotmail.com>
2023-09-17 17:02:19 +0200

    add balance on the error

--- core/state_processor.go
+++ core/state_processor.go
@@ -312,7 +312,7 @@ func ApplyTransaction(config *params.ChainConfig, bc ChainContext, author *commo
 	// Apply the transaction to the current state (included in the env)
 	result, err := ApplyMessage(vmenv, msg, gp)
 	if err != nil {
-		return nil, nil, nil, 0, fmt.Errorf("apply failed from='%s' to='%s': %w", msg.From().Hex(), msg.To().Hex(), err)
+		return nil, nil, nil, 0, fmt.Errorf("apply failed from='%s' to='%s' balance='%s': %w", msg.From().Hex(), msg.To().Hex(), statedb.GetBalance(msg.From()).String(), err)
 	}
 	// Update the state with pending changes
 	var root []byte
