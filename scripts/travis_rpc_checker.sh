38dcda7dd857ac8eaf40e3e7c42c0e7e23b5b580
frozen <355847+Frozen@users.noreply.github.com>
2023-10-11 15:12:29 -0400

    Go 1.21

--- scripts/travis_rpc_checker.sh
+++ scripts/travis_rpc_checker.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+echo $TRAVIS_PULL_REQUEST_BRANCH
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 echo $DIR
 echo $GOPATH
 cd $GOPATH/src/github.com/harmony-one/harmony-test
 git fetch
+git checkout $TRAVIS_PULL_REQUEST_BRANCH || true
 git pull
 git branch --show-current
 cd localnet
