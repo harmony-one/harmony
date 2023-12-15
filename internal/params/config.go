92a1fea3977a5cbd10c7a968bb9feada2fcead7b
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2023-12-15 16:13:26 +0000

    internal: testnet max rate schedule

--- internal/params/config.go
+++ internal/params/config.go
@@ -119,7 +119,7 @@ var (
 		ValidatorCodeFixEpoch:                 big.NewInt(1296), // 2023-04-28 07:14:20+00:00
 		HIP30Epoch:                            big.NewInt(2176), // 2023-10-12 10:00:00+00:00
 		BlockGas30MEpoch:                      big.NewInt(2176), // 2023-10-12 10:00:00+00:00
-		MaxRateEpoch:                          EpochTBD,
+		MaxRateEpoch:                          big.NewInt(2520), // 2023-12-16 12:17:14+00:00
 	}
 	// PangaeaChainConfig contains the chain parameters for the Pangaea network.
 	// All features except for CrossLink are enabled at launch.
