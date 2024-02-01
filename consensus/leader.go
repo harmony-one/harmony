a6b7383bd634303c7f3af9b401b6ff9c876f5017
Diego Nava <diego.nava77@hotmail.com>
2024-02-01 14:48:18 +0100

    fix(consensus): add the IsAllSigsCollected

--- consensus/leader.go
+++ consensus/leader.go
@@ -296,7 +296,7 @@ func (consensus *Consensus) onCommit(recvMsg *FBFTMessage) {
 	quorumIsMet := consensus.decider.IsQuorumAchieved(quorum.Commit)
 	//// Read - End
 
-	if !quorumWasMet && quorumIsMet {
+	if (!quorumWasMet && quorumIsMet) || consensus.decider.IsAllSigsCollected() {
 		logger.Info().Msg("[OnCommit] 2/3 Enough commits received")
 		consensus.fBFTLog.MarkBlockVerified(blockObj)
 
