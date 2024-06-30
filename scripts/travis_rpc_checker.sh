f204b08bf0da1af1531aaf466a68260d3053d26b
frozen <355847+Frozen@users.noreply.github.com>
2024-06-29 20:25:15 -0400

    Fixed rosetta

--- scripts/travis_rpc_checker.sh
+++ scripts/travis_rpc_checker.sh
@@ -6,8 +6,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 echo $DIR
 echo $GOPATH
 pwd
-#echo "$DIR/../:/go/src/github.com/harmony-one/harmony"
 docker build --progress=plain -t harmonyone/localnet-test -f Tests.Dockerfile .
-
-#docker run -v "$DIR/../:/go/src/github.com/harmony-one/harmony" harmonyone/localnet-test -n
 docker run harmonyone/localnet-test -n
