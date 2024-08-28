b9f39c46cc4eae923d6033e8d473dbf70d15c19d
GheisMohammadi <Gheis.Mohammadi@gmail.com>
2024-08-28 15:49:33 +0800

    todo for enable services

--- cmd/bootnode/main.go
+++ cmd/bootnode/main.go
@@ -196,6 +196,14 @@ func main() {
 	rpcConfigs.HTTPPort = *httpPort
 	rpcConfigs.WSPort = *wsPort
 
+	// TODO: enable boot services
+	/*
+	if err := currentBootNode.StartServices(); err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
+		os.Exit(-1)
+	}
+	*/
+	
 	if err := currentBootNode.StartRPC(); err != nil {
 		utils.Logger().Error().
 			Err(err).
