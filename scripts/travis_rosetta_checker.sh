a44e996e40754a79a9dcd9b4f16008e40e56f3a3
Uladzislau Muraveika <uladzislau.muraveika@protofire.io>
2024-08-28 16:14:06 +0300

    chore(CI) - point tests to the master

--- scripts/travis_rosetta_checker.sh
+++ scripts/travis_rosetta_checker.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-#TODO: return back to master
-TEST_REPO_BRANCH="feature/go1.22"
 TEST_REPO_BRANCH=${TEST_REPO_BRANCH:-master}
 # handle for the Travis build run:
 # * uses TRAVIS_PULL_REQUEST_BRANCH for RP branch
