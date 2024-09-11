3e23739cf8d197077f413b4f27843fa9af5091f3
Uladzislau Muraveika <uladzislau.muraveika@protofire.io>
2024-09-11 17:08:33 +0300

    feat(Makefile) - cover verbose mode for the local debug (#4755)

--- test/debug-external.sh
+++ test/debug-external.sh
@@ -5,4 +5,9 @@ rm -rf tmp_log* 2> /dev/null
 rm *.rlp 2> /dev/null
 rm -rf .dht* 2> /dev/null
 scripts/go_executable_build.sh -S || exit 1  # dynamic builds are faster for debug iteration...
-./test/deploy.sh -B -D 600000 ./test/configs/local-resharding-with-external.txt
+if [ "${VERBOSE}" = "true" ]; then
+    echo "[WARN] - running with verbose logs"
+    ./test/deploy.sh -v -B -D 600000 ./test/configs/local-resharding-with-external.txt
+else
+    ./test/deploy.sh -B -D 600000 ./test/configs/local-resharding-with-external.txt
+fi
