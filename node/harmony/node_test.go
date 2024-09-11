5a1903ca80fac25a005e74ea216703f2dab9017e
frozen <355847+Frozen@users.noreply.github.com>
2024-09-10 20:54:34 -0400

    Fix for localnet syncing ports.

--- node/harmony/node_test.go
+++ node/harmony/node_test.go
@@ -113,22 +113,22 @@ func TestLocalSyncingPeerProvider(t *testing.T) {
 		p := makeLocalSyncingPeerProvider()
 		expectedBeaconPeers := []p2p.Peer{
 			{IP: "127.0.0.1", Port: "6000"},
-			{IP: "127.0.0.1", Port: "6002"},
 			{IP: "127.0.0.1", Port: "6004"},
+			{IP: "127.0.0.1", Port: "6008"},
 		}
 		if actualPeers, err := p.SyncingPeers(0); assert.NoError(t, err) {
-			assert.ElementsMatch(t, actualPeers, expectedBeaconPeers)
+			assert.ElementsMatch(t, actualPeers[:3], expectedBeaconPeers)
 		}
 	})
 	t.Run("Shard1Chain", func(t *testing.T) {
 		p := makeLocalSyncingPeerProvider()
 		expectedShard1Peers := []p2p.Peer{
 			// port 6001 omitted because self
-			{IP: "127.0.0.1", Port: "6003"},
-			{IP: "127.0.0.1", Port: "6005"},
+			{IP: "127.0.0.1", Port: "6002"},
+			{IP: "127.0.0.1", Port: "6006"},
 		}
 		if actualPeers, err := p.SyncingPeers(1); assert.NoError(t, err) {
-			assert.ElementsMatch(t, actualPeers, expectedShard1Peers)
+			assert.ElementsMatch(t, actualPeers[:2], expectedShard1Peers)
 		}
 	})
 	t.Run("InvalidShard", func(t *testing.T) {
