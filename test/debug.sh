991d1a0f266673a0a1e8e9e157cfb28dc364c650
Uladzislau Muraveika <uladzislau.muraveika@protofire.io>
2024-09-25 08:02:12 +0300

    feat(Makefile) - add switch for the legacy sync, make stream sync (#4761)
    default option

--- test/debug.sh
+++ test/debug.sh
@@ -5,9 +5,16 @@ rm -rf tmp_log* 2> /dev/null
 rm *.rlp 2> /dev/null
 rm -rf .dht* 2> /dev/null
 scripts/go_executable_build.sh -S || exit 1  # dynamic builds are faster for debug iteration...
+LEGACY_SYNC=${LEGACY_SYNC:-"false"}
+if [ "${LEGACY_SYNC}" == "true" ]; then
+    echo "[WARN] - using legacy sync"
+    SYNC_OPT=(-L "true")
+else
+    SYNC_OPT=(-L "false")
+fi
 if [ "${VERBOSE}" = "true" ]; then
     echo "[WARN] - running with verbose logs"
-    ./test/deploy.sh -v -B -D 600000 ./test/configs/local-resharding.txt
+    ./test/deploy.sh -v -B -D 600000 "${SYNC_OPT[@]}" ./test/configs/local-resharding.txt
 else
-    ./test/deploy.sh -B -D 600000 ./test/configs/local-resharding.txt
+    ./test/deploy.sh -B -D 600000 "${SYNC_OPT[@]}" ./test/configs/local-resharding.txt
 fi
