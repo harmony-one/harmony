123898fc8b77dd52a94fb0324d54aaa73a46c46b
GheisMohammadi <Gheis.Mohammadi@gmail.com>
2024-10-30 21:40:10 +0800

    stream sync unsets consensus sync flags in long range

--- api/service/stagedstreamsync/syncing.go
+++ api/service/stagedstreamsync/syncing.go
@@ -345,6 +345,8 @@ func (s *StagedStreamSync) doSync(downloaderContext context.Context, initSync bo
 			s.logger.Info().Uint64("current number", curBN).Uint64("target number", estimatedHeight).
 				Msg(WrapStagedSyncMsg("early return of long range sync (chain is already ahead of target height)"))
 			return estimatedHeight, 0, nil
+		} else if curBN < estimatedHeight && s.consensus != nil {
+			s.consensus.BlocksNotSynchronized("StagedStreamSync.doSync")
 		}
 	}
 
