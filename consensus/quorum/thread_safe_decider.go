fffd55b8076da502ef6c80426775adc9bb234048
frozen <355847+Frozen@users.noreply.github.com>
2024-11-19 18:20:38 -0400

    Added tests and changed function to NthNext.

--- consensus/quorum/thread_safe_decider.go
+++ consensus/quorum/thread_safe_decider.go
@@ -56,6 +56,12 @@ func (a threadSafeDeciderImpl) NthNextValidator(slotList shard.SlotList, pubKey 
 	return a.decider.NthNextValidator(slotList, pubKey, next)
 }
 
+func (a threadSafeDeciderImpl) NthNext(pubKey *bls.PublicKeyWrapper, next int) (*bls.PublicKeyWrapper, error) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	return a.decider.NthNext(pubKey, next)
+}
+
 func (a threadSafeDeciderImpl) NthNextValidatorV2(slotList shard.SlotList, pubKey *bls.PublicKeyWrapper, next int) (bool, *bls.PublicKeyWrapper) {
 	a.mu.Lock()
 	defer a.mu.Unlock()
