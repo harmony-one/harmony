37e81e15b4cc7d059fff4b61ab31e81dd75d0a51
frozen <355847+Frozen@users.noreply.github.com>
2024-11-27 18:04:18 -0400

    Fixed tests.

--- consensus/quorum/quorum.go
+++ consensus/quorum/quorum.go
@@ -280,7 +280,7 @@ func (s *cIdentities) NthNextValidator(slotList shard.SlotList, pubKey *bls.Publ
 			Str("key", pubKey.Bytes.Hex()).
 			Msg("[NthNextHmy] pubKey not found")
 	}
-	for i := 0; i < len(slotList); i++ {
+	for {
 		numNodes := len(s.publicKeys)
 		idx = (idx + next) % numNodes
 		if publicToAddress[s.publicKeys[idx].Bytes] == publicToAddress[pubKey.Bytes] {
@@ -290,7 +290,6 @@ func (s *cIdentities) NthNextValidator(slotList shard.SlotList, pubKey *bls.Publ
 		}
 		return found, &s.publicKeys[idx]
 	}
-	return false, pubKey
 }
 
 func (s *cIdentities) NthNextHmy(instance shardingconfig.Instance, pubKey *bls.PublicKeyWrapper, next int) (bool, *bls.PublicKeyWrapper) {
