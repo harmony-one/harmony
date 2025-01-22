a63980b987af793489e8b1017ca6b3555dd0a5e6
Soph <35721420+sophoah@users.noreply.github.com>
2025-01-22 13:42:26 +0700

    update harmony and bootnode version (#4834)
    * update harmony and bootnode version
    * add support for commitAt

--- cmd/config/version.go
+++ cmd/config/version.go
@@ -3,13 +3,11 @@ package config
 import (
 	"fmt"
 	"os"
+	"path"
+	"time"
 
 	"github.com/harmony-one/harmony/internal/cli"
 	"github.com/spf13/cobra"
-)
-
-const (
-	versionFormat = "Harmony (C) 2023. %v, version %v-%v (%v %v)"
 )
 
 var VersionMetaData []interface{}
@@ -36,7 +34,22 @@ func VersionFlag() cli.BoolFlag {
 }
 
 func GetHarmonyVersion() string {
-	return fmt.Sprintf(versionFormat, VersionMetaData[:5]...) // "harmony", version, commit, builtBy, builtAt
+	var version, commit, builtBy, builtAt, commitAt string
+	version = VersionMetaData[1].(string)
+	commit = VersionMetaData[2].(string)
+	commitAt = VersionMetaData[3].(string)
+	builtBy = VersionMetaData[4].(string)
+	builtAt = VersionMetaData[5].(string)
+
+	//Harmony (C) 2025. harmony, version v8507-v2024.3.1-85-g735e68a31-dirty (soph@ 2025-01-21T14:07:44+0700)
+	commitYear, err := time.Parse("2006-01-02T15:04:05-0700", commitAt)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing commit date: %v\n", err)
+		os.Exit(1)
+	}
+	var currentYear = commitYear.Year()
+	versionFormat := fmt.Sprintf("Harmony (C) %d. %%v, version %%v-%%v (%%v %%v)", currentYear)
+	return fmt.Sprintf(versionFormat, path.Base(os.Args[0]), version, commit, builtBy, builtAt)
 }
 
 func PrintVersion() {
