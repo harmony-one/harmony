75a93a7a9c0943bcf2bbde985e428c23ea551506
GheisMohammadi <Gheis.Mohammadi@gmail.com>
2025-02-04 14:48:53 +0800

    disabled go generate for mockery, fix goimports

--- p2p/gating/blocking.go
+++ p2p/gating/blocking.go
@@ -9,7 +9,7 @@ import (
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 )
 
-//TODO: go:generate mockery --name BlockingConnectionGater --output mocks/ --with-expecter=true
+// TODO: [DISABLED] go:generate mockery --name BlockingConnectionGater --output mocks/ --with-expecter=true
 type BlockingConnectionGater interface {
 	connmgr.ConnectionGater
 
