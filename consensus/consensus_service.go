bcbe4c1620d4428063cc14e832a478e733636e7c
frozen <355847+Frozen@users.noreply.github.com>
2025-02-13 20:06:55 -0400

    fixed effectiveGasPrice.

--- consensus/consensus_service.go
+++ consensus/consensus_service.go
@@ -365,7 +365,7 @@ func (consensus *Consensus) updateConsensusInformation(reason string) Mode {
 		consensus.BlockPeriod = 2 * time.Second
 	}
 	if consensus.Blockchain().Config().IsOneSecond(nextEpoch) {
-		consensus.BlockPeriod = 1 * time.Second
+		consensus.BlockPeriod = 950 * time.Millisecond
 	}
 
 	isFirstTimeStaking := consensus.Blockchain().Config().IsStaking(nextEpoch) &&
