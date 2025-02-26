b8c0c8fdd74140993054d7d2bb0377d6f3a0a66c
frozen <355847+Frozen@users.noreply.github.com>
2025-02-26 18:27:32 -0400

    Fixed test variable dereference.

--- rpc/harmony/v2/types_test.go
+++ rpc/harmony/v2/types_test.go
@@ -63,7 +63,7 @@ func TestNewReceipt(t *testing.T) {
 		}
 		r, err = NewReceipt(tx, blockHash, blockNumber, blockIndex, rec)
 		require.NoError(t, err)
-		require.EqualValues(t, 1, MustReceiptEffectivePrice(r))
+		require.EqualValues(t, 1, *MustReceiptEffectivePrice(r))
 	})
 	t.Run("effectiveGasPrice-staking", func(t *testing.T) {
 		unsigned, err := helpers.CreateTestStakingTransaction(payloadMaker, nil, 0, gasLimit, gasPrice)
