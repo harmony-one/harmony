4a7864c3494b07b255d682d3c88a819b652e8d2d
frozen <355847+Frozen@users.noreply.github.com>
2025-03-06 22:21:16 -0500

    Fixed code

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -553,7 +553,7 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 	if err == nil && !maxCodeSizeExceeded {
 		createDataGas := uint64(len(ret)) * params.CreateDataGas
 		if contract.UseGas(createDataGas) {
-			evm.StateDB.SetCode(address, ret)
+			evm.StateDB.SetCode(address, ret, false)
 		} else {
 			err = ErrCodeStoreOutOfGas
 		}
