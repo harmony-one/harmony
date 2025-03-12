daf78075d9bd765e978a65fd464c57f0a9be9818
frozen <355847+Frozen@users.noreply.github.com>
2025-03-12 19:58:37 -0400

    BlockContext

--- hmy/tracers/block_tracer.go
+++ hmy/tracers/block_tracer.go
@@ -251,7 +251,7 @@ func (jst *ParityBlockTracer) CaptureStart(env *vm.EVM, from common.Address, to 
 	jst.cur.blockHash = env.StateDB.BlockHash()
 	jst.cur.transactionPosition = uint64(env.StateDB.TxIndex())
 	jst.cur.transactionHash = env.StateDB.TxHashETH()
-	jst.cur.blockNumber = env.BlockNumber.Uint64()
+	jst.cur.blockNumber = env.Context.BlockNumber.Uint64()
 	jst.cur.descended = false
 	jst.cur.push(&jst.cur.action)
 	return nil
