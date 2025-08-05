3724bce708688bb11b0225aa079ab97971fe8ba5
GheisMohammadi <Gheis.Mohammadi@gmail.com>
2025-08-05 23:28:40 +0800

    refactor: remove old sync service locations
    - Remove old legacysync from api/service/legacysync/
    - Remove old stagedstreamsync from api/service/stagedstreamsync/
    - Remove old downloader from hmy/downloader/
    - Clean up old sync service structure

--- api/service/legacysync/downloader/build.sh
+++
@@ -1,4 +0,0 @@
-
-# if command fails, try this
-# docker buildx create --use
-docker buildx build -t frozen621/harmony-proto:latest --platform linux/amd64,linux/arm64 -f Proto.Dockerfile --progress=plain .
