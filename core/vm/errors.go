8bf0e4cb5837f37a753e64c957d81b0e46039ffd
frozen <355847+Frozen@users.noreply.github.com>
2025-08-11 20:23:25 -0400

    Uncomment execution reverted error and clean up commented code in EVM

--- core/vm/errors.go
+++ core/vm/errors.go
@@ -28,12 +28,12 @@ var (
 	ErrDepth                    = errors.New("max call depth exceeded")
 	ErrInsufficientBalance      = errors.New("insufficient balance for transfer")
 	ErrContractAddressCollision = errors.New("contract address collision")
-	//ErrExecutionReverted        = errors.New("execution reverted") // TODO should be uncommented in future version
-	ErrMaxCodeSizeExceeded     = errors.New("max code size exceeded")
-	ErrInvalidJump             = errors.New("invalid jump destination")
-	ErrWriteProtection         = errors.New("write protection")
-	ErrReturnDataOutOfBounds   = errors.New("return data out of bounds")
-	ErrGasUintOverflow         = errors.New("gas uint64 overflow")
+	ErrExecutionReverted        = errors.New("execution reverted")
+	ErrMaxCodeSizeExceeded      = errors.New("max code size exceeded")
+	ErrInvalidJump              = errors.New("invalid jump destination")
+	ErrWriteProtection          = errors.New("write protection")
+	ErrReturnDataOutOfBounds    = errors.New("return data out of bounds")
+	ErrGasUintOverflow          = errors.New("gas uint64 overflow")
 	ErrInvalidCode              = errors.New("invalid code: must not begin with 0xef")
 	ErrNonceUintOverflow        = errors.New("nonce uint64 overflow")
 
