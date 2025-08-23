3c239931d72c507d2b2ba27b7fb189fd8beda98c
GheisMohammadi <Gheis.Mohammadi@gmail.com>
2025-08-23 22:05:34 +0800

    Fix progress tracking logic and remove unused code
    - Fix bug where HasProgress was called with wrong parameters causing false timeouts
    - Remove unused HasProgress method and lastCheckedSize field
    - Simplify progress tracker to focus only on essential functionality
    - Clean up unused test code for removed methods

--- p2p/stream/types/progress_tracker_test.go
+++ p2p/stream/types/progress_tracker_test.go
@@ -39,34 +39,6 @@ func TestProgressTracker_UpdateProgress(t *testing.T) {
 	}
 }
 
-func TestProgressTracker_HasProgress(t *testing.T) {
-	pt := NewProgressTracker(30*time.Second, 1024)
-
-	// No progress initially
-	if pt.HasProgress(0) {
-		t.Error("Should not have progress initially")
-	}
-
-	// Update with data
-	pt.UpdateProgress(2048)
-
-	// Check progress - should have progress since we updated with 2048 bytes
-	// and the threshold is 1024, so 2048 - 0 = 2048 >= 1024
-	if !pt.HasProgress(2048) {
-		t.Error("Should have progress after update")
-	}
-
-	// Check that calling HasProgress with the same size doesn't show progress
-	if pt.HasProgress(2048) {
-		t.Error("Should not show progress for same size")
-	}
-
-	// Check that calling HasProgress with larger size shows progress
-	if !pt.HasProgress(4096) {
-		t.Error("Should show progress for larger size")
-	}
-}
-
 func TestProgressTracker_ShouldTimeout(t *testing.T) {
 	pt := NewProgressTracker(100*time.Millisecond, 1024)
 
