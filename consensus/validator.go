d2049a4ba240e6f632c4274270027725c8c67f02
GheisMohammadi <Gheis.Mohammadi@gmail.com>
2025-09-01 04:34:12 +0800

    change block verification log in OnPrepared to Debug

--- consensus/validator.go
+++ consensus/validator.go
@@ -237,7 +237,7 @@ func (consensus *Consensus) onPrepared(recvMsg *FBFTMessage) {
 
 	var blockObj *types.Block
 	if blockObj, err = consensus.validateNewBlock(recvMsg); err != nil {
-		consensus.getLogger().Err(err).
+		consensus.getLogger().Debug().Err(err).
 			Uint64("MsgBlockNum", recvMsg.BlockNum).
 			Uint64("MsgViewID", recvMsg.ViewID).
 			Msg("[OnPrepared] failed to verify new block")
