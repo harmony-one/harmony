ac38e84ec003a2185d7814c58af2f4b694528dd0
frozen <355847+Frozen@users.noreply.github.com>
2025-11-05 18:47:09 -0400

    Add logging for transaction tracing results in tracer.go

--- rpc/harmony/tracer.go
+++ rpc/harmony/tracer.go
@@ -223,6 +223,9 @@ func (s *PublicTracerService) TraceCall(ctx context.Context, args CallArgs, bloc
 			config.Stateoverrides.Apply(statedb, precompiles)
 		}
 	}
+
+	rs, err := s.hmy.TraceTx(ctx, msg, vmctx, statedb, config)
+	fmt.Printf("%T rs:`%v`, err: %s\n", rs, rs, err)
 	// Trace the transaction and return
 	return s.hmy.TraceTx(ctx, msg, vmctx, statedb, config)
 }
