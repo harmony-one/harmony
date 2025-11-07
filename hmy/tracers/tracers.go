5a44dbc602486aad78a5ea3570760a736c080b1e
frozen <355847+Frozen@users.noreply.github.com>
2025-11-07 15:12:56 -0400

    Enhance error message for tracer not found in tracers.go to include lookup count

--- hmy/tracers/tracers.go
+++ hmy/tracers/tracers.go
@@ -71,7 +71,7 @@ func New(code string, ctx *Context, cfg json.RawMessage) (Tracer, error) {
 			return tracer, nil
 		}
 	}
-	return nil, errors.Errorf("tracer not found for code `%s`", code)
+	return nil, errors.Errorf("tracer not found for code `%s`, lookups %d", code, len(lookups))
 }
 
 type ctorFn func(*Context, json.RawMessage) (Tracer, error)
