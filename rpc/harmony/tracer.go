edc7c0c6f0796cac60ad1c984cbe6130d8f6bb9f
frozen <355847+Frozen@users.noreply.github.com>
2025-12-22 15:11:05 -0400

    memory tracing in TraceTransaction

--- rpc/harmony/tracer.go
+++ rpc/harmony/tracer.go
@@ -186,6 +186,9 @@ func (s *PublicTracerService) TraceTransaction(ctx context.Context, hash common.
 	}
 	config.DisableMemory = false
 	config.DisableStack = false
+	if config.DisableMemory {
+		panic(321)
+	}
 	return s.hmy.TraceTx(ctx, msg, vmctx, statedb, config)
 }
 
