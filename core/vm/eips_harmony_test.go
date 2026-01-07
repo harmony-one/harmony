da78a4068b3c7d1c34ed98099f795a04bff558a3
frozen <355847+Frozen@users.noreply.github.com>
2026-01-07 19:52:29 -0400

    fix(tests): enhance opChainID tests for enabled and disabled chain ID scenarios

--- core/vm/eips_harmony_test.go
+++ core/vm/eips_harmony_test.go
@@ -8,25 +8,51 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestOnChainID(t *testing.T) {
-	v := EVMInterpreter{
-		evm: &EVM{
-			Context: BlockContext{
-				EpochNumber: big.NewInt(100),
-			},
-			chainConfig: &params.ChainConfig{
-				ChainIdFixEpoch:      big.NewInt(1323),
-				EthCompatibleChainID: big.NewInt(12345),
-				ChainID:              big.NewInt(1),
-			},
-		},
-	}
+func TestOpChainID(t *testing.T) {
 
-	stack := newstack()
-	_, err := opChainID(nil, &v, &ScopeContext{Stack: stack})
-	if err != nil {
-		t.Fatalf("opChainID error: %v", err)
-	}
-	rs := stack.pop()
-	require.Equal(t, rs.Uint64(), uint64(1))
+	t.Run("disabled-chainid-fix", func(t *testing.T) {
+		v := EVMInterpreter{
+			evm: &EVM{
+				Context: BlockContext{
+					EpochNumber: big.NewInt(100),
+				},
+				chainConfig: &params.ChainConfig{
+					ChainIdFixEpoch:      big.NewInt(1323),
+					EthCompatibleChainID: big.NewInt(12345),
+					ChainID:              big.NewInt(1),
+				},
+			},
+		}
+
+		stack := newstack()
+		_, err := opChainID(nil, &v, &ScopeContext{Stack: stack})
+		if err != nil {
+			t.Fatalf("opChainID error: %v", err)
+		}
+		rs := stack.pop()
+		require.Equal(t, rs.Uint64(), uint64(1))
+	})
+
+	t.Run("enabled-chainid-fix", func(t *testing.T) {
+		v := EVMInterpreter{
+			evm: &EVM{
+				Context: BlockContext{
+					EpochNumber: big.NewInt(1325),
+				},
+				chainConfig: &params.ChainConfig{
+					ChainIdFixEpoch:      big.NewInt(1323),
+					EthCompatibleChainID: big.NewInt(12345),
+					ChainID:              big.NewInt(1),
+				},
+			},
+		}
+
+		stack := newstack()
+		_, err := opChainID(nil, &v, &ScopeContext{Stack: stack})
+		if err != nil {
+			t.Fatalf("opChainID error: %v", err)
+		}
+		rs := stack.pop()
+		require.Equal(t, rs.Uint64(), uint64(1))
+	})
 }
