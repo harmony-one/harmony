f1d341eadd7fa85f39031d5b422c007c58dd3121
frozen <355847+Frozen@users.noreply.github.com>
2026-03-18 22:02:45 -0400

    removed block_tracer.go and block_tracer_storage.go

--- hmy/tracer.go
+++ hmy/tracer.go
@@ -741,6 +741,7 @@ func (hmy *Harmony) traceTx(ctx context.Context, message core.Message, txctx *tr
 			return nil, err
 		}
 	}
+	fmt.Printf("tracer: %T, %v\n", tracer, config)
 	// Define a meaningful timeout of a single transaction trace
 	if config.Timeout != nil {
 		if timeout, err = time.ParseDuration(*config.Timeout); err != nil {
