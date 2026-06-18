1c0f6b7ec5d23d5aef7a6a3615da8c45003164b5
frozen <355847+Frozen@users.noreply.github.com>
2026-06-18 00:45:21 -0400

    ci: update Docker build script to use configurable cache scope

--- scripts/build_harmony_test_image.sh
+++ scripts/build_harmony_test_image.sh
@@ -6,10 +6,10 @@ main_repo_branch=${1:?missing main repo branch}
 main_repo_org=${2:?missing main repo org}
 image_tag=${3:-harmonyone/localnet-test}
 
-cache_scope_branch=$(printf "%s" "${main_repo_branch}" | tr -c "[:alnum:]_.-" "-")
-cache_scope="harmony-test-localnet-${cache_scope_branch}"
+cache_scope=${HARMONY_TEST_DOCKER_CACHE_SCOPE:-harmony-test-localnet}
 
 if [[ "${HARMONY_TEST_DOCKER_CACHE:-false}" == "true" ]]; then
+	echo "Building ${image_tag} with Docker Buildx cache scope: ${cache_scope}"
 	docker buildx build \
 		--build-arg MAIN_REPO_BRANCH="${main_repo_branch}" \
 		--build-arg MAIN_REPO_ORG="${main_repo_org}" \
@@ -19,6 +19,7 @@ if [[ "${HARMONY_TEST_DOCKER_CACHE:-false}" == "true" ]]; then
 		--load \
 		-t "${image_tag}" .
 else
+	echo "Building ${image_tag} without Docker Buildx cache"
 	docker build \
 		--build-arg MAIN_REPO_BRANCH="${main_repo_branch}" \
 		--build-arg MAIN_REPO_ORG="${main_repo_org}" \
