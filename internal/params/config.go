d89e1ed5c408cd6de653e470ba76ee71835c7ba6
GheisMohammadi <Gheis.Mohammadi@gmail.com>
2026-08-19 15:24:41 +0330

    Set mainnet activation epoch to TBD

--- internal/params/config.go
+++ internal/params/config.go
@@ -105,7 +105,7 @@ var (
 		SlashBallotSignerFixEpoch:             big.NewInt(2964),
 		VerifyBeaconHeaderSlashEpoch:          big.NewInt(2964),
 		BloomEpoch:                            big.NewInt(2964),
-		StrictStateValidationEpoch:            big.NewInt(3003),
+		StrictStateValidationEpoch:            EpochTBD,
 	}
 
 	// TestnetChainConfig contains the chain parameters to run a node on the harmony test network.
